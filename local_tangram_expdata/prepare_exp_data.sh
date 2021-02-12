4d8d41dfb81f31b8ebf1c568a0465ba662dd6800
Arta <arta.seyedian@gmail.com>
2021-02-12 17:03:53 -0500

    automated downloading from JHPCE

--- local_tangram_expdata/prepare_exp_data.sh
+++ local_tangram_expdata/prepare_exp_data.sh
@@ -4,11 +4,11 @@
 #  https://github.com/broadinstitute/Tangram/blob/master/example/1_tutorial_tangram.ipynb
 mkdir data/
 
-echo "Downloading scRNAseq data..."
-sftp aseyedia@jhpce-transfer01.jhsph.edu
+echo "Downloading scRNAseq and spatial data..."
+sftp aseyedia@jhpce-transfer01.jhsph.edu << EOF
 get /dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/spatialDLPFC/analysis/sce_combined.rda data/sce_combined.rda
-
-echo "Downloading spatial data..."
-get /dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/forAmazonS3/SCE_DLPFC_tran-etal.rda
+get /dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/forAmazonS3/SCE_DLPFC_tran-etal.rda data/SCE_DLPFC_tran-etal.rda
+bye
+EOF
 
 exit 0
