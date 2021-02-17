1a5a7f8349945b383be32ec121ac5626a6f3ed17
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-02-17 14:56:47 -0500

    UPdate code style

--- analysis/sample_metrics.R
+++ analysis/sample_metrics.R
@@ -1,25 +1,55 @@
+## Automatically style the code in this script:
+styler::style_file(here::here("analysis", "sample_metrics.R"),
+    transformers = biocthis::bioc_style()
+)
+
+
 library("ggplot2")
 library("sessioninfo")
 
 
-sample_names <- c("DLPFC_Br2743_ant_manual_alignment", "DLPFC_Br2743_mid_manual_alignment","DLPFC_Br2743_post_manual_alignment","DLPFC_Br3942_ant_manual_alignment","DLPFC_Br3942_mid_manual_alignment","DLPFC_Br3942_post_manual_alignment","DLPFC_Br6423_ant_manual_alignment","DLPFC_Br6423_mid_manual_alignment","DLPFC_Br6423_post_manual_alignment","DLPFC_Br8492_ant_manual_alignment","DLPFC_Br8492_mid_manual_alignment","DLPFC_Br8492_post_manual_alignment")
+sample_names <-
+    c(
+        "DLPFC_Br2743_ant_manual_alignment",
+        "DLPFC_Br2743_mid_manual_alignment",
+        "DLPFC_Br2743_post_manual_alignment",
+        "DLPFC_Br3942_ant_manual_alignment",
+        "DLPFC_Br3942_mid_manual_alignment",
+        "DLPFC_Br3942_post_manual_alignment",
+        "DLPFC_Br6423_ant_manual_alignment",
+        "DLPFC_Br6423_mid_manual_alignment",
+        "DLPFC_Br6423_post_manual_alignment",
+        "DLPFC_Br8492_ant_manual_alignment",
+        "DLPFC_Br8492_mid_manual_alignment",
+        "DLPFC_Br8492_post_manual_alignment"
+    )
 dir_outputs <- here::here("outputs", "NextSeq")
 
 df_metrics_all <- NULL
-for (sample_name in sample_names){
-  dir_csv <- file.path(dir_outputs, sample_name, "outs", "metrics_summary.csv")
-  df_metrics <- read.csv(dir_csv, header = TRUE)
-  df_metrics_all <- rbind(df_metrics_all,df_metrics)
+for (sample_name in sample_names) {
+    dir_csv <-
+        file.path(dir_outputs, sample_name, "outs", "metrics_summary.csv")
+    df_metrics <- read.csv(dir_csv, header = TRUE)
+    df_metrics_all <- rbind(df_metrics_all, df_metrics)
 }
 
 # save
 sample_metrics <- df_metrics_all
-save(sample_metrics, file = here::here("rdata", "spe", "sample_metrics.Rdata"))
-write.csv(sample_metrics, file = here::here("rdata", "spe", "sample_metrics.csv"))
+save(sample_metrics,
+    file = here::here("rdata", "spe", "sample_metrics.Rdata")
+)
+write.csv(sample_metrics,
+    file = here::here("rdata", "spe", "sample_metrics.csv")
+)
 
 
 ## Read in older data
-pilot_metrics <- read.table("/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/Analysis/visium_dlpfc_pilot_sample_metrics.tsv", header = TRUE, sep = "\t")
+pilot_metrics <-
+    read.table(
+        "/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/Analysis/visium_dlpfc_pilot_sample_metrics.tsv",
+        header = TRUE,
+        sep = "\t"
+    )
 
 ## Reshape the data
 pilot_metrics <- as.data.frame(t(pilot_metrics))
@@ -28,7 +58,7 @@ pilot_metrics <- pilot_metrics[-1, ]
 rownames(pilot_metrics) <- gsub("X", "", rownames(pilot_metrics))
 
 ## Make it usable in R
-for(i in c(
+for (i in c(
     "Estimated.Number.of.Spots",
     "Mean.Reads.per.Spot",
     "Median.Genes.per.Spot",
@@ -42,7 +72,7 @@ for(i in c(
 )) {
     pilot_metrics[[i]] <- as.numeric(gsub(",", "", pilot_metrics[[i]]))
 }
-for(i in c(
+for (i in c(
     "Valid.Barcodes",
     "Sequencing.Saturation",
     "Q30.Bases.in.Barcode",
@@ -61,10 +91,13 @@ for(i in c(
     pilot_metrics[[i]] <- as.numeric(gsub("%", "", pilot_metrics[[i]]))
 }
 summary(pilot_metrics)
-save(pilot_metrics, file = here::here("rdata", "spe", "pilot_metrics.Rdata"))
+save(pilot_metrics,
+    file = here::here("rdata", "spe", "pilot_metrics.Rdata")
+)
 
 
-shared_cols <- intersect(colnames(sample_metrics), colnames(pilot_metrics))
+shared_cols <-
+    intersect(colnames(sample_metrics), colnames(pilot_metrics))
 shared_cols
 #  [1] "Number.of.Reads"                                "Mean.Reads.per.Spot"
 #  [3] "Median.Genes.per.Spot"                          "Median.UMI.Counts.per.Spot"
@@ -82,11 +115,48 @@ shared_metrics <- rbind(
 shared_metrics$study <- rep(c("current", "pilot"), each = 12)
 
 
-pdf(here::here("plots", "spaceranger_metrics_by_number_of_reads.pdf"), useDingbats = FALSE, width = 10)
-ggplot(shared_metrics, aes(x = Mean.Reads.per.Spot, y = Number.of.Reads / 1e6)) + geom_point() + geom_smooth(method = "lm") + theme_bw(base_size = 20) + facet_grid(~study)
+pdf(
+    here::here("plots", "spaceranger_metrics_by_number_of_reads.pdf"),
+    useDingbats = FALSE,
+    width = 10
+)
+ggplot(
+    shared_metrics,
+    aes(x = Mean.Reads.per.Spot, y = Number.of.Reads / 1e6)
+) +
+    geom_point() +
+    geom_smooth(method = "lm") +
+    theme_bw(base_size = 20) +
+    facet_grid(~
+    study)
 
-ggplot(shared_metrics, aes(x = Median.Genes.per.Spot, y = Number.of.Reads / 1e6, color = study)) + geom_point() + geom_smooth(method = "lm") + theme_bw(base_size = 20)
-ggplot(shared_metrics, aes(x = Total.Genes.Detected, y = Number.of.Reads / 1e6, color = study)) + geom_point() + geom_smooth(method = "lm") + theme_bw(base_size = 20)
-ggplot(shared_metrics, aes(x = Median.UMI.Counts.per.Spot, y = Number.of.Reads / 1e6, color = study)) + geom_point() + geom_smooth(method = "lm") + theme_bw(base_size = 20)
+ggplot(
+    shared_metrics,
+    aes(x = Median.Genes.per.Spot, y = Number.of.Reads / 1e6, color = study)
+) +
+    geom_point() +
+    geom_smooth(method = "lm") +
+    theme_bw(base_size = 20)
+ggplot(
+    shared_metrics,
+    aes(x = Total.Genes.Detected, y = Number.of.Reads / 1e6, color = study)
+) +
+    geom_point() +
+    geom_smooth(method = "lm") +
+    theme_bw(base_size = 20)
+ggplot(
+    shared_metrics,
+    aes(x = Median.UMI.Counts.per.Spot, y = Number.of.Reads / 1e6, color = study)
+) +
+    geom_point() +
+    geom_smooth(method = "lm") +
+    theme_bw(base_size = 20)
 
 dev.off()
+
+
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
