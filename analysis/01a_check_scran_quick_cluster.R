60a7f76004c1cb1a1487145a63d7f870e50aabb2
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-02-17 21:57:45 -0500

    Start exploring the scuttle quick clusters for #14. Note that the previous clusters from Abby & Brenda match the new ones. I'm not sure why cluster 89 was the one failing before and now it's 64. Either we had the wrong info through options(error = recover) or the warning in scuttle 1.1.15 doesn't use the same info. I'll explore cluster 64 for now

--- analysis/01a_check_scran_quick_cluster.R
+++ analysis/01a_check_scran_quick_cluster.R
@@ -17,19 +17,59 @@ library("SpatialExperiment")
 ## vis
 library("spatialLIBD")
 
-## Load SPE raw data
+## Load SPE data
 load(here::here("rdata", "spe", "spe.Rdata"), verbose = TRUE)
 
 ## Check cluster 89 from before
-load(file = here::here("analysis", "clusters_nonzero.rda"), verbose = TRUE)
-table(clusters == "89")
+load(
+    file = here::here("analysis", "clusters_nonzero.rda"),
+    verbose = TRUE
+)
+
+table(spe$scran_quick_cluster == clusters)
+## Clusters did change!
+# FALSE  TRUE
+# 41244  8755
+
+## old cluster "89" from https://github.com/LTLA/scuttle/issues/7
+## is not the same one now, despite the number of spots matching
+identical(which(spe$scran_quick_cluster == "89"), which(clusters == "89")) # FALSE
+stopifnot(identical(sum(spe$scran_quick_cluster == "89"), sum(clusters == "89")))
+
+## Hm... maybe it's because the samples are in a different order!
+## due to https://github.com/LieberInstitute/spatialDLPFC/blob/main/analysis/01_build_SPE.R#L72
+stopifnot(sum(table(spe$scran_quick_cluster) - table(clusters)) == 0)
+## Ok, that matches. Let's check more closely then.
+load(
+    file = here::here("analysis", "sce_nonzero.rda"),
+    verbose = TRUE
+)
+m <-
+    match(spe$key, with(colData(sce_nonzero), paste0(barcode_id, "_", sample_name)))
+## Ok, the quick clusters are the same ones!
+stopifnot(identical(spe$scran_quick_cluster, clusters[m]))
+
+## Remove the older R objects since we don't need them
+rm(clusters, sce_nonzero)
+
+table(spe$scran_quick_cluster == "89")
 # FALSE  TRUE
 # 49727   272
-stopifnot(length(clusters) == ncol(spe))
-spe$quick_cluster_89 <- factor(clusters == "89", levels = c("TRUE", "FALSE"))
-colSums(as.matrix(qcfilter[clusters == "89", ]))
-# low_lib_size            low_n_features high_subsets_Mito_percent                   discard
-#            2                         3                         0                         3
+spe$quick_cluster_89 <-
+    factor(spe$scran_quick_cluster == "89", levels = c("TRUE", "FALSE"))
+
+summary(as.data.frame(colData(spe)[spe$scran_quick_cluster == "89", c(
+    "scran_low_lib_size",
+    "scran_low_n_features",
+    "scran_high_subsets_Mito_percent",
+    "scran_discard"
+)]))
+# scran_low_lib_size scran_low_n_features scran_high_subsets_Mito_percent
+# TRUE : 62          TRUE : 86            TRUE :  2
+# FALSE:210          FALSE:186            FALSE:270
+# scran_discard
+# TRUE : 87
+# FALSE:185
 vis_grid_clus(
     spe = spe,
     clustervar = "quick_cluster_89",
@@ -38,9 +78,32 @@ vis_grid_clus(
     colors = c("FALSE" = "grey90", "TRUE" = "orange")
 )
 
+## Now check cluster 64 since that's the one giving the error at
+## https://github.com/LieberInstitute/spatialDLPFC/blob/main/analysis/01_build_SPE.R#L338
+spe$quick_cluster_64 <-
+    factor(spe$scran_quick_cluster == "64", levels = c("TRUE", "FALSE"))
+summary(as.data.frame(colData(spe)[spe$scran_quick_cluster == "64", c(
+    "scran_low_lib_size",
+    "scran_low_n_features",
+    "scran_high_subsets_Mito_percent",
+    "scran_discard"
+)]))
+# scran_low_lib_size scran_low_n_features scran_high_subsets_Mito_percent
+# TRUE :  0          TRUE :  0            TRUE : 56
+# FALSE:542          FALSE:542            FALSE:486
+# scran_discard
+# TRUE : 56
+# FALSE:486
+vis_grid_clus(
+    spe = spe,
+    clustervar = "quick_cluster_64",
+    pdf = here::here("plots", paste0("scuttle_", "quick_cluster_64", ".pdf")),
+    sort_clust = FALSE,
+    colors = c("FALSE" = "grey90", "TRUE" = "orange")
+)
 
 ## Reproducibility information
-print('Reproducibility information:')
+print("Reproducibility information:")
 Sys.time()
 proc.time()
 options(width = 120)
