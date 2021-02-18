bf8d5fcca4ebdeb34e0b4015914f8aee6607a093
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-02-17 21:54:57 -0500

    Get the spatialLIBD::run_app() working ^^ Close #7 #8. We still have to make some edits on spatialLIBD but for now it works ^^

--- analysis/03_try_shiny.R
+++ analysis/03_try_shiny.R
@@ -1,6 +1,5 @@
 ## Automatically style the code in this script:
-styler::style_file(
-    here::here("analysis", "03_try_shiny.R"),
+styler::style_file(here::here("analysis", "03_try_shiny.R"),
     transformers = biocthis::bioc_style()
 )
 
@@ -21,16 +20,34 @@ library("spatialLIBD")
 ## Load SPE data
 load(here::here("rdata", "spe", "spe.Rdata"), verbose = TRUE)
 
+## Make it easy to copy over the data for the images in the format expected by
+## spatialLIBD
+dir.create(here::here("outputs", "images_spatialLIBD"), showWarnings = FALSE)
+sapply(imgData(spe)$sample_id, function(x) {
+    dir.create(here::here("outputs", "images_spatialLIBD", x), showWarnings = FALSE)
+    new_image <-  here::here("outputs", "images_spatialLIBD", x, "tissue_lowres_image.png")
+    file.copy(imgPath(spe, sample_id = x), new_image)
+    new_image
+})
+
 ## Add random layers cluster
+set.seed(20210217)
 spe$layer_guess_reordered_short <-
     sample(c(paste0("L", 1:6), "WM"), size = ncol(spe), replace = TRUE)
 
 ## Try to run shiny
-run_app(spe,
-    spe_discrete_vars = "overlaps_tissue"
+run_app(
+    spe,
+    spe_discrete_vars = "overlaps_tissue",
+    spe_continuous_vars = c(
+        "sum_umi",
+        "sum_gene",
+        "expr_chrM",
+        "expr_chrM_ratio"
+    ),
+    #image_path = "/Users/lcollado/Dropbox/Code/spatialDLPFC/outputs/images_spatialLIBD"
+    image_path = here::here("outputs", "images_spatialLIBD")
 )
-# Error in check_image_path(image_path, spe) :
-#   all(file.exists(file.path(image_path, unique(spe$sample_id),  .... is not TRUE
 
 ## Current R info:
 # SpatialExperiment      * 1.1.432    2021-02-17 [1] Github (drighelli/SpatialExperiment@8407ee8)
