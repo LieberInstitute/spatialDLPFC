6ae0dad50bc70aeecdc344de4a1f583652585dff
Nick-Eagles <nick.eagles@libd.org>
2021-05-04 11:08:30 -0400

    Deleted obsolete 'local_tangram_testing' folder

--- local_tangram_testing/prepare_example_data.sh
+++
@@ -1,8 +0,0 @@
-#  Download and unzip data required for the newer tutorial:
-#  https://github.com/broadinstitute/Tangram/blob/master/example/1_tutorial_tangram.ipynb
-mkdir data/
-wget https://storage.googleapis.com/tommaso-brain-data/tangram_demo/mop_sn_tutorial.h5ad.gz -O data/mop_sn_tutorial.h5ad.gz
-wget https://storage.googleapis.com/tommaso-brain-data/tangram_demo/slideseq_MOp_1217.h5ad.gz -O data/slideseq_MOp_1217.h5ad.gz
-wget https://storage.googleapis.com/tommaso-brain-data/tangram_demo/MOp_markers.csv -O data/MOp_markers.csv
-gunzip -f data/mop_sn_tutorial.h5ad.gz
-gunzip -f data/slideseq_MOp_1217.h5ad.gz
