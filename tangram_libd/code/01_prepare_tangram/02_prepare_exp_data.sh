5159dc7f49ecde3222a6525882be26fafe89a428
Arta <arta.seyedian@gmail.com>
2021-06-03 14:16:04 -0400

    fixed paths of input data prep script

--- tangram_libd/code/01_prepare_tangram/02_prepare_exp_data.sh
+++ tangram_libd/code/01_prepare_tangram/02_prepare_exp_data.sh
@@ -2,10 +2,10 @@
 
 #  Download and unzip data required for the newer tutorial:
 #  https://github.com/broadinstitute/Tangram/blob/master/example/1_tutorial_tangram.ipynb
-mkdir data/
-mkdir logs/
 
-cp /dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/spatialDLPFC/analysis/sce_combined.rda data/sce_combined.rda
-cp /dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/forAmazonS3/SCE_DLPFC_tran-etal.rda data/SCE_DLPFC_tran-etal.rda
+mkdir -p ../../raw-data/01_prepare_tangram/
+
+cp /dcl02/lieber/ajaffe/SpatialTranscriptomics/LIBD/spatialDLPFC/analysis/sce_combined.rda ../../raw-data/01_prepare_tangram/sce_combined.rda
+cp /dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/forAmazonS3/SCE_DLPFC_tran-etal.rda ../../raw-data/01_prepare_tangram/SCE_DLPFC_tran-etal.rda
 
 exit 0
