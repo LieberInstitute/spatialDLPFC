0a84df1873aed6c4885405f5c4ace128f7f38c6d
Nick-Eagles <nick.eagles@libd.org>
2021-06-17 15:09:23 -0400

    Add an initial script (needs testing and possible adjustment of number of marker genes) to select marker genes for the 12 NN DLPFC spatial samples

---
+++ tangram_libd/code/03_nn_run/01_sce_to_anndata.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=50G,h_vmem=50G,h_fsize=100G
+#$ -N "sce-to-anndata"
+#$ -j y
+#$ -o ../../processed-data/03_nn_run/sce-2-anndata.log
+#$ -e ../../processed-data/03_nn_run/sce-2-anndata.log
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load dependencies
+module load conda_R/4.0.x
+
+## List current modules
+module list
+
+Rscript code/03_nn_run/01_sce_to_anndata.R
+
+echo "**** Job ends ****"
+date
