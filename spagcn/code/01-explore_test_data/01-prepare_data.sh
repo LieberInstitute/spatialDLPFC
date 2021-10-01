2f98b266286edfbec3c18e24ecddb59164d904d1
Nick-Eagles <nick.eagles@libd.org>
2021-10-01 17:04:17 -0400

    Update test-data-download instructions after seeing there were two separate test directories

--- spagcn/code/01-explore_test_data/01-prepare_data.sh
+++ spagcn/code/01-explore_test_data/01-prepare_data.sh
@@ -6,8 +6,9 @@ base_dir=$(git rev-parse --show-toplevel)
 
 #  Manually download test data (done here on 9/22/21) from:
 #  https://drive.google.com/drive/folders/1zten54vkjorp26T4iD0ApQGa9ut5eY42?usp=sharing
+#  Download both folders, each of which are downloaded as zip files.
 
-#  Upload the zip file to 'spagcn/raw-data/01-explore_test_data/'
+#  Upload each zip file to 'spagcn/raw-data/01-explore_test_data/'
 
 #  Ignore the test data, which is too large to commit and push
 echo "spagcn/raw-data/01-explore_test_data" >> $base_dir/.gitignore
