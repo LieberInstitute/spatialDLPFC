a2248ad38316895b22a7b625c318b06397a2c106
Nick-Eagles <nick.eagles@libd.org>
2021-10-22 11:37:16 -0400

    Debug file naming and other small issues to get our analysis to run on the first sample in the array

--- spagcn/code/03-our_data_analysis/01_run_spagcn.py
+++ spagcn/code/03-our_data_analysis/01_run_spagcn.py
@@ -1,4 +1,4 @@
-import os,csv,re
+import os,sys,csv,re
 import pandas as pd
 import numpy as np
 import scanpy as sc
--- spagcn/code/03-our_data_analysis/01_run_spagcn.sh
+++ spagcn/code/03-our_data_analysis/01_run_spagcn.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #$ -cwd
-#$ -N "main_analysis_03"
-#$ -o ../../processed-data/02-our_data/03_main_analysis_$TASK_ID.log
-#$ -e ../../processed-data/02-our_data/03_main_analysis_$TASK_ID.log
+#$ -N "run_spagcn_01"
+#$ -o ../../processed-data/03-our_data_analysis/01_run_spagcn_$TASK_ID.log
+#$ -e ../../processed-data/03-our_data_analysis/01_run_spagcn_$TASK_ID.log
 #$ -l mf=20G,h_vmem=20G
-#$ -t 1-12
+#$ -t 1
 #$ -tc 1
 
 module load spagcn/1.2.0
-python 03_main_analysis.py -i $SGE_TASK_ID
+python 01_run_spagcn.py -i $SGE_TASK_ID
