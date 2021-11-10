4f0a4ba8ed112814c091d1be9a05fb886db02ab6
Nick-Eagles <nick.eagles@libd.org>
2021-11-10 13:08:12 -0500

    Require more memory for spaGCN

--- spagcn/code/03-our_data_analysis/01_run_spagcn.sh
+++ spagcn/code/03-our_data_analysis/01_run_spagcn.sh
@@ -3,8 +3,8 @@
 #$ -N "run_spagcn_01"
 #$ -o ../../processed-data/03-our_data_analysis/01_run_spagcn_$TASK_ID.log
 #$ -e ../../processed-data/03-our_data_analysis/01_run_spagcn_$TASK_ID.log
-#$ -l mf=20G,h_vmem=20G
-#$ -t 1
+#$ -l mf=60G,h_vmem=60G
+#$ -t 3
 #$ -tc 1
 
 module load spagcn/1.2.0
