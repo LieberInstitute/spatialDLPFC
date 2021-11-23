b46aebea4ad64f164603659aeafad5e1908d2a64
abspangler13 <aspangle@gmail.com>
2021-11-23 14:44:07 -0500

    adding code to use wrapper function and merge results from other spe's

---
+++ code/analysis/merge_spe.R
@@ -0,0 +1,91 @@
+## This script requires R 4.1
+# module load conda_R/4.1.x
+
+## utils
+library("here")
+library("sessioninfo")
+
+## reading the data
+library("SpatialExperiment")
+
+## vis
+library("spatialLIBD")
+
+
+## Define some info for the samples
+sample_info <- data.frame(
+  sample_id = c(
+    "DLPFC_Br2743_ant_manual_alignment",
+    "DLPFC_Br2743_mid_manual_alignment_extra_reads",
+    "DLPFC_Br2743_post_manual_alignment",
+    "DLPFC_Br3942_ant_manual_alignment",
+    "DLPFC_Br3942_mid_manual_alignment",
+    "DLPFC_Br3942_post_manual_alignment",
+    "DLPFC_Br6423_ant_manual_alignment_extra_reads",
+    "DLPFC_Br6423_mid_manual_alignment",
+    "DLPFC_Br6423_post_extra_reads",
+    "DLPFC_Br8492_ant_manual_alignment",
+    "DLPFC_Br8492_mid_manual_alignment_extra_reads",
+    "DLPFC_Br8492_post_manual_alignment",
+    "Round2/DLPFC_Br2720_ant_manual_alignment",
+    "Round2/DLPFC_Br2720_mid_manual_alignment",
+    "Round2/DLPFC_Br2720_post_extra_reads",
+    "Round2/DLPFC_Br6432_ant_manual_alignment",
+    "Round2/DLPFC_Br6432_mid_manual_alignment",
+    "Round2/DLPFC_Br6432_post_manual_alignment",
+    "Round3/DLPFC_Br6471_ant_manual_alignment_all",
+    "Round3/DLPFC_Br6471_mid_manual_alignment_all",
+    "Round3/DLPFC_Br6471_post_manual_alignment_all",
+    "Round3/DLPFC_Br6522_ant_manual_alignment_all",
+    "Round3/DLPFC_Br6522_mid_manual_alignment_all",
+    "Round3/DLPFC_Br6522_post_manual_alignment_all",
+    "Round3/DLPFC_Br8325_ant_manual_alignment_all",
+    "Round3/DLPFC_Br8325_mid_manual_alignment_all",
+    "Round3/DLPFC_Br8325_post_manual_alignment_all",
+    "Round3/DLPFC_Br8667_ant_extra_reads",
+    "Round3/DLPFC_Br8667_mid_manual_alignment_all",
+    "Round3/DLPFC_Br8667_post_manual_alignment_all",
+    "Round4/DLPFC_Br2720_ant_2",
+    "Round4/DLPFC_Br6432_ant_2",
+    "Round4/DLPFC_Br8325_mid_2"
+  ),
+  subjects = c(rep(
+    c("Br2743", "Br3942", "Br6423", "Br8492", "Br2720","Br6432","Br6471","Br6522","Br8325","Br8667"),
+    each = 3
+  ),"Br2720","Br6432","Br8325"),
+  regions = c(rep(
+    c("anterior", "middle", "posterior"),
+    10
+  ),"anterior","anterior","middle"),
+  sex = c(rep(
+    c("M", "M", "M", "F","F","M","M","M","F","F"),
+    each = 3
+  ),"F","M","F"),
+  age = c(rep(
+    c(61.54, 47.53, 51.73, 53.40,48.22,48.88,55.46,33.39,57.62,37.33),
+    each = 3
+  ),48.22,48.88,57.62),
+  diagnosis = "Control"
+)
+sample_info$sample_path <- file.path(
+  here::here("processed-data", "NextSeq"),
+  sample_info$sample_id,
+  "outs"
+)
+stopifnot(all(file.exists(sample_info$sample_path)))
+
+#clean up sample_id
+sample_info$sample_id <- gsub("_all|_extra_reads|DLPFC_|_manual_alignment", "", basename(sample_info$sample_id))
+
+## Build SPE object
+Sys.time()
+spe_wrapper <- read10xVisiumWrapper(
+  sample_info$sample_path,
+  sample_info$sample_id,
+  type = "sparse",
+  data = "raw",
+  images = c("lowres", "hires", "detected", "aligned"),
+  load = TRUE,
+  reference_gtf = "/dcl02/lieber/ajaffe/SpatialTranscriptomics/refdata-gex-GRCh38-2020-A/genes/genes.gtf"
+)
+Sys.time()
