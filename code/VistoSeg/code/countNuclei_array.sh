91bdf5f1b43d3e613edca414eb6fc0635a1fe2a4
lcolladotor <lcollado@jhsph.edu>
2021-12-14 14:52:30 -0500

    Complete array job script for re-running countNuclei
    Missing sample 28 which is the one from #40
    
    Co-authored-by: Heena Divecha <heenadivecha@gmail.com>
    Co-authored-by: Abby Spangler <aspangle@gmail.com>

--- code/VistoSeg/code/countNuclei_array.sh
+++ code/VistoSeg/code/countNuclei_array.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=25G,h_vmem=25G,h_fsize=100G
+#$ -l mem_free=5G,h_vmem=5G,h_fsize=100G
 #$ -pe local 4
-#$ -o Logs/DLPFC_Br3942_ant_manual_alignment.txt
-#$ -e Logs/DLPFC_Br3942_ant_manual_alignment.txt
+#$ -N spatialDLPFC_rerun_countNuclei
+#$ -o Logs/rerun_countNuclei.$TASK_ID.txt
+#$ -e Logs/rerun_countNuclei.$TASK_ID.txt
 #$ -m e
-#$ -M heenadivecha@gmail.com
-
+#$ -t 1-27,29-30
+#$ -tc 30
 
 echo "**** Job starts ****"
 date
-
 
 echo "**** JHPCE info ****"
 echo "User: ${USER}"
@@ -19,18 +19,26 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: DLPFC_Br3942_ant_manual_alignment"
-echo "****"
 
 module load matlab/R2019a
 
-toolbox='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/code/VistoSeg/code'
-mask='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/raw-data/Images/round1/Liebert_Institute_OTS-20-7690_rush_anterior_2_nuclei.mat'
-jsonname='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rerun_spaceranger/DLPFC_Br3942_ant_manual_alignment/outs/spatial/scalefactors_json.json'
-posname='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rerun_spaceranger/DLPFC_Br3942_ant_manual_alignment/outs/spatial/tissue_positions_list.csv'
+## Read parameters
+SAMPLE=$(awk 'BEGIN {FS="\t"} {print $1}' ../../spaceranger/spaceranger_parameters.txt | awk "NR==${SGE_TASK_ID}")
+IMAGEPATH=$(awk 'BEGIN {FS="\t"} {print $4}' ../../spaceranger/spaceranger_parameters.txt | awk "NR==${SGE_TASK_ID}")
+MASKPATH=$(echo ${IMAGEPATH} | sed "s/.tif/_nuclei.mat/g")
+
+echo "Processing sample ${SAMPLE} with mask ${MASKPATH}"
+date
+
+toolbox="/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/code/VistoSeg/code"
+jsonname="/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rerun_spaceranger/${SAMPLE}/outs/spatial/scalefactors_json.json"
+posname="/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rerun_spaceranger/${SAMPLE}/outs/spatial/tissue_positions_list.csv"
 
 
-matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), countNuclei('$mask','$jsonname','$posname')"
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), countNuclei('${MASKPATH}','$jsonname','$posname')"
 
 echo "**** Job ends ****"
 date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
