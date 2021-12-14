2e544190378464b9e1eb3a842bbbba8f610e0045
lcolladotor <lcollado@jhsph.edu>
2021-12-14 14:33:06 -0500

    Finished the 2 problematic samples from #48
    Co-authored-by: Heena Divecha <heenadivecha@gmail.com>

--- code/VistoSeg/code/countNuclei.sh
+++ code/VistoSeg/code/countNuclei.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -cwd
 #$ -l mem_free=25G,h_vmem=25G,h_fsize=100G
-#$ -pe local 9
-#$ -o Logs/V10B01-002_D1_Br2720_ant_DLPFC.txt
-#$ -e Logs/V10B01-002_D1_Br2720_ant_DLPFC.txt
+#$ -pe local 4
+#$ -o Logs/DLPFC_Br3942_ant_manual_alignment.txt
+#$ -e Logs/DLPFC_Br3942_ant_manual_alignment.txt
 #$ -m e
 #$ -M heenadivecha@gmail.com
 
@@ -19,15 +19,15 @@ echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 echo "****"
-echo "Sample id: V10B01-002_D1_Br2720_ant_DLPFC"
+echo "Sample id: DLPFC_Br3942_ant_manual_alignment"
 echo "****"
 
 module load matlab/R2019a
 
 toolbox='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/code/VistoSeg/code'
-mask='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/raw-data/Images/round4/V10B01-002_D1_Br2720_ant_DLPFC_nuclei.mat'
-jsonname='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/NextSeq/Round4/DLPFC_Br2720_ant_2/outs/spatial/scalefactors_json.json'
-posname='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/NextSeq/Round4/DLPFC_Br2720_ant_2/outs/spatial/tissue_positions_list.csv'
+mask='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/raw-data/Images/round1/Liebert_Institute_OTS-20-7690_rush_anterior_2_nuclei.mat'
+jsonname='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rerun_spaceranger/DLPFC_Br3942_ant_manual_alignment/outs/spatial/scalefactors_json.json'
+posname='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rerun_spaceranger/DLPFC_Br3942_ant_manual_alignment/outs/spatial/tissue_positions_list.csv'
 
 
 matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), countNuclei('$mask','$jsonname','$posname')"
