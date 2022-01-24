9c613311e21c8a192cd5ff280e24f4cca5e0c4e7
Nick-Eagles <nick.eagles@libd.org>
2022-01-24 17:02:41 -0500

    Test just the first sample at first

--- spagcn/code/03-our_data_analysis/01_run_spagcn.sh
+++ spagcn/code/03-our_data_analysis/01_run_spagcn.sh
@@ -4,7 +4,7 @@
 #$ -o ../../processed-data/03-our_data_analysis/01_run_spagcn_$TASK_ID.log
 #$ -e ../../processed-data/03-our_data_analysis/01_run_spagcn_$TASK_ID.log
 #$ -l mf=60G,h_vmem=60G
-#$ -t 3
+#$ -t 1
 #$ -tc 1
 
 module load spagcn/1.2.0
