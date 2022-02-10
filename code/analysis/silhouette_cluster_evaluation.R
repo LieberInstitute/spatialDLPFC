4e0a4589516e7004e4dc917a135b2846242503a9
abspangler13 <aspangle@gmail.com>
2022-02-10 13:02:40 -0500

    adding script to analyze clusters using the silhouette method.

---
+++ code/analysis/silhouette_cluster_evaluation.R
@@ -0,0 +1,26 @@
+# qrsh -l bluejay,mem_free=75G,h_vmem=75G -now n
+
+library(SpatialExperiment)
+library(here)
+
+# Clustering
+library(cluster) 
+library(factoextra)
+
+# adapted from: https://medium.com/codesmart/r-series-k-means-clustering-silhouette-794774b46586
+# silhouette_score <- function(k){
+#   km <- kmeans(df, centers = k, nstart=25)
+#   ss <- silhouette(km$cluster, dist(df))
+#   mean(ss[, 3])
+# }
+# k <- 2:10
+# avg_sil <- sapply(k, silhouette_score)
+# plot(k, type='b', avg_sil, xlab='Number of clusters', ylab='Average Silhouette Scores', frame=FALSE)
+
+
+# load SPE object 
+load("/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/spe_final.Rdata")
+
+#import all BayesSpace cluster
+
+ss <- silhouette(spe$spatial.cluster, dist(reducedDims(spe)$HARMONY))
