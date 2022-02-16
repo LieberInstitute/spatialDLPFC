a3bdd1cde573d218c2b7766648f72d154cc38adf
abspangler13 <aspangle@gmail.com>
2022-02-16 13:33:33 -0500

    finished R and .sh scripts for running fasthplus on bayesSpace results for all values of k. Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>

--- code/analysis/fasthplus.R
+++ code/analysis/fasthplus.R
@@ -6,11 +6,34 @@
 library(fasthplus)
 library(SpatialExperiment)
 library(here)
+library("sessioninfo")
+library(spatialLIBD)
+
+k <- as.numeric(Sys.getenv("SGE_TASK_ID"))
 
 #load spe object
-load(file = here::here("processed-data","rdata","spe","spe_final.Rdata"))
+load(file = here::here("processed-data","rdata","spe","spe_final.Rdata"),verbose = TRUE)
+
+spe <- cluster_import(
+  spe,
+  cluster_dir = here::here("processed-data", "rdata", "spe", "clustering_results","bayesSpace_k"),
+  prefix = ""
+)
 
 #hpb estimate. t = pre-bootstrap sample size, D = reduced dimensions matrix, L = cluster labels, r = number of bootstrap iterations
-hpb(D= reducedDims(spe)$HARMONY,L=as.vector(spe$spatial.cluster),t=10,r=10)
+set.seed(20220216)
+fasthplus <- hpb(D= reducedDims(spe)$HARMONY,L=colData(spe)[[paste0("bayesSpace_harmony_",k)]],t=1446,r=30)
+results <- data.frame (k=k, fasthplus=fasthplus)
+write.csv(results,file = here::here("processed-data","rdata","spe","fasthplus_resuts.csv"), append = TRUE)
 
-## error is line 30 of bsf which is called by hpb() in line 49
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
+
+
+
+
+
