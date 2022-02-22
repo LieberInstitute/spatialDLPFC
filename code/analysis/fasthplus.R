30c6b577f1310bc66f6c2fb3c2b617e193161162
abspangler13 <aspangle@gmail.com>
2022-02-21 22:00:08 -0500

    added code to plot fasthplus resutsl. version controlling log files

--- code/analysis/fasthplus.R
+++ code/analysis/fasthplus.R
@@ -34,6 +34,16 @@ options(width = 120)
 session_info()
 
 
+####Plot results
 
+#load data
+library(ggplot2)
+df <- read.csv(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/fasthplus_resuts.csv", header = TRUE)
+
+pdf(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/plots/fasthplus.pdf")
+ggplot(data=df, aes(x=k, y=X1.h, group=1)) +
+  geom_line()+
+  geom_point()
+dev.off()
 
 
