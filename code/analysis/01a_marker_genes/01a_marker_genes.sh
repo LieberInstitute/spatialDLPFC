0ad61983a25303c77c8aac2c8a4fa4577e31fdb0
abspangler13 <aspangle@gmail.com>
2022-03-04 15:39:42 -0500

    added code to plot human marker genes

---
+++ code/analysis/01a_marker_genes/01a_marker_genes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=40G,h_vmem=40G,h_fsize=100G
+#$ -N marker_genes
+#$ -o logs/marker_genes.txt
+#$ -e logs/marker_genes.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/devel
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 01a_marker_genes.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
