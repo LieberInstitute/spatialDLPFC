974e02c0599580d9eb8d70cdc891977eaf735793
abspangler13 <aspangle@gmail.com>
2022-03-05 12:27:09 -0500

    changed bash scripts to run on caracol

--- code/analysis/02_graph_based_clustering/02_graph_based_clustering.sh
+++ code/analysis/02_graph_based_clustering/02_graph_based_clustering.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=100G,h_vmem=100G,h_fsize=100G
+#$ -l caracol,mem_free=100G,h_vmem=100G,h_fsize=100G
 #$ -N graph_based_clustering
 #$ -o logs/graph_based.txt
 #$ -e logs/graph_based.txt
 #$ -m e
+
+USAGE_CUTOFF=10
+NUM_GPUS=1
+
+avail_gpus=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | cut -d " " -f 1 | awk -v usage="$USAGE_CUTOFF" '$1 < usage {print NR - 1}')
+
+#  Simply exit with an error if there are no GPUs left
+if [[ -z $avail_gpus ]]; then
+    echo "No GPUs are available."
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=$(echo "$avail_gpus" | head -n $NUM_GPUS | paste -sd ",")
+
 
 echo "**** Job starts ****"
 date
