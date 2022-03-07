5a44e3698fc7dc9f47e709504331ca1b836e5988
abspangler13 <aspangle@gmail.com>
2022-03-07 15:02:17 -0500

    modified R script to remove white matter using bayesSpace  k = 2 to identify white matter. Modified bash script to run fasthplus from k = 3 to k = 15 since k = 2 is no longer relevant after you removed white matter. Made plotting code to plot 1-h for original  data, after bad spots were removed, and after white matter was removed.

--- code/analysis/06_fastplus/06_fasthplus.sh
+++ code/analysis/06_fastplus/06_fasthplus.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 #$ -cwd
-#$ -l bluejay,mem_free=20G,h_vmem=20G,h_fsize=100G
+#$ -l caracol,mem_free=20G,h_vmem=20G,h_fsize=100G
 #$ -N fasthplus
 #$ -o logs/fasthplus.$TASK_ID.txt
 #$ -e logs/fasthplus.$TASK_ID.txt
 #$ -m e
-#$ -t 2-15
-#$ -tc 20
+#$ -t 3-15
+#$ -tc 4
+
+USAGE_CUTOFF=10
+NUM_GPUS=1
+
+avail_gpus=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | cut -d " " -f 1 | awk -v usage="$USAGE_CUTOFF" '$1 < usage {print NR - 1}')
+
+#  Simply exit with an error if there are no GPUs left
+if [[ -z $avail_gpus ]]; then
+    echo "No GPUs are available."
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=$(echo "$avail_gpus" | head -n $NUM_GPUS | paste -sd ",")
+
 
 echo "**** Job starts ****"
 date
--- code/analysis/06_fastplus/plot_fasthplus.R
+++ code/analysis/06_fastplus/plot_fasthplus.R
@@ -1,10 +1,29 @@
 
 library(ggplot2)
 
-#load data
+#load original results
 df <- read.csv(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/fasthplus_resuts.csv", header = TRUE)
 
-pdf(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/plots/fasthplus.pdf")
+pdf(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/plots/06_fasthplus/fasthplus_original.pdf")
+ggplot(data=df, aes(x=k, y=X1.h, group=1)) +
+  geom_line()+
+  geom_point()
+dev.off()
+
+
+#load data after removed bad spots
+df <- read.csv(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/06_fasthplus/fasthplus_results.csv", header = TRUE)
+
+pdf(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/plots/06_fasthplus/fasthplus.pdf")
+ggplot(data=df, aes(x=k, y=X1.h, group=1)) +
+  geom_line()+
+  geom_point()
+dev.off()
+
+#load data after removed whtie matter
+df <- read.csv(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/06_fasthplus/fasthplus_results_no_WM.csv", header = TRUE)
+
+pdf(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/plots/06_fasthplus/fasthplus_no_WM.pdf")
 ggplot(data=df, aes(x=k, y=X1.h, group=1)) +
   geom_line()+
   geom_point()
