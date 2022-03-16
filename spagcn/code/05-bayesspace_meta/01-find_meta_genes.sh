da9797c7449a1bbc5361e6816b8ff2166fc0f9fc
Nick-Eagles <nick.eagles@libd.org>
2022-03-16 15:11:47 -0400

    Add a shell script to submit the python job to find meta genes on BayesSpace-called clusters

---
+++ spagcn/code/05-bayesspace_meta/01-find_meta_genes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -cwd
+#$ -N "find_meta_genes_01"
+#$ -o ../../processed-data/05-bayesspace_meta/01-find_meta_genes_$TASK_ID.log
+#$ -e ../../processed-data/05-bayesspace_meta/01-find_meta_genes_$TASK_ID.log
+#$ -l bluejay,mf=10G,h_vmem=10G
+#$ -t 1
+#$ -tc 1
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+#   The script internally reads the 'SGE_TASK_ID' environment variable
+module load spagcn/1.2.0
+python 01-find_meta_genes.py
+
+echo "**** Job ends ****"
+date
