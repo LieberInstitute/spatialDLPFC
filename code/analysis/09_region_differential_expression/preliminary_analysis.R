63b33377df52dd0273203ac23b6724c895d88c76
abspangler13 <aspangle@gmail.com>
2022-04-06 22:34:22 -0400

    finished adding code to plot explanatory variables. resolves #62

--- code/analysis/09_region_differential_expression/preliminary_analysis.R
+++ code/analysis/09_region_differential_expression/preliminary_analysis.R
@@ -48,10 +48,31 @@ reducedDims(spe_pseudo) <- list(PCA=pca_pseudo, test=pca_pseudo) #need to figure
 
 
 ###plot PCA###
-pdf(file = here::here("plots","09_region_differential_expression",paste0("sce_pseudobulk_pca_k",k,".Rdata")))
-plotPCA(spe_pseudo, colour_by = "region", ncomponents = 8)
+pdf(file = here::here("plots","09_region_differential_expression",paste0("sce_pseudobulk_pca_k",k,".pdf")))
+plotPCA(spe_pseudo, colour_by = "region", ncomponents = 12)
 dev.off()
 
+####plot explanatory variables ####
+
+# pdf(file = here::here("plots","09_region_differential_expression","test_plot_high_genes.pdf"))
+# plotHighestExprs(spe_pseudo, exprs_values = "counts")
+# dev.off()
+
+vars <- getVarianceExplained(spe_pseudo, 
+                             variables=c("subject", "region", "sex", "age"))
+head(vars)
+
+# subject    region       sex         age
+# ENSG00000243485  6.149209 0.8049553 0.9891498 0.684494271
+# ENSG00000238009 22.059419 0.3825081 6.0221979 0.758922808
+# ENSG00000239945 10.112360 2.2471910 0.7490637 0.006556772
+# ENSG00000241860 15.910843 1.7697885 2.7770465 3.317220956
+# ENSG00000229905 10.113068 0.5911937 1.0110245 0.266643458
+# ENSG00000237491 17.499031 9.9500577 0.2762791 0.570564318
+
+pdf(file = here::here("plots","09_region_differential_expression",paste0("plot_explanatory_vars_k",k,".pdf")))
+plotExplanatoryVariables(vars)
+dev.off()
 
 ########installations###########
 # if (!require("BiocManager", quietly = TRUE))
