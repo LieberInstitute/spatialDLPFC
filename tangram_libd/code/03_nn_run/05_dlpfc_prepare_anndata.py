49c57df1fae16a300fd3530f3e122e56071a9450
Nick-Eagles <nick.eagles@libd.org>
2022-04-26 14:07:01 -0400

    Isolate the AnnData-preprocessing step from the original large tangram script

---
+++ tangram_libd/code/03_nn_run/05_dlpfc_prepare_anndata.py
@@ -0,0 +1,184 @@
+#   While 03_sce_to_anndata_dlpfc.R ideally would produce an AnnData totally
+#   ready for use with Tangram and squidpy, in reality there are a few details
+#   to adjust. This code is separate from 03_sce_to_anndata_dlpfc.R because it
+#   is simpler to perform in python than R.
+
+import os, sys
+import pyhere
+from pathlib import Path
+
+import scanpy as sc
+import numpy as np
+import pandas as pd
+from anndata import AnnData
+import skimage
+import seaborn as sns
+import tangram as tg
+from PIL import Image
+import json
+
+################################################################################
+#   Variable definitions
+################################################################################
+
+#-------------------------------------------------------------------------------
+#   Paths
+#-------------------------------------------------------------------------------
+
+plot_dir = pyhere.here('tangram_libd', 'plots', '03_nn_run', 'DLPFC')
+processed_dir = pyhere.here(
+    'tangram_libd', 'processed-data', '03_nn_run', 'tangram_out_DLPFC'
+)
+Path(plot_dir).mkdir(parents=True, exist_ok=True)
+Path(processed_dir).mkdir(parents=True, exist_ok=True)
+
+sc_path = pyhere.here(
+    'tangram_libd', 'processed-data', '03_nn_run', 'sce_DLPFC.h5ad'
+)
+sp_path = pyhere.here(
+    'tangram_libd', 'processed-data', '03_nn_run', 'visium_DLPFC.h5ad'
+)
+marker_path = pyhere.here(
+    'tangram_libd', 'processed-data', '03_nn_run', 'pan_markers.txt'
+)
+sample_path = pyhere.here(
+    'tangram_libd', 'processed-data', '03_nn_run', 'brain_samples.txt'
+)
+
+#   Parent directory whose subdirectories include JSON files with spot sizes
+#   for each sample
+json_dir = '/dcl02/lieber/ajaffe/SpatialTranscriptomics/HumanPilot/10X'
+
+#-------------------------------------------------------------------------------
+#   Dataset-specific variables
+#-------------------------------------------------------------------------------
+
+#  Genes we want to plot predicted vs. actual expression for
+select_genes_names = [
+    'SNAP25', 'MBP', 'PCP4', 'CCK', 'RORB', 'ENC1', 'CARTPT', 'NR4A2', 'RELN'
+]
+
+spatial_coords_names = ('pxl_row_in_fullres', 'pxl_col_in_fullres')
+
+#   "full" or "hi". Hires is used for speedy testing, but fullres is more
+#   appropriate for the actual analysis
+resolution = 'hi'
+
+################################################################################
+#   Preprocessing
+################################################################################
+
+#  Grab the full list of sample names we will subset from
+with open(sample_path, 'r') as f:
+    sample_names = f.read().splitlines()
+
+#  Determine this particular sample name
+sample_name = sample_names[int(os.environ['SGE_TASK_ID']) - 1]
+print('Subsetting to just sample {}.'.format(sample_name))
+
+#  Load AnnDatas and list of marker genes
+print('Loading AnnDatas...')
+ad_sp = sc.read_h5ad(sp_path)
+ad_sp = ad_sp[ad_sp.obs['sample_id'] == sample_name, :]
+ad_sc = sc.read_h5ad(sc_path)
+
+with open(marker_path, 'r') as f:
+    markers = f.read().splitlines()
+
+#  Use Ensembl IDs as gene names for both AnnDatas
+ad_sc.var.index = ad_sc.var.gene_id
+    
+#  Note when genes of interest are present in the training set  
+select_genes = ad_sp.var.gene_id[ad_sp.var.gene_name.isin(select_genes_names)]
+assert len(select_genes_names) == len(select_genes)
+
+for i in range(len(select_genes)):
+    gene = select_genes[i]
+    gene_name = select_genes_names[i]
+    
+    #  Verify genes of interest were measured in the experiment
+    assert gene in ad_sp.var.gene_id.values
+    assert gene in ad_sc.var.gene_id.values
+    
+    if gene in markers:
+        print('Gene', gene, '(' + gene_name + ') is in the training set.')
+    else:
+        print('Gene', gene, '(' + gene_name + ') is in the test set.')
+
+tg.pp_adatas(ad_sc, ad_sp, genes=markers)
+
+
+#   Path to JSON (from spaceranger?) including spot size for this sample
+json_path = json_dir + '/' + sample_name + '/scalefactors_json.json'
+
+with open(json_path) as f: 
+    json_data = json.load(f)
+
+#   Store scalefactors in AnnData as squidpy expects
+ad_sp.uns['spatial'] = {
+    sample_name: {
+        'scalefactors': json_data
+    }
+}
+
+#   Read in image and attach to AnnData object
+if resolution == 'full':
+    SPOT_SIZE = json_data["spot_diameter_fullres"]
+    SCALE_FACTOR = 1
+
+    #   Read in full-resolution histology image
+    img_path = str(
+        pyhere.here(
+            "spagcn/raw-data/02-our_data_tutorial/" + sample_name + ".tif"
+        )
+    )
+
+    #   Read histology image in as numpy array
+    img_arr = np.array(Image.open(img_path))
+    assert img_arr.shape == (13332, 13332, 3), img_arr.shape
+elif resolution == 'hi':
+    SCALE_FACTOR = ad_sp.uns['spatial'][sample_name]['scalefactors']['tissue_hires_scalef']
+    SPOT_SIZE = json_data["spot_diameter_fullres"] * SCALE_FACTOR
+    ad_sp.obs[spatial_coords_names[0]] *= SCALE_FACTOR
+    ad_sp.obs[spatial_coords_names[1]] *= SCALE_FACTOR
+
+    #   Read in high-resolution histology image
+    img_path = str(
+        pyhere.here(
+            "tangram_libd/raw-data/03_nn_run/hires_histology", sample_name + ".png"
+        )
+    )
+
+    #   Read histology image in as numpy array
+    img_arr = np.array(Image.open(img_path))
+    assert img_arr.shape == (2000, 2000, 3), img_arr.shape
+else:
+    print('Resolution', resolution, 'not supported.')
+    sys.exit(1)
+
+ad_sp.uns['spatial'][sample_name]['images'] = {
+    resolution + 'res': img_arr  # / 256 ?
+}
+
+#   Squidpy expects spatial coords in a very specific format
+ad_sp.obsm['spatial'] = np.array(
+    list(
+        zip(
+            ad_sp.obs[spatial_coords_names[0]],
+            ad_sp.obs[spatial_coords_names[1]]
+        )
+    )
+)
+
+#-------------------------------------------------------------------------------
+#   Save AnnDatas
+#-------------------------------------------------------------------------------
+
+ad_sp.write_h5ad(
+    os.path.join(
+        out_dir, 'ad_sp_orig_{}_{}res.h5ad'.format(sample_name, resolution)
+    )
+)
+
+if int(os.environ['SGE_TASK_ID']) == 1:
+    ad_sc.write_h5ad(os.path.join(out_dir, 'ad_sc.h5ad'))
