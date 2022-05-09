dd5456aa9ea0974028c0dcaa9307aa17b7f361fd
Heena Divecha <heenadivecha@eb-li-612uml85.win.ad.jhu.edu>
2022-05-09 10:32:52 -0400

    Copied and edited the files from https://github.com/LieberInstitute/spatial_DG_lifespan/tree/main/code/03_shinyapp

---
+++ code/analysis_IF/02_shinyapp/app.R
@@ -0,0 +1,25 @@
+library("spatialLIBD")
+library("markdown") ## due to a shinyapps.io bug
+
+## spatialLIBD uses golem
+options("golem.app.prod" = TRUE)
+
+## You need this to enable shinyapps to install Bioconductor packages
+options(repos = BiocManager::repositories())
+
+## Load the data (all paths are relative to this script's location)
+spe <- readRDS("spe.rds")
+spe$CellCount <- spe$segmentation_info
+vars <- colnames(colData(spe))
+
+## Deploy the website
+spatialLIBD::run_app(
+    spe,
+    sce_layer = NULL,
+    modeling_results = NULL,
+    sig_genes = NULL,
+    title = "spatialDLPFC_IF",
+    spe_discrete_vars = c(vars[grep("10x_", vars)], "ManualAnnotation"),
+    spe_continuous_vars = c("sum_umi", "sum_gene", "expr_chrM", "expr_chrM_ratio","CellCount"),
+    default_cluster = "10x_graphclust"
+  )
---
+++ code/analysis_IF/02_shinyapp/deploy.R
@@ -0,0 +1,28 @@
+library("rsconnect")
+
+## Locate app_dir. Edit as needed
+app_dir <- here::here("code","03_shinyapp")
+
+## Or you can go to your shinyapps.io account and copy this
+## Here we do this to keep our information hidden.
+load(file.path(app_dir, ".deploy_info.Rdata"), verbose = TRUE)
+
+source(file.path(app_dir,"token.R"))
+
+## You need this to enable shinyapps to install Bioconductor packages
+options(repos = BiocManager::repositories())
+
+## Deploy the app, that is, upload it to shinyapps.io
+## Note that appFiles has to be relative to app_dir.
+## Drop the www directory if you didn't customize the documentation files and
+## edit app.R accordingly.
+rsconnect::deployApp(
+    appDir = app_dir,
+    appFiles = c(
+        "app.R",
+        "spe.rds"
+    ),
+    appName = "lifespanDG_Ramnauth_2022",
+    account = "libd",
+    server = "shinyapps.io"
+)
