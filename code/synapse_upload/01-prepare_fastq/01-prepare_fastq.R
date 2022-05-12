dd311469add5ff7bc1b03f77becdfbceca3713fd
Nick-Eagles <nick.eagles@libd.org>
2022-05-12 17:45:14 -0400

    Complete the script for renaming (making new symlinks) FASTQ files in preparation for the synapse upload and produce a corresponding shell script

--- code/synapse_upload/01-prepare_fastq/01-prepare_fastq.R
+++ code/synapse_upload/01-prepare_fastq/01-prepare_fastq.R
@@ -5,27 +5,70 @@
 #   including the correct sample ID.
 
 library('here')
+library('jaffelab')
+library('sessioninfo')
 
 sr_table_path = here('code', 'spaceranger', 'spaceranger_parameters.txt')
-dest_dir = here('code', 'synapse_upload', 'FASTQ')
+dest_dir = here('raw-data', 'FASTQ_renamed')
+df_out_path = here(
+    'processed-data', 'synapse_upload', '01-prepare_fastq',
+    'fastq_renaming_scheme.csv'
+)
+
+dir.create(dest_dir, showWarnings = FALSE)
 
 sr_table = read.table(sr_table_path)
+
 ids = sr_table[,1]
+fastq_old = lapply(strsplit(sr_table[,6], ','), list.files, full.names = TRUE)
 
-#   For each row (sample), check if:
-#       1. every directory in the 6th column contains the sample ID (in col 1)
-#       2. the FASTQs in those directories contain the sample ID
-#   Trying to check if information about the sample ID is contained in FASTQ
-#   full file paths. I manually found that at least one sample has FASTQ file
-#   paths lacking the needed information
+#   The new naming convention for the FASTQ files to upload to synapse will be:
+#
+#       [sample ID]_[MATE]_[INDEX].fastq.gz
+#
+#   where [INDEX] is assigned randomly. For example, if there are 3 pairs of
+#   FASTQ files for a given sample, one of the pairs will be randomly given the
+#   index 1, and so on for index 2 and 3.
 
-for (i in 1:nrow(sr_table)) {
-    contains_id = FALSE
-    id = sr_table[i, 1]
-    print(paste0('Checking sample ID ', id, ':'))
-    fastq_dirs = strsplit(sr_table[i, 6], ',')[[1]]
-    contains_id = any(grepl(id, basename(fastq_dirs)))
-    if (! contains_id) {
-        print('Did not find ID in FASTQ dirnames!')
+fastq_new = lapply(
+    1:nrow(sr_table),
+    function(i) {
+        num_files = length(fastq_old[[i]])
+        stopifnot(num_files %% 2 == 0) # FASTQ files should come in pairs
+        num_pairs = num_files / 2
+        
+        #   Here we take advantage of how 'list.files' orders files, and the
+        #   existing naming convention for fastq_old.
+        #   Specifically, mates 1 and 2 for a given pair will be adjacent, and
+        #   pairs for lanes 1 and 2 will be adjacent.
+        new_names = paste0(
+            ids[i],
+            rep(c('_R1_', '_R2_'), times = num_pairs),
+            rep(1:num_pairs, each = 2),
+            '.fastq.gz'
+        )
+        
+        return(file.path(dest_dir, new_names))
     }
-}
+)
+
+#   Create a table with columns ID, old filename, new filename. It will be in
+#   "long format" in the sense that an ID will be repeated some multiple of 2
+#   times. Save this table so we have a reference for how the old filenames
+#   relate to the new ones
+
+ids_long = ss(basename(unlist(fastq_new)), '_R[12]_')
+
+file_df = data.frame(
+    'sample_id' = ids_long,
+    'old_path' = unlist(fastq_old),
+    'new_path' = unlist(fastq_new)
+)
+
+write.csv(file_df, file = df_out_path, quote=FALSE, row.names = FALSE)
+
+#   Now symbolically link the old FASTQs to the new destination with the new
+#   naming scheme
+all(file.symlink(unlist(fastq_old), unlist(fastq_new)))
+
+session_info()
---
+++ code/synapse_upload/01-prepare_fastq/01-prepare_fastq.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=3G,h_vmem=3G
+#$ -N "prepare_fastq"
+#$ -o ../../../processed-data/synapse_upload/01-prepare_fastq/01-prepare_fastq.log
+#$ -e ../../../processed-data/synapse_upload/01-prepare_fastq/01-prepare_fastq.log
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+module load conda_R/4.1.x
+Rscript 01-prepare_fastq.R
+
+echo "**** Job ends ****"
+date
