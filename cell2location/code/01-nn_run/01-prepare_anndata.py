e4335372c6d9e868118ca7e900f569e82c4ed04a
Nick-Eagles <nick.eagles@libd.org>
2022-05-13 15:08:22 -0400

    Correct Anndata-preprocessing code after testing

--- cell2location/code/01-nn_run/01-prepare_anndata.py
+++ cell2location/code/01-nn_run/01-prepare_anndata.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 import scanpy as sc
 import anndata
@@ -6,6 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from matplotlib.backends.backend_pdf import PdfPages
 
 import cell2location
 from cell2location.utils.filtering import filter_genes
@@ -17,6 +19,9 @@ rcParams['pdf.fonttype'] = 42 # enables correct plotting of text
 import seaborn as sns
 
 import pyhere
+from pathlib import Path
+from PIL import Image
+import json
 
 ################################################################################
 #   Variable definitions
@@ -30,6 +35,9 @@ sp_path = pyhere.here(
 )
 
 processed_dir = pyhere.here('cell2location', 'processed-data', '01-nn_run')
+plot_dir = pyhere.here('cell2location', 'plots', '01-nn_run')
+Path(plot_dir).mkdir(parents=True, exist_ok=True)
+Path(processed_dir).mkdir(parents=True, exist_ok=True)
 
 marker_path = pyhere.here(
     'tangram_libd', 'processed-data', '03_nn_run', 'pan_markers.txt'
@@ -52,6 +60,8 @@ cell_type_var = 'cellType'    # in single-cell only
 spatial_coords_names = ('pxl_row_in_fullres', 'pxl_col_in_fullres')
 
 cell_types_to_drop = ['Macrophage', 'Mural', 'Tcell']
+
+plot_file_type = 'png' # 'pdf' is also supported for higher-quality plots
 
 ################################################################################
 #   Preprocessing
@@ -81,13 +91,10 @@ adata_vis.obsm['MT'] = adata_vis[:, adata_vis.var['MT_gene'].values].X.toarray()
 adata_vis = adata_vis[:, ~adata_vis.var['MT_gene'].values]
 
 # Use ENSEMBL as gene IDs to make sure IDs are unique and correctly matched
-adata_ref.var['SYMBOL'] = adata_ref.obs[gene_symbol_var]
+adata_ref.var['SYMBOL'] = adata_ref.var[gene_symbol_var]
 adata_ref.var.index = adata_ref.var[ensembl_id_var]
 adata_ref.var_names = adata_ref.var[ensembl_id_var]
 adata_ref.var.index.name = None
-adata_ref.raw.var['SYMBOL'] = adata_ref.obs[gene_symbol_var]
-adata_ref.raw.var.index = adata_ref.var[ensembl_id_var]
-adata_ref.raw.var.index.name = None
 
 #   Drop rare cell types from single-cell data
 adata_ref = adata_ref[
@@ -99,40 +106,50 @@ selected = filter_genes(
     adata_ref, cell_count_cutoff=5, cell_percentage_cutoff2=0.03,
     nonz_mean_cutoff=1.12
 )
+
+#   This code to save plot doesn't work! TODO
+f = plt.gcf()
+f.savefig(
+    os.path.join(
+        plot_dir, f'cell_annotation.{plot_file_type}'
+    ),
+    bbox_inches='tight'
+)
+
 adata_ref = adata_ref[:, selected].copy()
 
-#   Path to JSON from spaceranger including spot size for this sample
-json_path = json_dir + '/' + sample_name + '/scalefactors_json.json'
+#-------------------------------------------------------------------------------
+#   Attach hi-res images and scaleFactors to spatial AnnData
+#-------------------------------------------------------------------------------
 
-with open(json_path) as f: 
-    json_data = json.load(f)
+adata_vis.uns['spatial'] = {}
 
-#   Store scalefactors in AnnData as cell2location expects
-adata_vis.uns['spatial'] = {
-    sample_name: {
+for id in adata_vis.obs['sample'].cat.categories:
+    #   Path to JSON from spaceranger including spot size for this sample
+    json_path = os.path.join(json_dir, id, 'scalefactors_json.json')
+
+    with open(json_path) as f: 
+        json_data = json.load(f)
+
+    #   Store scalefactors in AnnData as cell2location expects
+    adata_vis.uns['spatial'][id] = {
         'scalefactors': json_data
     }
-}
 
-#-------------------------------------------------------------------------------
-#   Attach hi-res images to spatial AnnData
-#-------------------------------------------------------------------------------
-
-for id in adata_vis.obs['sample'].categories:
     #   Read in high-res image as numpy array with values in [0, 1] rather than
     #   [0, 255] (note that it isn't verified the original range is [0, 255]!). 
     #   Then attach to AnnData object
     img_path = str(
         pyhere.here(
             "tangram_libd/raw-data/03_nn_run/hires_histology",
-            sample_name + ".png"
+            id + ".png"
         )
     )
 
     img_arr = np.array(Image.open(img_path), dtype = np.float32) / 256
     assert img_arr.shape == (2000, 2000, 3), img_arr.shape
 
-    adata_vis.uns['spatial'][sample_name]['images'] = { 'hires': img_arr }
+    adata_vis.uns['spatial'][id]['images'] = { 'hires': img_arr }
 
 #-------------------------------------------------------------------------------
 #   Attach spatialCoords to spatial AnnData
@@ -166,12 +183,9 @@ adata_ref.write_h5ad(
 
 # prepare anndata for the regression model
 scvi.data.setup_anndata(
-    adata=adata_ref,
-    # 10X reaction / sample / batch
-    batch_key='Sample',
-    labels_key=cell_type_var,
-    # multiplicative technical effects (platform, 3' vs 5', donor effect)
-    categorical_covariate_keys=['Method'] # probably remove this
+    adata = adata_ref,
+    batch_key = 'Sample',
+    labels_key = cell_type_var
 )
 scvi.data.view_anndata_setup(adata_ref)
 
