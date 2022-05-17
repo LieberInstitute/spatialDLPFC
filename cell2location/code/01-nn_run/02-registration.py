0ce81b8feb05b0c92f72abcf9e11dc2ab04ff7a5
Nick-Eagles <nick.eagles@libd.org>
2022-05-17 16:37:59 -0400

    Make substantial progress on the cell2location tutorial code for our data

--- cell2location/code/01-nn_run/02-registration.py
+++ cell2location/code/01-nn_run/02-registration.py
@@ -40,8 +40,13 @@ cell_type_var = 'cellType'    # in single-cell only
 
 plot_file_type = 'png' # 'pdf' is also supported for higher-quality plots
 
+#   Default is 30 in tutorial, but 5 is recommended as an initial guess for
+#   Visium data:
+#   https://github.com/BayraktarLab/cell2location/blob/master/docs/images/Note_on_selecting_hyperparameters.pdf
+N_CELLS_PER_SPOT = 5 
+
 ################################################################################
-#   Perform regression
+#   Load AnnDatas
 ################################################################################
 
 #  Load AnnDatas
@@ -49,11 +54,15 @@ print('Loading AnnDatas...')
 adata_vis = sc.read_h5ad(sp_path)
 adata_ref = sc.read_h5ad(sc_path)
 
+################################################################################
+#   Estimate reference cell-type signatures
+################################################################################
+
 #   Prepare anndata for the regression model. The original code from the tutorial
 #   was changed, given the info here: https://github.com/BayraktarLab/cell2location/issues/145#issuecomment-1107410480
 RegressionModel.setup_anndata(
     adata = adata_ref,
-    batch_key = 'donor',
+    batch_key = 'donor', # tried 'processBatch' as well with poor results
     labels_key = cell_type_var
 )
 
@@ -69,7 +78,7 @@ mod.plot_history(20)
 f = plt.gcf()
 f.savefig(
     os.path.join(
-        plot_dir, f'training_history.{plot_file_type}'
+        plot_dir, f'cell_signature_training_history.{plot_file_type}'
     ),
     bbox_inches='tight'
 )
@@ -88,4 +97,97 @@ mod.save(f"{ref_run_name}", overwrite=True)
 # Save anndata object with results
 adata_ref.write_h5ad(
     os.path.join(processed_dir, 'adata_ref_after.h5ad')
+)
+
+mod.plot_QC()
+
+# export estimated expression in each cluster
+if 'means_per_cluster_mu_fg' in adata_ref.varm.keys():
+    inf_aver = adata_ref.varm['means_per_cluster_mu_fg'][[
+        f'means_per_cluster_mu_fg_{i}'
+        for i in adata_ref.uns['mod']['factor_names']
+    ]].copy()
+else:
+    inf_aver = adata_ref.var[[f'means_per_cluster_mu_fg_{i}'
+        for i in adata_ref.uns['mod']['factor_names']
+    ]].copy()
+
+inf_aver.columns = adata_ref.uns['mod']['factor_names']
+inf_aver.iloc[0:5, 0:5]
+
+# find shared genes and subset both anndata and reference signatures
+intersect = np.intersect1d(adata_vis.var_names, inf_aver.index)
+adata_vis = adata_vis[:, intersect].copy()
+inf_aver = inf_aver.loc[intersect, :].copy()
+
+################################################################################
+#   Spatial mapping
+################################################################################
+
+# prepare anndata for cell2location model
+cell2location.models.Cell2location.setup_anndata(
+    adata=adata_vis, batch_key="sample"
+)
+
+# create and train the model
+#   Note that 'detection_alpha' was changed from its default following:
+#   https://twitter.com/vitaliikl/status/1526510089514926081
+mod = cell2location.models.Cell2location(
+    adata_vis, cell_state_df=inf_aver,
+    # the expected average cell abundance: tissue-dependent
+    # hyper-prior which can be estimated from paired histology:
+    N_cells_per_location=N_CELLS_PER_SPOT,
+    # hyperparameter controlling normalisation of
+    # within-experiment variation in RNA detection (using default here):
+    detection_alpha=20 # default: 200
+)
+
+cell2location.models.Cell2location.view_anndata_setup(mod)
+
+mod.train(
+    max_epochs=30000,
+    batch_size=None, # train using full data (batch_size=None)
+    # use all data points in training because
+    # we need to estimate cell abundance at all locations
+    train_size=1,
+    use_gpu=True
+)
+
+# plot ELBO loss history during training, removing first 100 epochs from the plot
+mod.plot_history(1000)
+plt.legend(labels=['full data training']);
+f = plt.gcf()
+f.savefig(
+    os.path.join(
+        plot_dir, f'spatial_mapping_training_history.{plot_file_type}'
+    ),
+    bbox_inches='tight'
+)
+
+# In this section, we export the estimated cell abundance (summary of the
+# posterior distribution).
+adata_vis = mod.export_posterior(
+    adata_vis,
+    sample_kwargs={
+        'num_samples': 1000, 'batch_size': mod.adata.n_obs, 'use_gpu': True
+    }
+)
+
+# Save model
+mod.save(f"{run_name}", overwrite=True)
+
+# Save anndata object with results
+adata_vis.write_h5ad(os.path.join(processed_dir, 'adata_vis_after.h5ad'))
+
+# Examine reconstruction accuracy to assess if there are any issues with
+# mapping the plot should be roughly diagonal, strong deviations will signal
+# problems
+mod.plot_QC()
+
+fig = mod.plot_spatial_QC_across_batches()
+f.savefig(
+    os.path.join(
+        plot_dir, f'spatial_qc_across_batches.{plot_file_type}'
+    ),
+    bbox_inches='tight'
 )
