152f92ad3635fd7e8db641c59ba206ddd89a0696
abspangler13 <aspangle@gmail.com>
2022-05-17 16:30:53 -0400

    adjust point size and make plots square

--- code/analysis/03_BayesSpace/BayesSpace_grid_plots.R
+++ code/analysis/03_BayesSpace/BayesSpace_grid_plots.R
@@ -7,22 +7,44 @@ library(ggplot2)
 k <- as.numeric(Sys.getenv("SGE_TASK_ID"))
 
 #load spe object and clusters
-load(file = here::here("processed-data","rdata","spe","01_build_spe","spe_filtered_final.Rdata"),verbose = TRUE)
-spe <- cluster_import(
-  spe,
-  cluster_dir = here::here("processed-data", "rdata", "spe", "clustering_results"), 
-  prefix = ""
-)
+load(file = here::here("processed-data","rdata","spe","01_build_spe","spe_filtered_final_with_clusters.Rdata"),verbose = TRUE)
 
+mycolors <- brewer.pal(8, "Dark2")
+sample_order = unique(spe$sample_id)
 
-vis_grid_clus(
+pk <- vis_grid_clus(
   spe = spe,
   clustervar = paste0("bayesSpace_harmony_",k),
-  pdf = here("plots", "03_BayesSpace", paste0("vis_grid_clus_BayesSpace_k",k,".pdf")),
   sort_clust = FALSE,
-  colors = setNames(Polychrome::palette36.colors(k), 1:k),
+  colors = mycolors,
   spatial = FALSE,
-  point_size = 2,
-  height = 24,
-  width = 90
+  point_size = 1.5,
+  #height = 24,
+  #width = 90,
+  return_plots = TRUE
 )
+
+pk <- lapply(sample_order, function(sampleid){
+  p <- pk[[sampleid]]
+  p + theme(legend.position = "none")
+})
+names(pk) <- sample_order
+
+pdf(file = here::here("plots", "03_BayesSpace", paste0("vis_grid_clus_sfigu_BayesSpace_k",k,".pdf")), height = 5*8, width = 6*8)
+print(cowplot::plot_grid(plotlist = pk))
+dev.off()
+
+# bayesSpace_name <- paste0("bayesSpace_harmony_", k)
+# sample_ids <- unique(colData(spe)$sample_id)
+# 
+# pdf(file = here::here("plots","03_BayesSpace",paste0("test_vis_clus_bayesSpace_harmony_",k,".pdf")))
+# for (i in seq_along(sample_ids)){
+#   my_plot <- vis_clus(
+#     spe = spe,
+#     clustervar = bayesSpace_name,
+#     sampleid = sample_ids[i],
+#     colors = mycolors
+#   )
+#   print(my_plot)
+# }
+# dev.off()
