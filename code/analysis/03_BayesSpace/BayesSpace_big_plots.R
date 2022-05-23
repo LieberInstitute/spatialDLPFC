ded7bcd193257768e7dcd02a057d2bf241515681
abspangler13 <aspangle@gmail.com>
2022-05-23 13:40:03 -0400

    make script to re-make BayesSpace regular plots with new colors. make bash script to run as array job

---
+++ code/analysis/03_BayesSpace/BayesSpace_big_plots.R
@@ -0,0 +1,26 @@
+library("here")
+library("sessioninfo")
+library("spatialLIBD")
+library("RColorBrewer")
+library(ggplot2)
+
+k <- as.numeric(Sys.getenv("SGE_TASK_ID"))
+
+#load spe object and clusters
+load(file = here::here("processed-data","rdata","spe","01_build_spe","spe_filtered_final_with_clusters.Rdata"),verbose = TRUE)
+
+mycolors <- Polychrome::palette36.colors(k)
+bayesSpace_name <- paste0("bayesSpace_harmony_", k)
+sample_ids <- unique(colData(spe)$sample_id)
+
+pdf(file = here::here("plots","03_BayesSpace",paste0("polychrome_vis_clus_bayesSpace_harmony_",k,".pdf")))
+for (i in seq_along(sample_ids)){
+  my_plot <- vis_clus(
+    spe = spe,
+    clustervar = bayesSpace_name,
+    sampleid = sample_ids[i],
+    colors = mycolors
+  )
+  print(my_plot)
+}
+dev.off()
---
+++ code/analysis/03_BayesSpace/BayesSpace_big_plots.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#$ -cwd
+#$ -l caracol,mem_free=50G,h_vmem=50G,h_fsize=100G
+#$ -N bayesSpace_k_big_plots
+#$ -o logs/bayesSpace_k_big_plots.$TASK_ID.txt
+#$ -e logs/bayesSpace_k_big_plots.$TASK_ID.txt
+#$ -m e
+#$ -t 2-28
+#$ -tc 4
+
+USAGE_CUTOFF=10
+NUM_GPUS=1
+
+avail_gpus=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | cut -d " " -f 1 | awk -v usage="$USAGE_CUTOFF" '$1 < usage {print NR - 1}')
+
+#  Simply exit with an error if there are no GPUs left
+if [[ -z $avail_gpus ]]; then
+    echo "No GPUs are available."
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=$(echo "$avail_gpus" | head -n $NUM_GPUS | paste -sd ",")
+
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/devel
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript BayesSpace_big_plots.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
