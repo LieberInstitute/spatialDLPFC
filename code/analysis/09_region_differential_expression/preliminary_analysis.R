959d9e4ec70768471ee31b0b85c99cb384aab5bc
abspangler13 <aspangle@gmail.com>
2022-05-23 12:10:49 -0400

    fix error of throwing away normally/highly expressed genes instead of lowly expressed genes. make two different spe_pseudo objects, one where we filterByExpr grouping by region and another when we group by cluster.

--- code/analysis/09_region_differential_expression/preliminary_analysis.R
+++ code/analysis/09_region_differential_expression/preliminary_analysis.R
@@ -9,12 +9,14 @@ library(scater)
 k <- as.numeric(Sys.getenv("SGE_TASK_ID"))
 
 #load spe object and clusters
-load(file = here::here("processed-data","rdata","spe","01_build_spe","spe_filtered_final.Rdata"),verbose = TRUE)
-spe <- cluster_import(
-  spe,
-  cluster_dir = here::here("processed-data", "rdata", "spe", "clustering_results"), 
-  prefix = ""
-)
+# load(file = here::here("processed-data","rdata","spe","01_build_spe","spe_filtered_final.Rdata"),verbose = TRUE)
+# spe <- cluster_import(
+#   spe,
+#   cluster_dir = here::here("processed-data", "rdata", "spe", "clustering_results"), 
+#   prefix = ""
+# )
+
+load("/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/01_build_spe/spe_filtered_final_with_clusters.Rdata")
 
 ## Pseudo-bulk for our current BayesSpace cluster results
 spe_pseudo <- aggregateAcrossCells(
@@ -28,131 +30,85 @@ spe_pseudo$BayesSpace <- factor(spe_pseudo$BayesSpace)
 #save here for differential expression analysis
 save(spe_pseudo, file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("spe_pseudobulk_bayesSpace_k",k,".Rdata")))
 
+#drop samples with low numbers of spots here https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L207
+summary(spe_pseudo$ncells)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 80.0   394.8  1305.0  1898.8  3391.5  4629.0 
+#spe_pseudo <- spe_pseudo[, spe_pseudo$ncells >=15]
+
 #find a good expression cutoff using edgeR::filterByExpr https://rdrr.io/bioc/edgeR/man/filterByExpr.html
-rowData(spe_pseudo)$low_expr <- filterByExpr(spe_pseudo) #add group by region keep <- filterByExpr(y, group=current$tomato)
-summary(rowData(spe_pseudo)$low_expr)
+rowData(spe_pseudo)$filter_expr <- filterByExpr(spe_pseudo) #add group by region keep <- filterByExpr(y, group=current$tomato)
+summary(rowData(spe_pseudo)$filter_expr)
 # Mode   FALSE    TRUE 
-# logical   20406    8510 
+# logical   17914   11002 
 
-rowData(spe_pseudo)$low_expr_group_sample_id <- filterByExpr(spe_pseudo, group = spe_pseudo$sample_id)
-summary(rowData(spe_pseudo)$low_expr_group_sample_id )
+rowData(spe_pseudo)$filter_expr_group_sample_id <- filterByExpr(spe_pseudo, group = spe_pseudo$sample_id)
+summary(rowData(spe_pseudo)$filter_expr_group_sample_id )
 # Mode   FALSE    TRUE 
-# logical   15196   13720 
+# logical   13824   15092 
 
-rowData(spe_pseudo)$low_expr_group_cluster <- filterByExpr(spe_pseudo, group = colData(spe_pseudo)[[paste0("bayesSpace_harmony_",k)]])
-summary(rowData(spe_pseudo)$low_expr_group_cluster )
+rowData(spe_pseudo)$filter_expr_group_cluster <- filterByExpr(spe_pseudo, group = colData(spe_pseudo)[[paste0("bayesSpace_harmony_",k)]])
+summary(rowData(spe_pseudo)$filter_expr_group_cluster )
 # Mode   FALSE    TRUE 
 # logical   16762   12154 
 
-with(rowData(spe_pseudo), table(low_expr, low_expr_group_cluster))
-# low_expr_group_cluster
-# low_expr FALSE  TRUE
-# FALSE 16762  3644
-# TRUE      0  8510
-with(rowData(spe_pseudo), table(low_expr_group_sample_id, low_expr_group_cluster))
+rowData(spe_pseudo)$filter_expr_group_region <- filterByExpr(spe_pseudo, group = spe_pseudo$region)
+summary(rowData(spe_pseudo)$filter_expr_group_region )
+
+with(rowData(spe_pseudo), table(filter_expr,filter_expr_group_cluster))
+# filter_expr_group_cluster
+# filter_expr FALSE  TRUE
+# FALSE 16788  1126
+# TRUE      0 11002
+
+with(rowData(spe_pseudo), table(filter_expr_group_sample_id, filter_expr_group_cluster))
 # low_expr_group_cluster
 # low_expr_group_sample_id FALSE  TRUE
 # FALSE 15196     0
 # TRUE   1566 12154
 
-spe_pseudo <- spe_pseudo[which(!rowData(spe_pseudo)$low_expr),]
-dim(spe_pseudo)
+spe_pseudo_filter_cluster <- spe_pseudo[which(rowData(spe_pseudo)$filter_expr_group_cluster),]
+dim(spe_pseudo_filter_cluster)
 
+spe_pseudo_filter_region <- spe_pseudo[which(rowData(spe_pseudo)$filter_expr_group_region),]
+dim(spe_pseudo_filter_region)
 
 #log normalize the counts
 # spe_pseudo <- logNormCounts(spe_pseudo,size.factors = NULL)
-x <- edgeR::cpm(edgeR::calcNormFactors(spe_pseudo), log = TRUE, prior.count = 1)
+x <- edgeR::cpm(edgeR::calcNormFactors(spe_pseudo_filter_cluster), log = TRUE, prior.count = 1)
 ## Verify that the gene order hasn't changed
-stopifnot(identical(rownames(x), rownames(spe_pseudo)))
+stopifnot(identical(rownames(x), rownames(spe_pseudo_filter_cluster)))
 ## Fix the column names. DGEList will have samples names as Sample1 Sample2 etc
-dimnames(x) <- dimnames(spe_pseudo)
+dimnames(x) <- dimnames(spe_pseudo_filter_cluster)
 ## Store the log normalized counts on the SingleCellExperiment object
-logcounts(spe_pseudo) <- x
+logcounts(spe_pseudo_filter_cluster) <- x
 ## We don't need this 'x' object anymore
 rm(x)
 
-dim(spe_pseudo)
-#[1] 28916    90
+dim(spe_pseudo_filter_cluster)
+#[1] 12128    60
 
 #update this to indicate this version of the object is normalized and filtered
-save(spe_pseudo, file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("spe_pseudobulk_bayesSpace_normalized_filtered_k",k,".Rdata")))
+save(spe_pseudo_filter_cluster, file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("spe_pseudobulk_bayesSpace_normalized_filtered_cluster_k",k,".Rdata")))
 
-#run PCA
-pca <- prcomp(t(assays(spe_pseudo)$logcounts))
-jaffelab::getPcaVars(pca)[seq_len(50)]
-#  [1] 14.600  4.520  3.150  1.390  1.220  1.080  0.942  0.929  0.889  0.873
-# [11]  0.858  0.855  0.845  0.831  0.818  0.810  0.800  0.796  0.787  0.785
-# [21]  0.774  0.768  0.765  0.754  0.748  0.742  0.737  0.725  0.720  0.716
-# [31]  0.706  0.704  0.695  0.691  0.682  0.671  0.670  0.666  0.657  0.647
-# [41]  0.641  0.628  0.625  0.615  0.606  0.605  0.595  0.584  0.578  0.575
-pca_pseudo<- pca$x[, seq_len(50)]
+#log normalize the counts
+# spe_pseudo <- logNormCounts(spe_pseudo,size.factors = NULL)
+x <- edgeR::cpm(edgeR::calcNormFactors(spe_pseudo_filter_region), log = TRUE, prior.count = 1)
+## Verify that the gene order hasn't changed
+stopifnot(identical(rownames(x), rownames(spe_pseudo_filter_region)))
+## Fix the column names. DGEList will have samples names as Sample1 Sample2 etc
+dimnames(x) <- dimnames(spe_pseudo_filter_region)
+## Store the log normalized counts on the SingleCellExperiment object
+logcounts(spe_pseudo_filter_region) <- x
+## We don't need this 'x' object anymore
+rm(x)
+
+dim(spe_pseudo_filter_region)
+#[1] 28916    90
+
+save(spe_pseudo_filter_region, file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("spe_pseudobulk_bayesSpace_normalized_filtered_region_k",k,".Rdata")))
 
 
-
-reducedDims(spe_pseudo) <- list(PCA=pca_pseudo) 
-
-# code adapted from: http://bioconductor.org/packages/release/bioc/vignettes/scater/inst/doc/overview.html#2_Diagnostic_plots_for_quality_control
-###plot PCA###
-pdf(file = here::here("plots","09_region_differential_expression",paste0("spe_pseudobulk_pca_k",k,".pdf")), width = 14, height = 14)
-plotPCA(spe_pseudo, colour_by = "subject", ncomponents = 12, point_size = 1) 
-plotPCA(spe_pseudo, colour_by = "region", ncomponents = 12, point_size = 1) 
-plotPCA(spe_pseudo, colour_by = "sex", ncomponents = 12, point_size = 1) 
-plotPCA(spe_pseudo, colour_by = "age", ncomponents = 12, point_size = 1) 
-plotPCA(spe_pseudo, colour_by = "BayesSpace", ncomponents = 12, point_size = 1) 
-plotPCA(spe_pseudo, colour_by = "sample_id", ncomponents = 12, point_size = 1)
-dev.off()
-
-####plot explanatory variables ####
-
-#uses linear regression model
-vars <- getVarianceExplained(spe_pseudo, 
-                             variables=c("subject", "region", "sex", "age", "BayesSpace","sample_id")) 
-head(vars)
-
-# subject    region       sex         age
-# ENSG00000243485  6.149209 0.8049553 0.9891498 0.684494271
-# ENSG00000238009 22.059419 0.3825081 6.0221979 0.758922808
-# ENSG00000239945 10.112360 2.2471910 0.7490637 0.006556772
-# ENSG00000241860 15.910843 1.7697885 2.7770465 3.317220956
-# ENSG00000229905 10.113068 0.5911937 1.0110245 0.266643458
-# ENSG00000237491 17.499031 9.9500577 0.2762791 0.570564318
-
-pdf(file = here::here("plots","09_region_differential_expression",paste0("plot_explanatory_vars_k",k,"_testLeo.pdf")))
-plotExplanatoryVariables(vars)
-dev.off()
-
-## For the spatialLIBD shiny app
-rowData(spe_pseudo)$gene_search <-
-  paste0(
-    rowData(spe_pseudo)$gene_name,
-    "; ",
-    rowData(spe_pseudo)$gene_id
-  )
-
-## Drop things we don't need
-spatialCoords(spe_pseudo) <- NULL
-imgData(spe_pseudo) <- NULL
-
-## Simplify the colData()  for the pseudo-bulked data
-colData(spe_pseudo) <- colData(spe_pseudo)[, sort(c(
-  "age",
-  "sample_id",
-  "bayesSpace_harmony_9",
-  "subject",
-  "sex",
-  "diagnosis"
-))]
-
-# ## Load pathology colors
-# ## This info is used by spatialLIBD v1.7.18 or newer
-# source(here("code", "colors_pathology.R"), echo = TRUE, max.deparse.length = 500)
-# spe_pseudo$path_groups_colors <- colors_pathology[as.character(spe_pseudo$path_groups)]
-
-## save RDS file
-saveRDS(
-  spe_pseudo,
-  file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("sce_pseudobulk_bayesSpace_normalized_filtered_k",k,"_shiny.Rdata"))
-)
 
 ## Reproducibility information
 print("Reproducibility information:")
