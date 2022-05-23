9b7b541bc2db3ab58b1e675b7da193c90f07f5ff
Nick-Eagles <nick.eagles@libd.org>
2022-05-23 10:13:02 -0400

    Simplify the synapse download shell script, since the transfer node doesn't support 'qsub'ing

--- code/synapse_upload/03-download/01-download.sh
+++ code/synapse_upload/03-download/01-download.sh
@@ -1,20 +1,5 @@
-#!/bin/bash
-#$ -cwd
-#$ -l mem_free=5G,h_vmem=5G
-#$ -N "download"
-#$ -o ../../../processed-data/synapse_upload/03-download/01-download.log
-#$ -e ../../../processed-data/synapse_upload/03-download/01-download.log
-
-echo "**** Job starts ****"
-date
-echo "**** JHPCE info ****"
-echo "User: ${USER}"
-echo "Job id: ${JOB_ID}"
-echo "Job name: ${JOB_NAME}"
-echo "Hostname: ${HOSTNAME}"
+#   Performed from the transfer node for maximal speed:
+#       jhpce-transfer01.jhsph.edu
 
 module load synapse/2.6.0
 python 01-download.py
-
-echo "**** Job ends ****"
-date
