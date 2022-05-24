d4a921d51c62ccf88dde9d7237876a7c7afc913d
abspangler13 <aspangle@gmail.com>
2022-05-24 11:02:47 -0400

    finished script to make color bar for annotating figure two. Now uses correct color scale and plots the colors in the correct order

--- code/analysis/07_spatial_registration/color_bar.R
+++ code/analysis/07_spatial_registration/color_bar.R
@@ -1,9 +1,5 @@
 library(RColorBrewer)
 
-
-## Make the plot
-mycolors <- brewer.pal(8, "Dark2")
-
 color.bar <- function(mycolors){
   n.color <- length(mycolors)
   #par(mar = mar) #adjust width and height with par 
@@ -15,14 +11,31 @@ color.bar <- function(mycolors){
     xaxt = "n",
     yaxt = "n",
     xlab = "",
-    ylab = ""#,
-    #breaks = breaks,
-    #nlevel = length(mypal),
-    #axis.args = axis.args
+    ylab = ""
   )
   return(bar)
 }
 
-png("/Users/abbyspangler/Desktop/test_bar.png")
-color.bar(mycolors = mycolors)
-dev.off()
+## Make the plot
+k = c(3,9,16)
+for(i in seq_along(k) ){
+  print(k[i])
+  mycolors <- Polychrome::palette36.colors(k[i])
+  names(mycolors)<- c(1:k[i])
+  print(mycolors)
+  #print plot
+  # png(paste0("/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/plots/07_spatial_registration_",k[i],".png"))
+  # color.bar(mycolors = mycolors)
+  # dev.off()
+  # 
+  if(k[i]==9){
+    mycolors <- mycolors[c(1,2,3,5,7,6,9,4,8)]
+  }
+  if(k[i]==16){
+    mycolors <- mycolors[c(13,11,6,15,4,16,7,12,2,1,14,5,9,8,3,10)]
+  }
+  png(paste0("/Users/abbyspangler/Desktop/colorbar",k[i],".png"))
+  color.bar(mycolors = mycolors)
+  dev.off()
+}
+
