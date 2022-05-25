82eed612d3eec44280e052b2c2afe38787987918
abspangler13 <aspangle@gmail.com>
2022-05-25 16:04:19 -0400

    add row names to spe_pseudo object. remove all columns of colDatat I don't want to keep in the pseudobulked object. make csv of colData. filter out samples with less than 10 spots. add gene name and gene id column to rowData to be used with shiny app. drop image data to make object smaller. set colors for bayesSpace clusters. save as RDS. take out region pseudobulked object because I'll make a separate script to make that object.

--- code/analysis/09_region_differential_expression/preliminary_analysis.R
+++ code/analysis/09_region_differential_expression/preliminary_analysis.R
@@ -3,7 +3,7 @@ library(spatialLIBD)
 library(here)
 library(edgeR)
 library(scuttle)
-#library(scRNAseq)
+library(sessioninfo)
 library(scater)
 
 k <- as.numeric(Sys.getenv("SGE_TASK_ID"))
@@ -28,60 +28,54 @@ spe_pseudo <- aggregateAcrossCells(
 )
 spe_pseudo$BayesSpace <- factor(spe_pseudo$BayesSpace)
 
-# add this line https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L125
+# https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L125
+colnames(spe_pseudo) <- paste0(spe_pseudo$sample_id, "_", spe_pseudo$BayesSpace)
 
-#save here for differential expression analysis
-save(spe_pseudo, file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("spe_pseudobulk_bayesSpace_k",k,".Rdata")))
 
-#adapt this code to keep colData variables I want https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L181-L196
+#adapt this code to keep colData variables I want 
+#https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L181-L196
+colData(spe_pseudo) <- colData(spe_pseudo)[, sort(c(
+  "age",
+  "sample_id",
+  "BayesSpace",
+  "subject",
+  "sex",
+  "region",
+  "diagnosis",
+  "ncells"
+))]
 
-#drop samples with low numbers of spots here https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L207
+
+# write csv of pseudobulked colData in order to look at number of spots in each pseudobulked sample
+#https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L200
+write.csv(as.data.frame(colData(spe_pseudo)),file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("pseudobulk_colData_k",k,".csv")))
+
 summary(spe_pseudo$ncells)
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# 80.0   394.8  1305.0  1898.8  3391.5  4629.0 
-#spe_pseudo <- spe_pseudo[, spe_pseudo$ncells >=15]
 
-# write csv of this https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L200
+#drop samples with low numbers of spots here 
+#https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L207
+spe_pseudo <- spe_pseudo[, spe_pseudo$ncells >=10]
 
-#find a good expression cutoff using edgeR::filterByExpr https://rdrr.io/bioc/edgeR/man/filterByExpr.html
-rowData(spe_pseudo)$filter_expr <- filterByExpr(spe_pseudo) #add group by region keep <- filterByExpr(y, group=current$tomato)
-summary(rowData(spe_pseudo)$filter_expr)
-# Mode   FALSE    TRUE 
-# logical   17914   11002 
 
 rowData(spe_pseudo)$filter_expr_group_sample_id <- filterByExpr(spe_pseudo, group = spe_pseudo$sample_id)
 summary(rowData(spe_pseudo)$filter_expr_group_sample_id )
-# Mode   FALSE    TRUE 
-# logical   13824   15092 
 
-rowData(spe_pseudo)$filter_expr_group_cluster <- filterByExpr(spe_pseudo, group = colData(spe_pseudo)[[paste0("bayesSpace_harmony_",k)]])
+
+rowData(spe_pseudo)$filter_expr_group_cluster <- filterByExpr(spe_pseudo, group = spe_pseudo$BayesSpace)
 summary(rowData(spe_pseudo)$filter_expr_group_cluster )
-# Mode   FALSE    TRUE 
-# logical   16762   12154 
+
 
 rowData(spe_pseudo)$filter_expr_group_region <- filterByExpr(spe_pseudo, group = spe_pseudo$region)
 summary(rowData(spe_pseudo)$filter_expr_group_region )
 
 with(rowData(spe_pseudo), table(filter_expr,filter_expr_group_cluster))
-# filter_expr_group_cluster
-# filter_expr FALSE  TRUE
-# FALSE 16788  1126
-# TRUE      0 11002
-
 with(rowData(spe_pseudo), table(filter_expr_group_sample_id, filter_expr_group_cluster))
-# low_expr_group_cluster
-# low_expr_group_sample_id FALSE  TRUE
-# FALSE 15196     0
-# TRUE   1566 12154
 
 spe_pseudo_filter_cluster <- spe_pseudo[which(rowData(spe_pseudo)$filter_expr_group_cluster),]
 dim(spe_pseudo_filter_cluster)
 
 spe_pseudo_filter_region <- spe_pseudo[which(rowData(spe_pseudo)$filter_expr_group_region),]
 dim(spe_pseudo_filter_region)
-
-#log normalize the counts
-# spe_pseudo <- logNormCounts(spe_pseudo,size.factors = NULL)
 
 #replace calcNormFactors code with this https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L254-L258
 x <- edgeR::cpm(edgeR::calcNormFactors(spe_pseudo_filter_cluster), 
@@ -97,32 +91,29 @@ logcounts(spe_pseudo_filter_cluster) <- x
 rm(x)
 
 dim(spe_pseudo_filter_cluster)
-#[1] 12128    60
 
-#remove parts of pseudobulked objects to make them smaller https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L389-L399
+#remove parts of pseudobulked objects to make them smaller 
+#https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L389-L399
+rowData(spe_pseudo_filter_cluster)$gene_search <-
+  paste0(
+    rowData(spe_pseudo_filter_cluster)$gene_name,
+    "; ",
+    rowData(spe_pseudo_filter_cluster)$gene_id
+  )
 
-# adpate this to use the polychrome scale I'm already using for my other figures https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L403-L404
+## Drop things we don't need
+spatialCoords(spe_pseudo_filter_cluster) <- NULL
+imgData(spe_pseudo_filter_cluster) <- NULL
+
+
+# adpate this to use the polychrome scale I'm already using for my other figures 
+#https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/11_grey_matter_only/01_create_pseudobulk_data.R#L403-L404
+source(here("code", "analysis","colors_bayesSpace.R"), echo = TRUE, max.deparse.length = 500)
+spe_pseudo$BayesSpace_colors <- colors_bayesSpace[as.character(spe_pseudo$BayesSpace)]
 
 #update this to indicate this version of the object is normalized and filtered
-save(spe_pseudo_filter_cluster, file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("spe_pseudobulk_bayesSpace_normalized_filtered_cluster_k",k,".Rdata")))
-
-#log normalize the counts
-# spe_pseudo <- logNormCounts(spe_pseudo,size.factors = NULL)
-x <- edgeR::cpm(edgeR::calcNormFactors(spe_pseudo_filter_region), log = TRUE, prior.count = 1)
-## Verify that the gene order hasn't changed
-stopifnot(identical(rownames(x), rownames(spe_pseudo_filter_region)))
-## Fix the column names. DGEList will have samples names as Sample1 Sample2 etc
-dimnames(x) <- dimnames(spe_pseudo_filter_region)
-## Store the log normalized counts on the SingleCellExperiment object
-logcounts(spe_pseudo_filter_region) <- x
-## We don't need this 'x' object anymore
-rm(x)
-
-dim(spe_pseudo_filter_region)
-#[1] 28916    90
-
-save(spe_pseudo_filter_region, file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("spe_pseudobulk_bayesSpace_normalized_filtered_region_k",k,".Rdata")))
-
+saveRDS(spe_pseudo_filter_cluster, 
+        file = here::here("processed-data","rdata","spe","pseudo_bulked_spe",paste0("spe_pseudobulk_bayesSpace_normalized_filtered_cluster_k",k,".RDS")))
 
 
 ## Reproducibility information
