0553d1b5e65a23a007988a111b7814d5fdf215d4
Nick-Eagles <nick.eagles@libd.org>
2022-06-01 15:58:17 -0400

    Add python scripts to run cell2location for the 30 non-IF samples. Closes #87.

---
+++ code/spot_deconvo/03-cell2location/01-prepare_anndata_nonIF.py
@@ -0,0 +1,175 @@
+import sys
+import os
+
+import scanpy as sc
+import anndata
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import cell2location
+from cell2location.utils.filtering import filter_genes
+
+from matplotlib import rcParams
+rcParams['pdf.fonttype'] = 42 # enables correct plotting of text
+import seaborn as sns
+
+import pyhere
+from pathlib import Path
+from PIL import Image
+import json
+
+################################################################################
+#   Variable definitions
+################################################################################
+
+sc_path = pyhere.here(
+    "processed_data", "spot_deconvo", "01-tangram", "nonIF", "sce.h5ad"
+)
+sp_path = pyhere.here(
+    "processed_data", "spot_deconvo", "01-tangram", "nonIF", "spe.h5ad"
+)
+
+processed_dir = pyhere.here(
+    "processed_data", "spot_deconvo" "03-cell2location", "nonIF"
+)
+plot_dir = pyhere.here(
+    "plots", "spot_deconvo" "03-cell2location", "nonIF"
+)
+Path(plot_dir).mkdir(parents=True, exist_ok=True)
+Path(processed_dir).mkdir(parents=True, exist_ok=True)
+
+#   Directory containing hires image and a JSON containing scale factors and
+#   spot size for a given sample. Here '{}' will be replaced by a single
+#   sample name
+spaceranger_dir = pyhere.here(
+    'processed-data', 'rerun_spaceranger', '{}', 'outs', 'spatial'
+)
+
+#   TODO: check these below!
+
+#   Naming conventions used for different columns in the spatial AnnData
+sample_id_var = 'sample_id'   # in spatial object only
+ensembl_id_var = 'gene_id'    # in both spatial and single-cell objects
+gene_symbol_var = 'gene_name' # in both spatial and single-cell objects
+cell_type_var = 'cellType'    # in single-cell only
+spatial_coords_names = ('pxl_row_in_fullres', 'pxl_col_in_fullres')
+
+plot_file_type = 'png' # 'pdf' is also supported for higher-quality plots
+
+################################################################################
+#   Preprocessing
+################################################################################
+
+#  Load AnnDatas
+print('Loading AnnDatas...')
+adata_vis = sc.read_h5ad(sp_path)
+adata_ref = sc.read_h5ad(sc_path)
+
+adata_vis.obs['sample'] = adata_vis.obs[sample_id_var]
+
+# rename genes to ENSEMBL
+adata_vis.var['SYMBOL'] = adata_vis.var[gene_symbol_var]
+adata_vis.var_names = adata_vis.var[ensembl_id_var]
+adata_vis.var_names.name = None
+
+# find mitochondria-encoded (MT) genes
+adata_vis.var['MT_gene'] = [
+    gene.startswith('MT-') for gene in adata_vis.var['SYMBOL']
+]
+
+# remove MT genes for spatial mapping (keeping their counts in the object).
+adata_vis.obsm['MT'] = adata_vis[:, adata_vis.var['MT_gene'].values].X.toarray()
+adata_vis = adata_vis[:, ~adata_vis.var['MT_gene'].values]
+
+#   Spatial AnnData needs unique indices. Rather than using barcode (repeated
+#   for every sample), use "key" (barcode + sample ID)
+adata_vis.obs_names = adata_vis.obs['key']
+adata_vis.obs_names.name = None
+
+# Use ENSEMBL as gene IDs to make sure IDs are unique and correctly matched
+adata_ref.var['SYMBOL'] = adata_ref.var[gene_symbol_var]
+adata_ref.var.index = adata_ref.var[ensembl_id_var]
+adata_ref.var_names = adata_ref.var[ensembl_id_var]
+adata_ref.var.index.name = None
+
+#   Drop rare cell types from single-cell data
+adata_ref = adata_ref[
+    ~ adata_ref.obs[cell_type_var].isin(cell_types_to_drop), :
+]
+
+#   Subset to specific genes
+selected = filter_genes(
+    adata_ref, cell_count_cutoff=5, cell_percentage_cutoff2=0.03,
+    nonz_mean_cutoff=1.12
+)
+
+#   This code to save plot doesn't work! TODO
+f = plt.gcf()
+f.savefig(
+    os.path.join(
+        plot_dir, f'cell_annotation.{plot_file_type}'
+    ),
+    bbox_inches='tight'
+)
+
+adata_ref = adata_ref[:, selected].copy()
+
+#-------------------------------------------------------------------------------
+#   Attach hi-res images and scaleFactors to spatial AnnData
+#-------------------------------------------------------------------------------
+
+adata_vis.uns['spatial'] = {}
+
+for sample_id in adata_vis.obs['sample'].cat.categories:
+    #   Path to JSON from spaceranger including spot size for this sample
+    json_path = pyhere.here(
+        spaceranger_dir.format(sample_name), 'scalefactors_json.json'
+    )
+
+    with open(json_path) as f: 
+        json_data = json.load(f)
+
+    #   Store scalefactors in AnnData as cell2location expects
+    adata_vis.uns['spatial'][sample_id] = {
+        'scalefactors': json_data
+    }
+
+    #   Read in high-res image as numpy array with values in [0, 1] rather than
+    #   [0, 255] (note that it isn't verified the original range is [0, 255]!). 
+    #   Then attach to AnnData object
+    img_path = str(
+        pyhere.here(spaceranger_dir.format(sample_name), 'tissue_hires_image.png')
+    )
+
+    img_arr = np.array(Image.open(img_path), dtype = np.float32) / 256
+    assert img_arr.shape == (2000, 2000, 3), img_arr.shape
+
+    adata_vis.uns['spatial'][sample_id]['images'] = { 'hires': img_arr }
+
+#-------------------------------------------------------------------------------
+#   Attach spatialCoords to spatial AnnData
+#-------------------------------------------------------------------------------
+
+#   Squidpy expects spatial coords in a very specific format
+adata_vis.obsm['spatial'] = np.array(
+    list(
+        zip(
+            adata_vis.obs[spatial_coords_names[0]],
+            adata_vis.obs[spatial_coords_names[1]]
+        )
+    )
+)
+
+#-------------------------------------------------------------------------------
+#   Save AnnDatas
+#-------------------------------------------------------------------------------
+
+adata_vis.write_h5ad(
+    os.path.join(processed_dir, 'adata_vis_orig.h5ad')
+)
+
+adata_ref.write_h5ad(
+    os.path.join(processed_dir, 'adata_ref_orig.h5ad')
+)
---
+++ code/spot_deconvo/03-cell2location/02-registration_nonIF.py
@@ -0,0 +1,275 @@
+import sys
+import os
+
+import scanpy as sc
+import anndata
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import cell2location
+from cell2location.models import RegressionModel
+from cell2location.utils import select_slide
+from cell2location.plt import plot_spatial
+import scvi
+
+from matplotlib import rcParams
+rcParams['pdf.fonttype'] = 42 # enables correct plotting of text
+import seaborn as sns
+
+import pyhere
+import session_info
+from pathlib import Path
+
+################################################################################
+#   Variable definitions
+################################################################################
+
+processed_dir = pyhere.here(
+    "processed_data", "spot_deconvo" "03-cell2location", "nonIF"
+)
+plot_dir = pyhere.here(
+    "plots", "spot_deconvo" "03-cell2location", "nonIF"
+)
+
+sp_path = os.path.join(processed_dir, 'adata_vis_orig.h5ad')
+sc_path = os.path.join(processed_dir, 'adata_ref.h5ad')
+
+
+# create paths and names to results folders for reference regression and
+# cell2location models
+ref_run_name = f'{processed_dir}/reference_signatures'
+run_name = f'{processed_dir}/cell2location_map'
+
+#   TODO: adjust these as appropriate!
+
+#   Naming conventions used for different columns in the spatial AnnData
+cell_type_var = 'cellType'    # in single-cell only
+
+plot_file_type = 'png' # 'pdf' is also supported for higher-quality plots
+
+#   Default is 30 in tutorial, but 5 is recommended as an initial guess for
+#   Visium data:
+#   https://github.com/BayraktarLab/cell2location/blob/master/docs/images/Note_on_selecting_hyperparameters.pdf
+N_CELLS_PER_SPOT = 5
+
+################################################################################
+#   Function definitions
+################################################################################
+
+def perform_regression(
+    mod, adata, adata_name, max_epochs, lr, sample_kwargs, plot_name
+):
+    # Use all data for training (validation not implemented yet, train_size=1)
+    mod.train(
+        max_epochs=max_epochs, batch_size=sample_kwargs['batch_size'],
+        train_size=1, lr=lr, use_gpu=True
+    )
+
+    # plot ELBO loss history during training, removing first 10% of epochs from
+    # the plot
+    mod.plot_history(int(max_epochs / 10))
+    f = plt.gcf()
+    f.savefig(
+        os.path.join(plot_dir, f'{plot_name}.{plot_file_type}'),
+        bbox_inches='tight'
+    )
+    plt.close(f)
+
+    # In this section, we export the estimated cell abundance (summary of the
+    # posterior distribution).
+    adata = mod.export_posterior(
+        adata, sample_kwargs=sample_kwargs
+    )
+
+    # Save model
+    mod.save(f'{processed_dir}/{adata_name}', overwrite=True)
+
+    # Save anndata object with results
+    adata.write_h5ad(
+        os.path.join(processed_dir, f'{adata_name}_after.h5ad')
+    )
+
+    # Examine reconstruction accuracy to assess if there are any issues with
+    # mapping the plot should be roughly diagonal, strong deviations will signal
+    # problems
+    mod.plot_QC()
+    plt.savefig(
+        os.path.join(
+            plot_dir, f'{adata_name}_QC.{plot_file_type}'
+        ),
+        bbox_inches='tight'
+    )
+    plt.close('all')
+
+    return (adata, mod)
+
+################################################################################
+#   Load AnnDatas
+################################################################################
+
+#  Load AnnDatas
+print('Loading AnnDatas...')
+adata_vis = sc.read_h5ad(sp_path)
+adata_ref = sc.read_h5ad(sc_path)
+
+################################################################################
+#   Estimate reference cell-type signatures
+################################################################################
+
+#   Prepare anndata for the regression model. The original code from the tutorial
+#   was changed, given the info here: https://github.com/BayraktarLab/cell2location/issues/145#issuecomment-1107410480
+RegressionModel.setup_anndata(
+    adata = adata_ref,
+    batch_key = 'donor', # tried 'processBatch' as well with poor results
+    labels_key = cell_type_var
+)
+
+# create and train the regression model
+mod = RegressionModel(adata_ref)
+RegressionModel.view_anndata_setup(mod)
+
+adata_ref, mod = perform_regression(
+    mod, adata_ref, 'adata_ref', 250, 0.002,
+    {'num_samples': 1000, 'batch_size': 2500, 'use_gpu': True},
+    'cell_signature_training_history'
+)
+
+# export estimated expression in each cluster
+if 'means_per_cluster_mu_fg' in adata_ref.varm.keys():
+    inf_aver = adata_ref.varm['means_per_cluster_mu_fg'][[
+        f'means_per_cluster_mu_fg_{i}'
+        for i in adata_ref.uns['mod']['factor_names']
+    ]].copy()
+else:
+    inf_aver = adata_ref.var[[f'means_per_cluster_mu_fg_{i}'
+        for i in adata_ref.uns['mod']['factor_names']
+    ]].copy()
+
+inf_aver.columns = adata_ref.uns['mod']['factor_names']
+inf_aver.iloc[0:5, 0:5]
+
+# find shared genes and subset both anndata and reference signatures
+intersect = np.intersect1d(adata_vis.var_names, inf_aver.index)
+adata_vis = adata_vis[:, intersect].copy()
+inf_aver = inf_aver.loc[intersect, :].copy()
+
+################################################################################
+#   Spatial mapping
+################################################################################
+
+# prepare anndata for cell2location model
+cell2location.models.Cell2location.setup_anndata(
+    adata=adata_vis, batch_key="sample"
+)
+
+# create and train the model
+#   Note that 'detection_alpha' was changed from its default following:
+#   https://twitter.com/vitaliikl/status/1526510089514926081
+mod = cell2location.models.Cell2location(
+    adata_vis, cell_state_df=inf_aver,
+    # the expected average cell abundance: tissue-dependent
+    # hyper-prior which can be estimated from paired histology:
+    N_cells_per_location=N_CELLS_PER_SPOT,
+    # hyperparameter controlling normalisation of
+    # within-experiment variation in RNA detection:
+    detection_alpha=20 # default: 200
+)
+
+cell2location.models.Cell2location.view_anndata_setup(mod)
+
+adata_vis, mod = perform_regression(
+    mod, adata_vis, 'adata_vis', 30000, None,
+    {'num_samples': 1000, 'batch_size': mod.adata.n_obs, 'use_gpu': True},
+    'spatial_mapping_training_history'
+)
+
+fig = mod.plot_spatial_QC_across_batches()
+fig.savefig(
+    os.path.join(
+        plot_dir, f'spatial_qc_across_batches.{plot_file_type}'
+    ),
+    bbox_inches='tight'
+)
+plt.close(fig)
+
+# add 5% quantile, representing confident cell abundance, 'at least this amount
+# is present', to adata.obs with nice names for plotting
+adata_vis.obs[adata_vis.uns['mod']['factor_names']] = adata_vis.obsm[
+    'q05_cell_abundance_w_sf'
+]
+
+################################################################################
+#   Visualization
+################################################################################
+
+#   TODO:
+#       1. possibly change number of cell types + related choices
+
+for sample_id in adata_vis.obs['sample'].cat.categories:
+        #   Subset to this sample
+    slide = select_slide(adata_vis, sample_id)
+
+    cell_types = adata_ref.obs[cell_type_var].cat.categories
+
+    #   Plot 8 cell types separately for this sample
+    sc.pl.spatial(
+        slide, cmap='magma',
+        # show first 8 cell types
+        color=cell_types[:8],
+        ncols=4, size=1.3,
+        img_key='hires',
+        # limit color scale at 99.2% quantile of cell abundance
+        vmin=0, vmax='p99.2'
+    )
+    f = plt.gcf()
+    f.savefig(
+        os.path.join(
+            plot_dir, f'individual_cell_types_{sample_id}.{plot_file_type}'
+        ),
+        bbox_inches='tight'
+    )
+    plt.close(f)
+
+    fig = plot_spatial(
+        adata=slide,
+        # labels to show on a plot
+        color=cell_types[:6], labels=cell_types[:6],
+        show_img=True,
+        # 'fast' (white background) or 'dark_background'
+        style='fast',
+        # limit color scale at 99.2% quantile of cell abundance
+        max_color_quantile=0.992,
+        # size of locations (adjust depending on figure size)
+        circle_diameter=6,
+        colorbar_position='right'
+    )
+    fig.savefig(
+        os.path.join(
+            plot_dir, f'multi_cell_types_{sample_id}.{plot_file_type}'
+        ),
+        bbox_inches='tight'
+    )
+    plt.close(fig)
+
+################################################################################
+#   Export deconvo results
+################################################################################
+
+clusters = adata_vis.obs[
+    ['sample'] + list(adata_vis.uns['mod']['factor_names'])
+]
+clusters.index.name = 'key'
+
+clusters_by_id = clusters.groupby('sample')
+
+for sample_id in adata_vis.obs['sample'].cat.categories:
+    clusters_subset = clusters_by_id.get_group(sample_id)
+
+    Path(os.path.join(processed_dir, sample_id)).mkdir(
+        parents=True, exist_ok=True
+    )
+    clusters_subset.to_csv(os.path.join(processed_dir, sample_id, 'clusters.csv'))
+
+session_info.show(html=False)
