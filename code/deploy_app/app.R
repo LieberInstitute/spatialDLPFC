5f1685e4e0cf5ef2076e1289ea4faa44740d90a4
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-06-13 11:17:07 +0200

    Add checks for the right number of tests

--- code/deploy_app/app.R
+++ code/deploy_app/app.R
@@ -16,17 +16,27 @@ load("spe_subset.Rdata", verbose = TRUE)
 spe_pseudo <- readRDS("spe_pseudobulk_bayesSpace_normalized_filtered_cluster_k9.RDS")
 #load modeling results for k9 clustering/pseudobulking
 load("parsed_modeling_results_k9.Rdata",verbose = TRUE)
-warning("modeling results loaded")
 
 
 ## For sig_genes_extract_all() to work https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/05_deploy_app_wholegenome/app.R#L37-L44
 spe_pseudo$spatialLIBD <- spe_pseudo$BayesSpace
+
+## Check that we have the right number of tests
+k <- 9
+tests <- lapply(modeling_results, function(x) { colnames(x)[grep("stat", colnames(x))]})
+stopifnot(length(tests$anova) == 1) ## assuming only WM
+stopifnot(length(tests$enrichment) == k)
+stopifnot(length(tests$pairwise) == choose(k, 2) * 2)
+
 sig_genes <- sig_genes_extract_all(
   n = nrow(spe_pseudo),
   modeling_results = modeling_results,
   sce_layer = spe_pseudo
 )
-warning("sig_genes done")
+
+## Check that we have the right number of tests.
+## the + 1 at the end assumes only WM
+# stopifnot(length(unique(sig_genes$test)) == choose(k, 2) * 2 + k + 1)
 
 ## Extract FDR < 5%
 ## From
