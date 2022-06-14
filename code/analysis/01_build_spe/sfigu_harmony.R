b9a8e97b3df4863e64caba5e8104d683e0de2796
abspangler13 <aspangle@gmail.com>
2022-06-14 16:30:48 -0400

    change legend font size

--- code/analysis/01_build_spe/sfigu_harmony.R
+++ code/analysis/01_build_spe/sfigu_harmony.R
@@ -1,3 +1,8 @@
+library(ggplot2)
+library(here)
+library(spatialLIBD)
+
+load(file = here::here("processed-data","rdata","spe","01_build_spe","spe_filtered_final_with_clusters.Rdata"))
 
 #uncorrected umap
 pdf(file=here::here("plots", "01_build_spe","sfigu_harmony_UMAP_sample_id.pdf"))
@@ -9,7 +14,7 @@ ggplot(data.frame(reducedDim(spe, "UMAP")),
   ggtitle("UMAP of Uncorrected Data") + 
   guides(colour = guide_legend(override.aes = list(size=3))) +
   scale_colour_discrete(name = "Sample ID")+
-  theme(axis.text = element_text(size = 15),axis.title = element_text(size = 18),plot.title = element_text(size = 20))
+  theme(axis.text = element_text(size = 15),axis.title = element_text(size = 18),plot.title = element_text(size = 20), legend.text=element_text(size = 15))
 dev.off()
 
 
@@ -23,5 +28,5 @@ ggplot(data.frame(reducedDim(spe, "UMAP.HARMONY")),
   ggtitle("UMAP of Batch-Corrected Data") + 
   guides(colour = guide_legend(override.aes = list(size=3))) +
   scale_colour_discrete(name = "Sample ID")+
-  theme(axis.text = element_text(size = 15),axis.title = element_text(size = 18),plot.title = element_text(size = 20))
+  theme(axis.text = element_text(size = 15),axis.title = element_text(size = 18),plot.title = element_text(size = 20), legend.text=element_text(size = 15))
 dev.off()
