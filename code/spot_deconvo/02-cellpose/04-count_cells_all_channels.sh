8f2010dc333b97beab501f15cf9b6d969d6be071
Nick-Eagles <nick.eagles@libd.org>
2022-06-15 15:42:33 -0400

    Add a shell script to perform deconvolution given the cellpose IF segmentations

---
+++ code/spot_deconvo/02-cellpose/04-count_cells_all_channels.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#$ -cwd
+#$ -N "count_cells_all_channels"
+#$ -o ../../../processed-data/spot_deconvo/02-cellpose/04-count_cells_all_channels.log
+#$ -e ../../../processed-data/spot_deconvo/02-cellpose/04-count_cells_all_channels.log
+#$ -l bluejay,mf=20G,h_vmem=20G
+#$ -t 1
+#$ -tc 1
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+module load cellpose/2.0
+python 04-count_cells_all_channels.py
+
+echo "**** Job ends ****"
+date
