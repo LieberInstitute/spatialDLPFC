fa96ff5c49478f38f489c27d6b2c022b03f655dd
abspangler13 <aspangle@gmail.com>
2022-06-17 11:56:28 -0400

    creates two different t-stat correlations plots. One with all genes and one with just the genes used in spatial registration

--- code/analysis/07_spatial_registration/t_stat_cor_plot.R
+++ code/analysis/07_spatial_registration/t_stat_cor_plot.R
@@ -1,53 +1,120 @@
 library(spatialLIBD)
+library(ggplot2)
 
 k = 7
 load(file = here::here("processed-data","rdata","spe","08_layer_differential_expression",paste0("parsed_modeling_results_k",k,".Rdata")))
-my_enrich_tstats <-modeling_results$enrichment
-rownames(my_enrich_tstats) = my_enrich_tstats$ensembl
+stats <-modeling_results$enrichment
+rownames(stats) = stats$ensembl
+dim(stats)
+#[1] 12327    23
+# colnames(stats)
+# [1] "t_stat_1"  "t_stat_2"  "t_stat_3"  "t_stat_4"  "t_stat_5"  "t_stat_6" 
+# [7] "t_stat_7"  "p_value_1" "p_value_2" "p_value_3" "p_value_4" "p_value_5"
+# [13] "p_value_6" "p_value_7" "fdr_1"     "fdr_2"     "fdr_3"     "fdr_4"    
+# [19] "fdr_5"     "fdr_6"     "fdr_7"     "ensembl"   "gene"  
 
-ground_truth <- spatialLIBD::fetch_data("modeling_results")
-ground_truth_enrich <-ground_truth$enrichment
-rownames(ground_truth_enrich) = ground_truth_enrich$ensembl
+tstats <- spatialLIBD::fetch_data("modeling_results")
+tstats <-tstats$enrichment
+rownames(tstats) = tstats$ensembl
+dim(tstats)
+#[1] 22331    23
+colnames(tstats)
+# [1] "t_stat_WM"      "t_stat_Layer1"  "t_stat_Layer2"  "t_stat_Layer3" 
+# [5] "t_stat_Layer4"  "t_stat_Layer5"  "t_stat_Layer6"  "p_value_WM"    
+# [9] "p_value_Layer1" "p_value_Layer2" "p_value_Layer3" "p_value_Layer4"
+# [13] "p_value_Layer5" "p_value_Layer6" "fdr_WM"         "fdr_Layer1"    
+# [17] "fdr_Layer2"     "fdr_Layer3"     "fdr_Layer4"     "fdr_Layer5"    
+# [21] "fdr_Layer6"     "ensembl"        "gene"  
 
 tstats <-
-  ground_truth_enrich[, grep("[f|t]_stat_", colnames(ground_truth_enrich))]
+  tstats[, grep("[f|t]_stat_", colnames(tstats))]
 colnames(tstats) <-
   gsub("[f|t]_stat_", "", colnames(tstats))
+colnames(tstats)
+#[1] "WM"     "Layer1" "Layer2" "Layer3" "Layer4" "Layer5" "Layer6"
 
+stats <-
+  stats[, grep("[f|t]_stat_", colnames(stats))]
+colnames(stats) <-
+  gsub("[f|t]_stat_", "", colnames(stats))
+colnames(stats)
+# [1] "1" "2" "3" "4" "5" "6" "7"
+
+#from layer_stat_cor code https://github.com/LieberInstitute/spatialLIBD/blob/d44eea67ffd876ace89718024bf855dbce268111/R/layer_stat_cor.R#L77-L79
 top_n_index <- unique(as.vector(apply(tstats, 2, function(t) {
   order(t, decreasing = TRUE)[seq_len(100)]
 })))
+length(top_n_index)
+#[1] 692
 
-tstats <- tstats[top_n_index, , drop = FALSE]
+tstats_small <- tstats[top_n_index, , drop = FALSE]
+dim(tstats_small)
+#[1] 692   7
 
-mm<-match(rownames(tstats),rownames(my_enrich_tstats))
+mm<-match(rownames(tstats_small),rownames(stats))
+length(which(!is.na(mm)))
+#[1] 584
 
-tstats <- tstats[!is.na(mm), ]
-my_enrich_tstats <- my_enrich_tstats[mm[!is.na(mm)], ]
+tstats_small <- tstats_small[!is.na(mm), ]
+dim(tstats_small)
+# [1] 584   7
 
-top_genes <- rownames(tstats)
+stats_small <-stats[mm[!is.na(mm)], ]
+dim(stats_small)
+#[1] 584   7
 
-#make single dataframe 
+top_genes <- rownames(tstats_small)
+length(top_genes)
+#[1] 584
 
+#make plot 
 pdf(file = here::here("plots","07_spatial_registration","t_cor_k7_wm.pdf"))
-plot(my_enrich_tstats$t_stat_7,tstats$WM)
+plot(stats_small[,7],tstats_small$WM)
 dev.off()
 
-#try to plot all genes
-k = 7
-load(file = here::here("processed-data","rdata","spe","08_layer_differential_expression",paste0("parsed_modeling_results_k",k,".Rdata")))
-my_enrich_tstats <-modeling_results$enrichment
-rownames(my_enrich_tstats) = my_enrich_tstats$ensembl
 
-ground_truth <- spatialLIBD::fetch_data("modeling_results")
-ground_truth_enrich <-ground_truth$enrichment
-rownames(ground_truth_enrich) = ground_truth_enrich$ensembl
+#make nicer plot 
+dat_small <- cbind(stats_small,tstats_small)
+colnames(dat_small)
 
-common_genes <- intersect(rownames(ground_truth_enrich),rownames(my_enrich_tstats))
-ground_truth_enrich <-ground_truth_enrich[common_genes,]
-my_enrich_tstats <- my_enrich_tstats[common_genes,]
+#from Louise's code https://github.com/LieberInstitute/goesHyde_mdd_rnaseq/blob/2bb13a25fad8d1260ef38a8f073be4387c1f9ed0/differential_expression/code/utils.R#L51-L59
+pdf(file = here::here("plots","07_spatial_registration","ggplot_t_cor_k7_wm.pdf"))
+ggplot(dat_small, aes(x = dat_small[,7], y = WM)) +
+  geom_point(alpha = 0.7, size = 0.5) +
+  labs(x = "t-stats BayesSpace Cluster 7", 
+       y = "t-stats Ground Truth WM",
+       title = "t-stat Correlation") +
+  theme_bw() +
+  geom_smooth(method=lm, se=FALSE, colour = "#00BA38") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+dev.off()
+
+
+#Make cor plot with all genes and color by just the 
+common_genes <- intersect(rownames(stats),rownames(tstats))
+length(common_genes)
+# [1] 12150
+stats <- stats[common_genes,]
+tstats <- tstats[common_genes,]
 
 pdf(file = here::here("plots","07_spatial_registration","t_cor_k7_wm_all_genes.pdf"))
-plot(my_enrich_tstats$t_stat_7,tstats$WM)
+plot(stats[,7],tstats$WM)
+dev.off()
+
+dat <- cbind(stats,tstats)
+dat$TopGene<- "FALSE"
+dat[top_genes,]$TopGene <-"TRUE"
+
+pdf(file = here::here("plots","07_spatial_registration","ggplot_t_cor_k7_wm_all_genes.pdf"))
+ggplot(dat, aes(x = dat[,7], y = WM,color = TopGene)) +
+  geom_point(alpha = 0.7, size = 0.5) +
+  labs(x = "t-stats BayesSpace Cluster 7", 
+       y = "t-stats Ground Truth WM",
+       title = "t-stat Correlation") +
+  theme_bw() +
+  geom_smooth(method=lm, se=FALSE, colour = "#00BA38") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  scale_color_manual(values = c ("TRUE" = "red", "FALSE" = "black"))+
+  guides(fill=guide_legend(title="Top Gene"))
 dev.off()
 
