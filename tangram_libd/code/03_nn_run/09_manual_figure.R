9a62921b14ff8c2abdb50be823b426fde276ff5d
Nick-Eagles <nick.eagles@libd.org>
2022-06-27 10:21:18 -0400

    Begin to start an R script to generate a tangram-like deconvolution figure. I'll put this off for now, as it's lower priority atm

---
+++ tangram_libd/code/03_nn_run/09_manual_figure.R
@@ -0,0 +1,5 @@
+#   The "main tangram deconvolution figure" (Fig 4a from the paper) is hard to
+#   generate outside of squidpy, and requires cell centroid info, which doesn't
+#   seem critically important to the value of the figure. Here I explore if we
+#   can generate a similar figure that only requires cell counts per spot.
+
