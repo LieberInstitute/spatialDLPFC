23026ceb96c06ed7f3f6a328b255f23c591716dd
abspangler13 <aspangle@gmail.com>
2022-07-01 12:34:49 -0400

    re-order color bars for figure 2. Add color bar for k = 7

--- code/analysis/07_spatial_registration/color_bar.R
+++ code/analysis/07_spatial_registration/color_bar.R
@@ -17,7 +17,7 @@ color.bar <- function(mycolors){
 }
 
 ## Make the plot
-k = c(3,9,16,28)
+k = c(3,7,9,16,28)
 for(i in seq_along(k) ){
   print(k[i])
   mycolors <- Polychrome::palette36.colors(k[i])
@@ -28,16 +28,19 @@ for(i in seq_along(k) ){
   # color.bar(mycolors = mycolors)
   # dev.off()
   # 
+  if(k[i]==7){
+    mycolors <- mycolors[c(1,5,7,2,3,4,6)]
+  }
   if(k[i]==9){
-    mycolors <- mycolors[c(1,2,3,5,7,6,9,4,8)]
+    mycolors <- mycolors[c(1,2,6,9,4,7,3,5,8)]
   }
   if(k[i]==16){
-    mycolors <- mycolors[c(13,11,6,15,4,16,7,12,2,1,14,5,9,8,3,10)]
+    mycolors <- mycolors[c(1,2,14,6,15,11,13,4,16,7,12,5,9,3,8,10)]
   }
   if(k[i]==28){
-    mycolors <- mycolors[c(16,17,20,6,28,4,13,19,7,14,11,23,27,26,12,15,22,25,5,10,24,1,8,9,2,3)]
+    mycolors <- mycolors[c(16,17,20,5,28,4,13,19,7,14,11,26,27,23,12,15,22,25,5,10,24,8,9,1,2,3)]
   }
-  png(paste0("/Users/abbyspangler/Desktop/colorbar",k[i],".png"))
+  png(paste0("/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/plots/07_spatial_registration/colobar_k",k[i],".png"))
   color.bar(mycolors = mycolors)
   dev.off()
 }
