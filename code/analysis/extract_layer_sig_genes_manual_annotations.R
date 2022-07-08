55079532bfd7ba2e47e29c0199745d296c919b29
abspangler13 <aspangle@gmail.com>
2022-07-08 11:28:53 -0400

    short script to extract sig genes from manual annotations and save object for Louise

---
+++ code/analysis/extract_layer_sig_genes_manual_annotations.R
@@ -0,0 +1,12 @@
+library(spatialLIBD)
+modeling_results = fetch_data(type = "modeling_results")
+
+sig_genes <- sig_genes_extract(
+  n = 10,
+  modeling_results = modeling_results,
+  model_type = names(modeling_results)[2],
+  reverse = FALSE,
+  sce_layer = fetch_data(type = "sce_layer")
+)
+
+save(sig_genes, file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/sig_genes_manual_annotations.rda")
