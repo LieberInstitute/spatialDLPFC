ed71fcfbf2ec24a19d21fe44a5a15a1a72caa124
abspangler13 <aspangle@gmail.com>
2022-07-15 09:55:43 -0400

    add top 25 genes for pairwise comparison. export to csv and version control

--- code/analysis/08_layer_differential_expression/top_500_enriched_genes.R
+++ code/analysis/08_layer_differential_expression/top_500_enriched_genes.R
@@ -45,3 +45,28 @@ for(i in 1:ncol(stats)) {
 write.csv(df_enriched,
           file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/08_layer_differential_expression/top_500_enriched_genes_k16.csv",
           row.names = TRUE,col.names = TRUE)
+
+#### top 25 genes from pairwise model k =16
+
+library(spatialLIBD)
+load(file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/08_layer_differential_expression/parsed_modeling_results_k16.Rdata")
+
+sig_genes = sig_genes_extract(
+  n = 25,
+  modeling_results = modeling_results,
+  model_type = names(modeling_results)[3],
+  reverse = FALSE,
+  sce_layer = fetch_data(type = "sce_layer")
+)
+
+# Pairwise: 5 vs. 9 (Layer 4?)
+# Pairwise: 4 vs. 16 (Layer 5?)
+# Pairwise: 7 vs. 13 (Layer "6A" vs. "6B")
+# Pairwise: 12 vs. 13 (layer 6 apex vs base)
+tests <- c("BayesSpace5-BayesSpace9","BayesSpace4-BayesSpace16","BayesSpace7-BayesSpace13","BayesSpace12-BayesSpace13")
+
+sig_genes = sig_genes[which(sig_genes$test %in% tests),]
+write.csv(sig_genes,
+          file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/08_layer_differential_expression/top_25_pairwise_k16_subset.csv",
+          row.names = TRUE,col.names = TRUE)
+
