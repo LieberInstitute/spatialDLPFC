102be98c0f05dc66b175d01d6f8462680ea733e4
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-07-15 18:22:58 -0400

    Move the sig_genes code to subset.R for the k = 9 spatialLIBD shiny app

--- code/deploy_app/app.R
+++ code/deploy_app/app.R
@@ -16,50 +16,7 @@ load("spe_subset.Rdata", verbose = TRUE)
 spe_pseudo <- readRDS("spe_pseudobulk_bayesSpace_normalized_filtered_cluster_k9.RDS")
 #load modeling results for k9 clustering/pseudobulking
 load("parsed_modeling_results_k9.Rdata",verbose = TRUE)
-
-
-## For sig_genes_extract_all() to work https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/05_deploy_app_wholegenome/app.R#L37-L44
-spe_pseudo$spatialLIBD <- spe_pseudo$BayesSpace
-
-## Check that we have the right number of tests
-k <- 9
-tests <- lapply(modeling_results, function(x) { colnames(x)[grep("stat", colnames(x))]})
-stopifnot(length(tests$anova) == 1) ## assuming only noWM
-stopifnot(length(tests$enrichment) == k)
-stopifnot(length(tests$pairwise) == choose(k, 2))
-
-sig_genes <- sig_genes_extract_all(
-  n = nrow(spe_pseudo),
-  modeling_results = modeling_results,
-  sce_layer = spe_pseudo
-)
-
-lobstr::obj_size(modeling_results) / 1024^3
-#0.01460112 B
-lobstr::obj_size(sig_genes) / 1024^3
-#0.3946267 B
-
-dim(sig_genes)
-#[1] 1002450      12
-
-## Check that we have the right number of tests.
-## the + 1 at the end assumes only noWM
-# stopifnot(length(unique(sig_genes$test)) == choose(k, 2) * 2 + k + 1)
-
-## Extract FDR < 5%
-## From
-## https://github.com/LieberInstitute/brainseq_phase2/blob/be2b7f972bb2a0ede320633bf06abe1d4ef2c067/supp_tabs/create_supp_tables.R#L173-L181
-# fix_csv <- function(df) {
-#   for (i in seq_len(ncol(df))) {
-#     if (any(grepl(",", df[, i]))) {
-#       message(paste(Sys.time(), "fixing column", colnames(df)[i]))
-#       df[, i] <- gsub(",", ";", df[, i])
-#     }
-#   }
-#   return(df)
-# }
-# z <- fix_csv(as.data.frame(subset(sig_genes, fdr < 0.05)))
-# write.csv(z, file = file.path(dir_rdata, "Visium_IF_AD_wholegenome_model_results_FDR5perc.csv"))
+load("sig_genes_subset.Rdata", verbose = TRUE)
 
 spe$BayesSpace <- spe$bayesSpace_harmony_9
 vars <- colnames(colData(spe))
