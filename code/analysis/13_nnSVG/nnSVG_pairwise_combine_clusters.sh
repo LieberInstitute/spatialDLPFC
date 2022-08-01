abad3ddd38e50778c05b36de63d45e2718f6b562
abspangler13 <aspangle@gmail.com>
2022-08-01 14:41:42 -0400

    new scripts for running nnSVG by merging cluster pairs instead of subsetting object for cluster pairs

---
+++ code/analysis/13_nnSVG/nnSVG_pairwise_combine_clusters.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#$ -cwd
+#$ -l caracol,mem_free=100G,h_vmem=100G,h_fsize=100G
+#$ -N nnSVG_pairwise_combine
+#$ -o logs/nnSVG_pairwise_combine_clusters.$TASK_ID.txt
+#$ -e logs/nnSVG_pairwise_combine_clusters.$TASK_ID.txt
+#$ -m e
+
+
+
+USAGE_CUTOFF=10
+NUM_GPUS=1
+
+avail_gpus=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | cut -d " " -f 1 | awk -v usage="$USAGE_CUTOFF" '$1 < usage {print NR - 1}')
+
+#  Simply exit with an error if there are no GPUs left
+if [[ -z $avail_gpus ]]; then
+    echo "No GPUs are available."
+    exit 1
+fi
+
+export CUDA_VISIBLE_DEVICES=$(echo "$avail_gpus" | head -n $NUM_GPUS | paste -sd ",")
+
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/devel
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript nnSVG_pairwise_combine_clusters.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
