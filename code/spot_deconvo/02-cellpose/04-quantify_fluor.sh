eb1062f6884a6c1187229bf05ea0ea5795214236
Nick-Eagles <nick.eagles@libd.org>
2022-08-09 14:57:51 -0400

    Re-quantify flourescence after bug fixes and a better nucleus-area threshold

--- code/spot_deconvo/02-cellpose/04-quantify_fluor.sh
+++ code/spot_deconvo/02-cellpose/04-quantify_fluor.sh
@@ -4,8 +4,8 @@
 #$ -o ../../../processed-data/spot_deconvo/02-cellpose/04-quantify_fluor_$TASK_ID.log
 #$ -e ../../../processed-data/spot_deconvo/02-cellpose/04-quantify_fluor_$TASK_ID.log
 #$ -l mf=20G,h_vmem=20G
-#$ -t 2-4
-#$ -tc 2
+#$ -t 1-4
+#$ -tc 4
 
 echo "**** Job starts ****"
 date
