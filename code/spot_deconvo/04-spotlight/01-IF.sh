7474a407c590c024392fc2bf0c36495b892159be
Nick-Eagles <nick.eagles@libd.org>
2022-08-22 15:57:28 -0400

    Add a shell script to run spotlight on IF samples

---
+++ code/spot_deconvo/04-spotlight/01-IF.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -cwd
+#$ -N "IF"
+#$ -o ../../../processed-data/spot_deconvo/04-spotlight/01-IF.log
+#$ -e ../../../processed-data/spot_deconvo/04-spotlight/01-IF.log
+#$ -l mf=20G,h_vmem=20G,h_fsize=50G
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+module load conda_R/devel
+Rscript 01-IF.R
+
+echo "**** Job ends ****"
+date
