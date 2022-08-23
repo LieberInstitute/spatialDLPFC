51d22a24ef3fb9a97884247f645c852584bedd53
Heena Divecha <heenadivecha@eb-li-612uml85.win.ad.jhu.edu>
2022-08-23 16:11:22 -0400

    Edited the script to deploy pseudo_bulk iSEE app for k=16

--- code/iSEE_apps_pseudobulk/k=16/deploy.R
+++ code/iSEE_apps_pseudobulk/k=16/deploy.R
@@ -5,8 +5,8 @@ source("token.R")
 
 options(repos = BiocManager::repositories())
 rsconnect::deployApp(
-    appFiles = c("app.R", "spe_pseudobulk_bayesSpace_normalized_filtered_region_k9.RDS", "initial.R"),
-    appName = "Spangler2022_pseudobulk_cluster_k09",
+    appFiles = c("app.R", "spe_pseudobulk_bayesSpace_normalized_filtered_region_k16.RDS", "initial.R"),
+    appName = "Spangler2022_pseudobulk_cluster_k16",
     account = "libd",
     server = "shinyapps.io"
 )
