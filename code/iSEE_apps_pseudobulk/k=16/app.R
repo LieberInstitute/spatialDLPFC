e901720b11a25b81e646303e546edef3c2fb6a79
Heena Divecha <heenadivecha@eb-li-612uml85.win.ad.jhu.edu>
2022-08-23 15:52:47 -0400

    Adopted the script from K=9 (https://github.com/LieberInstitute/spatialDLPFC/tree/main/code/iSEE_apps_pseudobulk/k%3D9)

---
+++ code/iSEE_apps_pseudobulk/k=16/app.R
@@ -0,0 +1,53 @@
+
+library("SingleCellExperiment")
+library("iSEE")
+library("shiny")
+library("paletteer")
+library("scuttle")
+library("SpatialExperiment")
+
+#load("sce_for_iSEE_LS.rda", verbose = TRUE)# load the pseudobulked object spe_pseudo
+spe_pseudo <- readRDS("spe_pseudobulk_bayesSpace_normalized_filtered_region_k9.RDS")
+
+## Make unique gene names
+rownames(spe_pseudo) <-
+  uniquifyFeatureNames(rowData(spe_pseudo)$gene_id, rowData(spe_pseudo)$gene_name)
+
+#stopifnot(all(unique(spe_pseudo$BayesSpace) %in% names(cell_cols.clean)))
+
+## Don't run this on app.R since we don't want to run this every single time
+# lobstr::obj_size(spe_pseudo)
+# 876.33 MB
+
+source("initial.R", print.eval = TRUE)
+
+## From https://github.com/LieberInstitute/10xPilot_snRNAseq-human/blob/810b47364af4c8afe426bd2a6b559bd6a9f1cc98/shiny_apps/tran2021_AMY/app.R#L10-L14
+## Related to https://github.com/iSEE/iSEE/issues/568
+colData(spe_pseudo) <- cbind(
+  colData(spe_pseudo)[, !colnames(colData(spe_pseudo)) %in% c("subject", "BayesSpace")],
+  colData(spe_pseudo)[, c("BayesSpace", "subject")]
+)
+
+spe_pseudo$subject <- as.factor(spe_pseudo$subject)
+
+spe_pseudo <- registerAppOptions(spe_pseudo, color.maxlevels = length(colData(spe_pseudo)$BayesSpace_colors))
+
+iSEE(
+    spe_pseudo,
+    appTitle = "Spangler2022_pseudobulk_cluster_k09",
+     initial = initial,
+     colormap = ExperimentColorMap(colData = list(
+        # subject = function(n) {
+        #     cols <- paletteer::paletteer_d(
+        #         palette = "RColorBrewer::Dark2",
+        #         n = length(unique(spe_pseudo$subject))
+        #     )
+        #     cols <- as.vector(cols)
+        #     names(cols) <- levels(spe_pseudo$subject)
+        #     return(cols)
+        # },
+    BayesSpace = function(n) {
+        return(colData(spe_pseudo)$BayesSpace_colors)
+    }
+    ))
+)
---
+++ code/iSEE_apps_pseudobulk/k=16/initial.R
@@ -0,0 +1,96 @@
+initial <- list()
+
+################################################################################
+# Settings for Reduced dimension plot 1
+################################################################################
+
+initial[["ReducedDimensionPlot1"]] <- new("ReducedDimensionPlot", Type = "PCA", XAxis = 1L, YAxis = 2L,
+    FacetRowByColData = "BayesSpace", FacetColumnByColData = "BayesSpace",
+    ColorByColumnData = "BayesSpace", ColorByFeatureNameAssay = "logcounts",
+    ColorBySampleNameColor = "#FF0000", ShapeByColumnData = "BayesSpace",
+    SizeByColumnData = "age", FacetRowBy = "None", FacetColumnBy = "None",
+    ColorBy = "Column data", ColorByDefaultColor = "#000000",
+    ColorByFeatureName = "LINC01128", ColorByFeatureSource = "---",
+    ColorByFeatureDynamicSource = FALSE, ColorBySampleName = "Br2720_ant_1",
+    ColorBySampleSource = "---", ColorBySampleDynamicSource = FALSE,
+    ShapeBy = "None", SizeBy = "None", SelectionAlpha = 0.1,
+    ZoomData = numeric(0), BrushData = list(), VisualBoxOpen = FALSE,
+    VisualChoices = "Color", ContourAdd = FALSE, ContourColor = "#0000FF",
+    PointSize = 1, PointAlpha = 1, Downsample = FALSE, DownsampleResolution = 200,
+    CustomLabels = FALSE, CustomLabelsText = "Br2720_ant_1",
+    FontSize = 1, LegendPointSize = 1, LegendPosition = "Bottom",
+    HoverInfo = TRUE, LabelCenters = FALSE, LabelCentersBy = "BayesSpace",
+    LabelCentersColor = "#000000", VersionInfo = list(iSEE = structure(list(
+        c(2L, 6L, 0L)), class = c("package_version", "numeric_version"
+    ))), PanelId = c(ReducedDimensionPlot = 1L), PanelHeight = 600L,
+    PanelWidth = 5L, SelectionBoxOpen = FALSE, RowSelectionSource = "---",
+    ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE,
+    ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE,
+    ColumnSelectionRestrict = FALSE, SelectionHistory = list())
+
+################################################################################
+# Settings for Complex heatmap 1
+################################################################################
+
+initial[["ComplexHeatmapPlot1"]] <- new("ComplexHeatmapPlot", Assay = "logcounts", CustomRows = TRUE,
+    CustomRowsText = "SNAP25\nAQP4\nGAD1\nSLC17A6\nSLC32A1\nRELN\nMBP\nGFAP",
+    ClusterRows = FALSE, ClusterRowsDistance = "spearman", ClusterRowsMethod = "ward.D2",
+    DataBoxOpen = FALSE, VisualChoices = "Annotations", ColumnData = c("BayesSpace",
+    "subject"), RowData = character(0), CustomBounds = FALSE,
+    LowerBound = NA_real_, UpperBound = NA_real_, AssayCenterRows = FALSE,
+    AssayScaleRows = FALSE, DivergentColormap = "purple < black < yellow",
+    ShowDimNames = "Rows", LegendPosition = "Right", LegendDirection = "Vertical",
+    VisualBoxOpen = FALSE, NamesRowFontSize = 10, NamesColumnFontSize = 10,
+    ShowColumnSelection = TRUE, OrderColumnSelection = TRUE,
+    VersionInfo = list(iSEE = structure(list(c(2L, 6L, 0L)), class = c("package_version",
+    "numeric_version"))), PanelId = c(ComplexHeatmapPlot = 1L),
+    PanelHeight = 600L, PanelWidth = 7L, SelectionBoxOpen = FALSE,
+    RowSelectionSource = "---", ColumnSelectionSource = "---",
+    RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE,
+    RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE,
+    SelectionHistory = list())
+
+################################################################################
+# Settings for Row data table 1
+################################################################################
+
+initial[["RowDataTable1"]] <- new("RowDataTable", Selected = "SNAP25", Search = "", SearchColumns = c("",
+"", "", "", "", "", "", ""), HiddenColumns = character(0), VersionInfo = list(
+    iSEE = structure(list(c(2L, 6L, 0L)), class = c("package_version",
+    "numeric_version"))), PanelId = c(RowDataTable = 1L), PanelHeight = 600L,
+    PanelWidth = 5L, SelectionBoxOpen = FALSE, RowSelectionSource = "---",
+    ColumnSelectionSource = "---", DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE,
+    ColumnSelectionDynamicSource = FALSE, RowSelectionRestrict = FALSE,
+    ColumnSelectionRestrict = FALSE, SelectionHistory = list())
+
+################################################################################
+# Settings for Feature assay plot 1
+################################################################################
+
+initial[["FeatureAssayPlot1"]] <- new("FeatureAssayPlot", Assay = "logcounts", XAxis = "Column data",
+    XAxisColumnData = "BayesSpace", XAxisFeatureName = "LINC01128",
+    XAxisFeatureSource = "---", XAxisFeatureDynamicSource = FALSE,
+    YAxisFeatureName = "SNAP25", YAxisFeatureSource = "RowDataTable1",
+    YAxisFeatureDynamicSource = TRUE, FacetRowByColData = "BayesSpace",
+    FacetColumnByColData = "BayesSpace", ColorByColumnData = "BayesSpace",
+    ColorByFeatureNameAssay = "logcounts", ColorBySampleNameColor = "#FF0000",
+    ShapeByColumnData = "BayesSpace", SizeByColumnData = "age",
+    FacetRowBy = "None", FacetColumnBy = "None", ColorBy = "Column data",
+    ColorByDefaultColor = "#000000", ColorByFeatureName = "LINC01128",
+    ColorByFeatureSource = "---", ColorByFeatureDynamicSource = FALSE,
+    ColorBySampleName = "Br2720_ant_1", ColorBySampleSource = "---",
+    ColorBySampleDynamicSource = FALSE, ShapeBy = "None", SizeBy = "None",
+    SelectionAlpha = 0.1, ZoomData = numeric(0), BrushData = list(),
+    VisualBoxOpen = FALSE, VisualChoices = "Color", ContourAdd = FALSE,
+    ContourColor = "#0000FF", PointSize = 1, PointAlpha = 1,
+    Downsample = FALSE, DownsampleResolution = 200, CustomLabels = FALSE,
+    CustomLabelsText = "Br2720_ant_1", FontSize = 1, LegendPointSize = 1,
+    LegendPosition = "Bottom", HoverInfo = TRUE, LabelCenters = FALSE,
+    LabelCentersBy = "BayesSpace", LabelCentersColor = "#000000",
+    VersionInfo = list(iSEE = structure(list(c(2L, 6L, 0L)), class = c("package_version",
+    "numeric_version"))), PanelId = c(FeatureAssayPlot = 1L),
+    PanelHeight = 600L, PanelWidth = 7L, SelectionBoxOpen = FALSE,
+    RowSelectionSource = "---", ColumnSelectionSource = "---",
+    DataBoxOpen = FALSE, RowSelectionDynamicSource = FALSE, ColumnSelectionDynamicSource = FALSE,
+    RowSelectionRestrict = FALSE, ColumnSelectionRestrict = FALSE,
+    SelectionHistory = list())
