7e19563fdf75e1cac8754d6e8fd5574852db57e1
Nick-Eagles <nick.eagles@libd.org>
2022-08-24 14:53:20 -0400

    Add a pair of scripts to run SPOTlight on nonIF data

---
+++ code/spot_deconvo/04-spotlight/02-nonIF.R
@@ -0,0 +1,278 @@
+library('ggplot2')
+library('ggcorrplot')
+library('scatterpie')
+library('SPOTlight')
+library('SingleCellExperiment')
+library('SpatialExperiment')
+library('scater')
+library('scran')
+library('here')
+library('NMF')
+library('sessioninfo')
+
+################################################################################
+#   Variable definitions
+################################################################################
+
+sce_in <- "/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/sce/sce_DLPFC.Rdata"
+spe_in <- here(
+    "processed-data", "rdata", "spe", "01_build_spe",
+    "spe_filtered_final_with_clusters.Rdata"
+)
+
+marker_path <- here(
+    "processed-data", "spot_deconvo", "01-tangram", "markers.txt"
+)
+marker_stats_path = here(
+    "processed-data", "spot_deconvo", "01-tangram", "marker_stats.rds"
+)
+
+plot_dir = here(
+    "plots", "spot_deconvo", "04-spotlight", "nonIF"
+)
+processed_dir = here(
+    "processed-data", "spot_deconvo", "04-spotlight", "nonIF"
+)
+
+#   Column names in colData(sce)
+cell_type_var = 'cellType_broad_hc'
+
+#   Column names in rowData(sce)
+ensembl_id_var = 'gene_id'
+symbol_var = 'gene_name'
+
+#   Column names in colData(spe)
+sample_var = 'sample_id'
+
+#   Used for downsampling single-cell object prior to training
+n_cells_per_type <- 100
+
+################################################################################
+#   Preprocessing
+################################################################################
+
+dir.create(plot_dir, recursive = TRUE, showWarnings = FALSE)
+dir.create(processed_dir, recursive = TRUE, showWarnings = FALSE)
+
+#   Load objects
+load(sce_in, verbose = TRUE)
+load(spe_in, verbose = TRUE)
+gc()
+
+rownames(sce) = rowData(sce)[[ensembl_id_var]]
+
+#   Drop rare cell types for single-cell data
+print("Distribution of cells to keep (FALSE) vs. drop (TRUE):")
+table(sce[[cell_type_var]] == "EndoMural")
+sce <- sce[, sce[[cell_type_var]] != "EndoMural"]
+
+dec <- modelGeneVar(sce)
+
+#   Plot gene-expression variance
+pdf(file.path(plot_dir, 'expr_variance.pdf'))
+plot(dec$mean, dec$total, xlab = "Mean log-expression", ylab = "Variance")
+curve(metadata(dec)$trend(x), col = "blue", add = TRUE)
+dev.off()
+
+#   Get the top 3000 highly variable genes.
+hvg <- getTopHVGs(dec, n = 3000)
+
+colLabels(sce) <- colData(sce)[[cell_type_var]]
+
+#   Read in markers found by Louise's method
+markers = readLines(marker_path)
+marker_stats = readRDS(marker_stats_path)
+stopifnot(all(markers %in% rownames(sce)))
+
+#   Filter out any mitochondrial/ribosomal genes
+genes = !grepl(
+    pattern = "^(RP[LS]|MT-)",
+    x = rowData(sce)[[symbol_var]]
+) & (rownames(sce) %in% markers)
+print(
+    paste(
+        length(which(genes)),
+        'markers used after filtering out mitochondrial/ribosomal genes.'
+    )
+)
+
+#   Score markers (determine weights) and form a data frame SPOTlight can use as
+#   input
+mgs <- scoreMarkers(sce, subset.row = genes)
+
+mgs_fil <- lapply(names(mgs), function(i) {
+    x <- mgs[[i]]
+    # Sort the genes from highest to lowest weight
+    x <- x[order(x$mean.AUC, decreasing = TRUE), ]
+    # Add gene and cluster id to the dataframe
+    x$gene <- rownames(x)
+    x$cluster <- i
+    
+    #   Only take genes that have already been determined to be markers for this
+    #   cell type
+    x = x[
+        marker_stats$cellType.target[
+            match(rownames(x), marker_stats$gene)
+        ] == i,
+    ]
+    
+    data.frame(x)
+})
+mgs_df <- do.call(rbind, mgs_fil)
+
+#   It's technically possible for the above 'lapply' to generate data frames
+#   with non-unique rownames, leading to R automatically changing some rownames
+#   and thus illegitimate gene names. Verify this most likely didn't happen
+stopifnot(all(rownames(mgs_df) %in% rownames(sce)))
+
+#-------------------------------------------------------------------------------
+
+# split cell indices by identity
+idx <- split(seq(ncol(sce)), sce[[cell_type_var]])
+
+#   This was slightly changed from the tutorial for simplicity
+cs_keep <- lapply(idx, function(i) sample(i, min(length(i), n_cells_per_type)))
+sce <- sce[, unlist(cs_keep)]
+
+################################################################################
+#   Train model and deconvolve cell types
+################################################################################
+
+res <- SPOTlight(
+    x = sce,
+    y = spe,
+    groups = as.character(sce[[cell_type_var]]),
+    mgs = mgs_df,
+    hvg = hvg,
+    weight_id = "mean.AUC",
+    group_id = "cluster",
+    gene_id = "gene"
+)
+
+saveRDS(res, file.path(processed_dir, 'results.rds'))
+
+################################################################################
+#   Visualization
+################################################################################
+
+#   Extract deconvolution matrix and NMF model fit
+mat <- res$mat
+mod <- res$NMF
+
+pdf(file.path(plot_dir, 'topic_profiles.pdf'))
+plotTopicProfiles(
+    x = mod,
+    y = sce[[cell_type_var]],
+    facet = FALSE,
+    min_prop = 0.01,
+    ncol = 1
+) +
+    theme(aspect.ratio = 1)
+
+plotTopicProfiles(
+    x = mod,
+    y = sce[[cell_type_var]],
+    facet = TRUE,
+    min_prop = 0.01,
+    ncol = 6
+)
+dev.off()
+
+sign <- basis(mod)
+colnames(sign) <- paste0("Topic", seq_len(ncol(sign)))
+
+pdf(file.path(plot_dir, 'visualizations.pdf'))
+plotCorrelationMatrix(mat)
+plotInteractions(mat, which = "heatmap", metric = "prop")
+plotInteractions(mat, which = "heatmap", metric = "jaccard")
+plotInteractions(mat, which = "network")
+dev.off()
+
+ct <- colnames(mat)
+mat[mat < 0.1] <- 0
+
+# Define color palette
+# (here we use 'paletteMartin' from the 'colorBlindness' package)
+paletteMartin <- c(
+    "#000000", "#004949", "#009292", "#ff6db6", "#ffb6db", 
+    "#490092", "#006ddb", "#b66dff", "#6db6ff", "#b6dbff", 
+    "#920000", "#924900", "#db6d00", "#24ff24", "#ffff6d"
+)
+
+pal <- colorRampPalette(paletteMartin)(length(ct))
+names(pal) <- ct
+
+spe$res_ss <- res[[2]][colnames(spe)]
+xy <- spatialCoords(spe)
+spe$x <- xy[, 1]
+spe$y <- xy[, 2]
+
+#   Split spatial-related plots by sample
+for (sample_id in unique(spe[[sample_var]])) {
+    #   Subset objects to this sample
+    this_sample_indices = which(spe[[sample_var]] == sample_id)
+    temp_spe = spe[, this_sample_indices]
+    temp_mat = mat[this_sample_indices,]
+    
+    #   Scatterpie
+    pdf(file.path(plot_dir, paste0('scatterpie_', sample_id, '.pdf')))
+    print(
+        plotSpatialScatterpie(
+            x = temp_spe,
+            y = temp_mat,
+            cell_types = colnames(temp_mat),
+            img = FALSE,
+            scatterpie_alpha = 1,
+            pie_scale = 0.4
+        ) +
+            scale_fill_manual(
+                values = pal,
+                breaks = names(pal)
+            )
+    )
+    dev.off()
+    
+    #   Residuals
+    pdf(file.path(plot_dir, paste0('residuals_', sample_id, '.pdf')))
+    print(
+        ggcells(temp_spe, aes(x, y, color = res_ss)) +
+            geom_point() +
+            scale_color_viridis_c() +
+            coord_fixed() +
+            theme_bw()
+    )
+    dev.off()
+}
+
+#   Save final objects
+saveRDS(sce, file.path(processed_dir, 'sce.rds'))
+saveRDS(spe, file.path(processed_dir, 'spe.rds'))
+
+################################################################################
+#   Export 'clusters.csv' file of cell counts
+################################################################################
+
+#   Create a data frame with cell counts for all samples, and add the 'key'
+#   column
+clusters = data.frame(res$mat)
+clusters$key = spe$key
+clusters = clusters[, c('key', as.character(unique(sce[[cell_type_var]])))]
+
+#   Write individual 'clusters.csv' files for each sample
+for (sample_id in unique(spe[[sample_var]])) {
+    clusters_small = clusters[spe[[sample_var]] == sample_id, ]
+    
+    #   Make sure processed directory exists for this sample
+    dir.create(
+        file.path(processed_dir, sample_id), recursive = TRUE,
+        showWarnings = FALSE
+    )
+    
+    write.csv(
+        clusters_small,
+        file.path(processed_dir, sample_id, 'clusters.csv'),
+        row.names = FALSE, quote = FALSE
+    )
+}
+
+session_info()
---
+++ code/spot_deconvo/04-spotlight/02-nonIF.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -cwd
+#$ -N "nonIF"
+#$ -o ../../../processed-data/spot_deconvo/04-spotlight/02-nonIF.log
+#$ -e ../../../processed-data/spot_deconvo/04-spotlight/02-nonIF.log
+#$ -l mf=20G,h_vmem=20G,h_fsize=50G
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+module load conda_R/devel
+Rscript 02-nonIF.R
+
+echo "**** Job ends ****"
+date
