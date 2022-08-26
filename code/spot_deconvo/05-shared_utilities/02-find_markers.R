6390dd545baa9ca3042774d46ca255276b0d6f1e
Nick-Eagles <nick.eagles@libd.org>
2022-08-26 16:58:40 -0400

    Add plots to check the quality of marker genes, and support a different number of marker genes for use in C2L vs. in other tools. Needs testing

--- code/spot_deconvo/05-shared_utilities/02-find_markers.R
+++ code/spot_deconvo/05-shared_utilities/02-find_markers.R
@@ -11,6 +11,7 @@ suppressPackageStartupMessages(library("here"))
 #   to need more markers than the other tools, motivating the exception below
 n_markers_per_type <- 25
 n_markers_per_type_c2l <- 100
+stopifnot(n_markers_per_type_c2l >= n_markers_per_type)
 
 #  Paths
 sce_in <- here(
@@ -28,57 +29,142 @@ marker_object_in <- here(
 )
 
 marker_out <- here(
-    "processed-data", "spot_deconvo", "01-tangram", "markers.txt"
+    "processed-data", "spot_deconvo", "markers.txt"
 )
 marker_c2l_out <- here(
-    "processed-data", "spot_deconvo", "01-tangram", "markers.txt"
+    "processed-data", "spot_deconvo", "markers.txt"
 )
 
+plot_dir <- here(
+    "plots", "spot_deconvo", "05-shared_utilities"
+)
+
+dir.create(plot_dir, recursive = TRUE, showWarnings = FALSE)
+
 ###############################################################################
-#  Find marker genes
+#  Load objects
 ###############################################################################
 
-#   Load objects
 sce = readRDS(sce_in)
 spe_IF <- readRDS(spe_IF_in)
 load(spe_nonIF_in, verbose = TRUE)
 spe_nonIF <- spe
 rm(spe)
+
 marker_stats <- readRDS(marker_object_in)
+
 gc()
 
-print("Writing markers...")
+###############################################################################
+#  Functions
+###############################################################################
 
-#   Take top N marker genes for each cell type
-marker_stats <- marker_stats %>%
-    filter(rank_ratio <= n_markers_per_type)
-
-markers_scratch <- marker_stats$gene
-
-#   It's technically possible to find a single gene that is used as a "marker"
-#   for two different cell types via this method. Verify this is not the case,
-#   because that would indicate the use of bad markers
-stopifnot(
-    length(unique(markers_scratch)) == n_markers_per_type * length(unique(marker_stats$cellType.target))
-)
-
-#   All the marker genes are present in the single-cell data (sanity check), but
-#   one marker is in neither the IF nor non-IF spatial data
-stopifnot(all(markers_scratch %in% rowData(sce)$gene_id))
-
-start_len <- length(markers_scratch)
-markers_scratch <- markers_scratch[
-    (markers_scratch %in% rowData(spe_IF)$gene_id) &
-        (markers_scratch %in% rowData(spe_nonIF)$gene_id)
-]
-print(
-    paste(
-        "Dropped", start_len - length(markers_scratch),
-        "markers, which were not in the spatial data."
+write_markers = function(n_markers, out_path) {
+    #   Take top N marker genes for each cell type
+    marker_stats_temp <- marker_stats %>%
+        filter(rank_ratio <= n_markers)
+    
+    markers_scratch <- marker_stats_temp$gene
+    
+    #   It's technically possible to find a single gene that is used as a
+    #   "marker" for two different cell types via this method. Verify this is
+    #   not the case, because that would indicate the use of bad markers
+    stopifnot(
+        length(unique(markers_scratch)) == n_markers_per_type * length(unique(marker_stats_temp$cellType.target))
     )
-)
+    
+    #   All the marker genes are present in the single-cell data (sanity check),
+    #   but some markers might be in neither the IF nor non-IF spatial data
+    stopifnot(all(markers_scratch %in% rowData(sce)$gene_id))
+    
+    start_len <- length(markers_scratch)
+    markers_scratch <- markers_scratch[
+        (markers_scratch %in% rowData(spe_IF)$gene_id) &
+            (markers_scratch %in% rowData(spe_nonIF)$gene_id)
+    ]
+    print(
+        paste(
+            "Dropped", start_len - length(markers_scratch),
+            "markers, which were not in the spatial data."
+        )
+    )
+    
+    #   Write list of markers
+    writeLines(markers_scratch, con = out_path)
+}
 
-#   Write list of markers
-writeLines(markers_scratch, con = marker_out)
+plot_markers = function(sce, marker_stats, n_genes, plot_name) {
+    p <- plot_marker_express(
+        sce,
+        marker_stats,
+        cell_type = ct,
+        n_genes = n_genes,
+        rank_col = "rank_ratio",
+        anno_col = "anno_ratio",
+        cellType_col = "cellType_broad_hc"
+    )
+    ggsave(
+        p,
+        filename = file.path(
+            plot_dir,
+            paste0(plot_name, "_", ct, ".png")
+        ),
+        height = 10, width = 10
+    )
+}
+
+###############################################################################
+#  Subset and write markers
+###############################################################################
+
+print("Writing markers for tangram/SPOTlight...")
+write_markers(n_markers_per_type, marker_out)
+
+print("Writing markers for cell2location...")
+write_markers(n_markers_per_type_c2l, marker_c2l_out)
+
+###############################################################################
+#  Visually check quality of markers
+###############################################################################
+
+#   Visually show how markers look for each cell type. In particular, look at
+#   the best 5 markers, the worst 5 markers used for C2L, and the worst 5
+#   markers used for tangram/SPOTlight
+walk(
+    levels(marker_stats$cellType.target),
+    function(ct){
+        #   First plot the top 5 markers
+        plot_markers(sce, marker_stats, 5, "top5_marker_genes")
+        
+        #   Next, plot worst 5 C2L markers: those with ranks in
+        #   [n_markers_per_type_c2l - 4, n_markers_per_type_c2l]
+        rank_range = paste(
+            as.character(n_markers_per_type_c2l - 4),
+            as.character(n_markers_per_type_c2l),
+            sep = "-"
+        )
+        marker_stats_temp = marker_stats %>%
+            filter(rank_ratio <= n_markers_per_type_c2l) %>%
+            mutate(rank_ratio, ~ 1 + n_markers_per_type_c2l - rank_ratio)
+        plot_markers(
+            sce, marker_stats_temp,
+            paste0("marker_genes_", rank_range)
+        )
+        
+        #   Finally, plot worst 5 non-C2L markers
+        rank_range = paste(
+            as.character(n_markers_per_type - 4),
+            as.character(n_markers_per_type),
+            sep = "-"
+        )
+        marker_stats_temp = marker_stats %>%
+            filter(rank_ratio <= n_markers_per_type) %>%
+            mutate(rank_ratio, ~ 1 + n_markers_per_type - rank_ratio)
+        plot_markers(
+            sce, marker_stats_temp,
+            paste0("marker_genes_", rank_range)
+        )
+    }
+)
 
 session_info()
