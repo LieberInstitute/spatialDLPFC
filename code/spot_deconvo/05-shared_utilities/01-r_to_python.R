d49e4ff9dbc588de7a890e9035e3bb8f81b87d59
Nick-Eagles <nick.eagles@libd.org>
2022-09-02 11:29:11 -0400

    Rank/find markers only after filtering out genes not present in all the spatial data

--- code/spot_deconvo/05-shared_utilities/01-r_to_python.R
+++ code/spot_deconvo/05-shared_utilities/01-r_to_python.R
@@ -161,6 +161,19 @@ writeLines(unique(spe_nonIF$sample_id), con = sample_nonIF_out)
 #   Rank marker genes
 #-------------------------------------------------------------------------------
 
+#   We won't consider genes that aren't in both the spatial objects
+keep = (rowData(sce)$gene_id %in% rowData(spe_IF)$gene_id) &
+    (rowData(sce)$gene_id %in% rowData(spe_nonIF)$gene_id)
+sce = sce[keep,]
+
+perc_keep = 100 * (1 - length(which(keep)) / length(keep))
+print(
+    paste0(
+        "Dropped ", round(perc_keep, 1), "% of potential marker genes ",
+        "that were not present in all the spatial data"
+    )
+)
+
 print('Running getMeanRatio2 and findMarkers_1vAll to rank genes as markers...')
 marker_stats <- get_mean_ratio2(
     sce, cellType_col = "cellType_broad_hc", assay_name = "logcounts"
--- code/spot_deconvo/05-shared_utilities/02-find_markers.R
+++ code/spot_deconvo/05-shared_utilities/02-find_markers.R
@@ -1,7 +1,6 @@
 #   Find marker genes that will be shared for both the IF and non-IF analyses.
 
 suppressPackageStartupMessages(library("SingleCellExperiment"))
-suppressPackageStartupMessages(library("SpatialExperiment"))
 suppressPackageStartupMessages(library("DeconvoBuddies"))
 suppressPackageStartupMessages(library("tidyverse"))
 suppressPackageStartupMessages(library("sessioninfo"))
@@ -16,13 +15,6 @@ stopifnot(n_markers_per_type_c2l >= n_markers_per_type)
 #  Paths
 sce_in <- here(
     "processed-data", "spot_deconvo", "sce.rds"
-)
-spe_IF_in <- here(
-    "processed-data", "rdata", "spe_IF", "01_build_spe_IF", "spe.rds"
-)
-spe_nonIF_in <- here(
-    "processed-data", "rdata", "spe", "01_build_spe",
-    "spe_filtered_final_with_clusters.Rdata"
 )
 marker_object_in <- here(
     "processed-data", "spot_deconvo", "marker_stats.rds"
@@ -46,13 +38,7 @@ dir.create(plot_dir, recursive = TRUE, showWarnings = FALSE)
 ###############################################################################
 
 sce = readRDS(sce_in)
-spe_IF <- readRDS(spe_IF_in)
-load(spe_nonIF_in, verbose = TRUE)
-spe_nonIF <- spe
-rm(spe)
-
 marker_stats <- readRDS(marker_object_in)
-
 gc()
 
 ###############################################################################
@@ -71,22 +57,6 @@ write_markers = function(n_markers, out_path) {
     #   not the case, because that would indicate the use of bad markers
     stopifnot(
         length(unique(markers_scratch)) == n_markers * length(unique(marker_stats_temp$cellType.target))
-    )
-    
-    #   All the marker genes are present in the single-cell data (sanity check),
-    #   but some markers might be in neither the IF nor non-IF spatial data
-    stopifnot(all(markers_scratch %in% rowData(sce)$gene_id))
-    
-    start_len <- length(markers_scratch)
-    markers_scratch <- markers_scratch[
-        (markers_scratch %in% rowData(spe_IF)$gene_id) &
-            (markers_scratch %in% rowData(spe_nonIF)$gene_id)
-    ]
-    print(
-        paste(
-            "Dropped", start_len - length(markers_scratch),
-            "markers, which were not in the spatial data."
-        )
     )
     
     #   Write list of markers
