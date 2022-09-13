b1af6a7f84ba3f80085fc0fa985c7fcea5dc49f4
lahuuki <lahuuki@gmail.com>
2022-09-13 16:23:33 -0400

    Add sh files for compute_registration_stats #122

---
+++ code/analysis/14_spatial_registration_PEC/02_compute_registration_stats_IsoHuB.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=25G,h_vmem=25G,h_fsize=100G
+#$ -N compute_registration_stats_IsoHuB
+#$ -o logs/02_compute_registration_stats_IsoHuB.txt
+#$ -e logs/02_compute_registration_stats_IsoHuB.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/4.2
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 02_compute_registration_stats.R IsoHuB
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
