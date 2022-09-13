a9670e4b4e79325155f347a44bc9b0ae221bd8a4
lahuuki <lahuuki@gmail.com>
2022-09-13 16:12:51 -0400

    Use code from registration_wrapper to compute stats #122

--- code/analysis/14_spatial_registration_PEC/02_compute_registration_stats.R
+++ code/analysis/14_spatial_registration_PEC/02_compute_registration_stats.R
@@ -8,48 +8,78 @@ library("sessioninfo")
 #### load dataset  ####
 args <- commandArgs(trailingOnly = TRUE)
 dataset <- args[1]
-message("Running -", dataset)
-filepath = here("raw-data","psychENCODE","version2",dataset, paste0(dataset, "-snRNAseq_annotated.h5ad"))
-stopifnot(file.exists(filepath))
+message("Running - ", dataset)
 
-message(Sys.time(), " - Reading data from: ", filepath)
-sce <- readH5AD(file = here("raw-data/psychENCODE/version2/DevBrain/DevBrain-snRNAseq_annotated.h5ad"))
+sce_pseudo <- readRDS(file = here("processed-data","rdata","spe","14_spatial_registration_PEC",
+                 paste0("pseudobulk_", dataset, ".rds")))
 
 message("\nSCE Dimesions:")
-dim(sce)
+dim(sce_pseudo)
+
+min_ncells <- 10 
+message(
+  Sys.time(),
+  " dropping ",
+  sum(sce_pseudo$ncells < min_ncells),
+  " pseudo-bulked samples that are below 'min_ncells'."
+)
+sce_pseudo <- sce_pseudo[, sce_pseudo$ncells >= min_ncells]
 
 message("Cell Types:")
 ## must be syntactically valid
-colData(sce)$cellType <- as.factor(make.names(colData(sce)$subclass))
-table(sce$cellType)
+table(sce_pseudo$cellType)
+## Drop Levels
+sce_pseudo$cellType <- droplevels(sce_pseudo$cellType)
 
-# identify annotation/cluster labels
-rowData(sce)$gene_name <- rownames(sce) #save gene name as column of rowData
-rownames(sce) <- rowData(sce)$featureid # have to make row names of object the ensembl id instead of gene names
+#### Run models ####
+registration_mod <- registration_model(sce_pseudo)
+block_cor <- registration_block_cor(sce_pseudo, registration_model = registration_mod)
 
-## Logcounts 
-# default “X” contain the log-normalized counts
-names(assays(sce)) <- "counts" ## this is wrong --- try for now
+gene_name = "gene_name"
+gene_ensembl = "featureid"
 
-registration_stats <- registration_wrapper(sce,
-                                     var_registration = "cellType",
-                                     var_sample_id = "individualID",
-                                     covars = NULL,
-                                     gene_ensembl = "featureid",
-                                     gene_name = "gene_name",
-                                     prefix = "",
-                                     min_ncells = 10)
+results_enrichment <-
+  registration_stats_enrichment(
+    sce_pseudo,
+    block_cor = block_cor,
+    gene_ensembl = gene_ensembl,
+    gene_name = gene_name
+  )
+
+results_pairwise <-
+  registration_stats_pairwise(
+    sce_pseudo,
+    registration_model = registration_mod,
+    gene_ensembl = gene_ensembl,
+    gene_name = gene_name
+  )
+
+results_anova <-
+  registration_stats_anova(
+    sce_pseudo,
+    block_cor = block_cor,
+    gene_ensembl = gene_ensembl,
+    gene_name = gene_name,
+    prefix = prefix
+  )
+
+modeling_results <- list(
+  "anova" = results_anova,
+  "enrichment" = results_enrichment,
+  "pairwise" = results_pairwise
+)
 
 ## Save results
-saveRDS(registration_stats,
+saveRDS(modeling_results,
         file = here("processed-data","rdata","spe","14_spatial_registration_PEC",
                     paste0("registration_stats_",dataset,".rds")))
 
-# sgejobs::job_single('01_compute_registration_stats_DevBrain', create_shell = TRUE, memory = '25G', command = "Rscript 01_compute_registration_stats.R DevBrain")
-# sgejobs::job_single('01_compute_registration_stats_SZBD', create_shell = TRUE, memory = '25G', command = "Rscript 01_compute_registration_stats.R SZBD")
-# sgejobs::job_single('01_compute_registration_stats_CMC', create_shell = TRUE, memory = '25G', command = "Rscript 01_compute_registration_stats.R CMC")
-# sgejobs::job_single('01_compute_registration_stats_IsoHUB', create_shell = TRUE, memory = '25G', command = "Rscript 01_compute_registration_stats.R IsoHUB")
-# sgejobs::job_single('01_compute_registration_stats_UCLA', create_shell = TRUE, memory = '25G', command = "Rscript 01_compute_registration_stats.R UCLA")
+# sgejobs::job_single('02_compute_registration_stats_DevBrain', create_shell = TRUE, memory = '25G', command = "Rscript 02_compute_registration_stats.R DevBrain")
+# sgejobs::job_single('02_compute_registration_stats_CMC', create_shell = TRUE, memory = '25G', command = "Rscript 02_compute_registration_stats.R CMC")
+# sgejobs::job_single('02_compute_registration_stats_IsoHuB', create_shell = TRUE, memory = '25G', command = "Rscript 02_compute_registration_stats.R IsoHuB")
+# sgejobs::job_single('02_compute_registration_stats_UCLA', create_shell = TRUE, memory = '25G', command = "Rscript 02_compute_registration_stats.R UCLA-ASD")
+
+# sgejobs::job_single('02_compute_registration_stats_SZBD', create_shell = TRUE, memory = '25G', command = "Rscript 02_compute_registration_stats.R SZBDMulti")
 
 ## Reproducibility information
 print("Reproducibility information:")
