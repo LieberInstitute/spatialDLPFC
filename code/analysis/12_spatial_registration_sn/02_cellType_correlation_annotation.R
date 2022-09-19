56edc80cb4e96b7afd2ee4e17af93bcefff7ea51
lahuuki <lahuuki@gmail.com>
2022-09-19 17:23:10 -0400

    Move layer fix function to utils

--- code/analysis/12_spatial_registration_sn/02_cellType_correlation_annotation.R
+++ code/analysis/12_spatial_registration_sn/02_cellType_correlation_annotation.R
@@ -171,25 +171,7 @@ dev.off()
 
 
 #### Add Layer Annotations to colData ####
-
-## Polish annotations
-fix_layer_order <- function(l) {
-    star <- ifelse(grepl("\\*", l), "*", "")
-    l <- gsub("L|\\*", "", l)
-    l <- sort(unlist(strsplit(l, "/")))
-
-    if (all(l == "WM")) {
-        return(paste0("WM", star))
-    }
-
-    l[[1]] <- paste0("L", l[[1]])
-    if ("WM" %in% l) l <- c("WM", l[l != "WM"])
-    fix <- paste0(paste0(l, collapse = "/"), star)
-
-    return(fix)
-}
-
-fix_layer_order2 <- Vectorize(fix_layer_order)
+source(here("code","analysis", "12_spatial_registration_sn","utils.R"))
 
 ## layer_annotation is the reordered layer label - removes detail from the ordering process but helps group
 layer_anno2 <- layer_anno |>
---
+++ code/analysis/12_spatial_registration_sn/utils.R
@@ -0,0 +1,18 @@
+## Polish annotations
+fix_layer_order <- function(l) {
+  star <- ifelse(grepl("\\*", l), "*", "")
+  l <- gsub("L|\\*", "", l)
+  l <- sort(unlist(strsplit(l, "/")))
+  
+  if (all(l == "WM")) {
+    return(paste0("WM", star))
+  }
+  
+  l[[1]] <- paste0("L", l[[1]])
+  if ("WM" %in% l) l <- c("WM", l[l != "WM"])
+  fix <- paste0(paste0(l, collapse = "/"), star)
+  
+  return(fix)
+}
+
+fix_layer_order2 <- Vectorize(fix_layer_order)
