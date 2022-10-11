6039f184386fe7a626a4a9069758eef73741de01
lahuuki <lahuuki@gmail.com>
2022-10-11 16:35:50 -0400

    Clean up script, do all cor and annotation at once, save all annotations

--- code/analysis/12_spatial_registration_sn/02_cellType_correlation_annotation.R
+++ code/analysis/12_spatial_registration_sn/02_cellType_correlation_annotation.R
@@ -1,9 +1,12 @@
 
 library("SpatialExperiment")
 library("spatialLIBD")
-library("dplyr")
+library("tidyverse")
 library("here")
 library("sessioninfo")
+
+## Set up plotting
+plot_dir <- here("plots", "12_spatial_registration_sn")
 
 ## Load Registration Results
 load(here("processed-data", "rdata", "spe", "12_spatial_registration_sn", "sn_hc_registration.RDS"), verbose = TRUE)
@@ -14,22 +17,48 @@ colnames(registration_t_stats) <- gsub("^t_stat_", "", colnames(registration_t_s
 
 #### Calculate Correlation Matrix ####
 ## get layer data
-modeling_results <- fetch_data(type = "modeling_results")
+#### Load Layer and k Data  ####
+layer_modeling_results <- fetch_data(type = "modeling_results")
 
-## Correlate t-stats vs. manual layer annotations for top 100 genes
-cor_top100 <- layer_stat_cor(
-    registration_t_stats,
-    modeling_results,
-    model_type = "enrichment",
-    reverse = FALSE,
-    top_n = 100
-)
+paths <- list(k9 = "parsed_modeling_results_k9.Rdata", k16 = "parsed_modeling_results_k16.Rdata")
+
+modeling_results <- lapply(paths, function(x) 
+  get(load(here("processed-data","rdata","spe","08_layer_differential_expression",x))))
+
+modeling_results <- c(list(layer = layer_modeling_results), modeling_results)
+names(modeling_results)
+
+#### Correlate with modeling results ####
+cor_top100 <- map(modeling_results, ~layer_stat_cor(registration_t_stats,
+                                                    .x,
+                                                    model_type = "enrichment",
+                                                    reverse = FALSE,
+                                                    top_n = 100))
+  
+## Plot
+pdf(here(plot_dir, paste0("spatial_registration_plot_sn.pdf")))
+map(cor_top100, layer_stat_cor_plot)
+dev.off()
+
+#### Annotate Layers ####
+layer_anno <- map2(cor_top100, names(cor_top100), function(cor, name){
+  anno <- annotate_registered_clusters(cor_stats_layer = cor,
+                                       confidence_threshold = 0.25,
+                                       cutoff_merge_ratio = 0.25)
+  colnames(anno) <- gsub("layer",name, colnames(anno))
+  return(anno)
+})
+
+layer_anno_all <- reduce(layer_anno, left_join, by = "cluster")
+
+
+modeling_results_pilot <- fetch_data(type = "modeling_results_pilot")
 
 ## Save correlation matrix
 save(cor_top100, file = here("processed-data", "rdata", "spe", "12_spatial_registration_sn", "sn_hc_cor_top100.RDS"))
 
 ##  Plot layer correlation
-plot_dir <- here("plots", "12_spatial_registration_sn")
+
 
 pdf(here(plot_dir, "spatial_registration_plot_sn_v_manual_top100.pdf"))
 layer_stat_cor_plot(cor_top100, max = max(cor_top100))
@@ -44,37 +73,37 @@ layer_anno <- annotate_registered_clusters(
     cutoff_merge_ratio = 0.25
 )
 
-layer_anno[order(layer_anno$cluster), ]
+layer_anno$layer |> arrange(cluster)
 #         cluster layer_confidence layer_label
-# 3         Astro             good          L1
-# 4  EndoMural_01             good       L1/WM
-# 5  EndoMural_02             good          L1
-# 8      Excit_01             good          L3
-# 17     Excit_02             good        L5/6
-# 13     Excit_03             good          L4
-# 16     Excit_04             good          L5
-# 9      Excit_05             good          L3
-# 19     Excit_06             good          L6
-# 18     Excit_07             good          L5
-# 20     Excit_08             good          L6
-# 14     Excit_09             good      L4/3/5
-# 15     Excit_10             good          L4
-# 11     Excit_11             good      L4/5/3
-# 12     Excit_12             poor       L4/5*
-# 22     Excit_13             poor         L4*
-# 27     Excit_14             good        L3/2
-# 21     Excit_15             poor         L1*
-# 24     Inhib_01             good          L2
-# 10     Inhib_02             good          L4
-# 28     Inhib_03             poor       L4/3*
-# 25     Inhib_04             poor         L2*
-# 26     Inhib_05             good          L2
-# 29     Inhib_06             poor         L2*
-# 6         Micro             good       WM/L1
-# 23     Oligo_01             poor       L3/4*
-# 1      Oligo_02             good          WM
-# 2      Oligo_03             good          WM
-# 7           OPC             good          WM
+# 1         Astro             good          L1
+# 2  EndoMural_01             good       L1/WM
+# 3  EndoMural_02             good          L1
+# 4      Excit_01             good          L3
+# 5      Excit_02             good        L5/6
+# 6      Excit_03             good          L4
+# 7      Excit_04             good          L5
+# 8      Excit_05             good          L3
+# 9      Excit_06             good          L6
+# 10     Excit_07             good          L5
+# 11     Excit_08             good          L6
+# 12     Excit_09             good      L4/3/5
+# 13     Excit_10             good          L4
+# 14     Excit_11             good      L4/5/3
+# 15     Excit_12             poor       L4/5*
+# 16     Excit_13             poor         L4*
+# 17     Excit_14             good        L3/2
+# 18     Excit_15             poor         L1*
+# 19     Inhib_01             good          L2
+# 20     Inhib_02             good          L4
+# 21     Inhib_03             poor       L4/3*
+# 22     Inhib_04             poor         L2*
+# 23     Inhib_05             good          L2
+# 24     Inhib_06             poor         L2*
+# 25        Micro             good       WM/L1
+# 26     Oligo_01             poor       L3/4*
+# 27     Oligo_02             good          WM
+# 28     Oligo_03             good          WM
+# 29          OPC             good          WM
 
 layer_anno |>
     filter(grepl("Excit", cluster), layer_confidence == "good") |>
@@ -89,49 +118,6 @@ layer_anno |>
 # 7        L5/6 1
 # 8          L6 2
 
-## compare to old results
-old_anno <- read.csv("/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/05_explore_sce/DLPFC_HC_layer_annotation.csv")
-
-old_anno <- old_anno |> select(cluster = cellType_hc, layer_label_old = layer_label)
-
-layer_compare <- layer_anno |> left_join(old_anno)
-
-layer_compare |> filter(grepl("Excit", cluster), layer_label == layer_label_old)
-#    cluster layer_confidence layer_label layer_label_old
-# 1 Excit_05             good          L3              L3
-# 2 Excit_04             good          L5              L5
-# 3 Excit_02             good        L5/6            L5/6
-# 4 Excit_07             good          L5              L5
-# 5 Excit_06             good          L6              L6
-# 6 Excit_08             good          L6              L6
-
-layer_compare |> filter(grepl("Excit", cluster), layer_label != layer_label_old)
-#    cluster layer_confidence layer_label layer_label_old
-# 1 Excit_01             good          L3            L3/2
-# 2 Excit_11             good      L4/5/3              L4
-# 3 Excit_12             poor       L4/5*             L4*
-# 4 Excit_03             good          L4            L4/5
-# 5 Excit_09             good      L4/3/5            L5/4
-# 6 Excit_10             good          L4            L4/5
-# 7 Excit_15             poor         L1*             L4*
-# 8 Excit_13             poor         L4*         L4/3/5*
-# 9 Excit_14             good        L3/2           L3/2*
-
-layer_compare |> filter(grepl("Excit", cluster), layer_label != layer_label_old, layer_confidence == "good")
-#    cluster layer_confidence layer_label layer_label_old
-# 1 Excit_01             good          L3            L3/2
-# 2 Excit_11             good      L4/5/3              L4
-# 3 Excit_03             good          L4            L4/5
-# 4 Excit_09             good      L4/3/5            L5/4
-# 5 Excit_10             good          L4            L4/5
-# 6 Excit_14             good        L3/2           L3/2*
-
-layer_compare |>
-    filter(grepl("Excit", cluster), layer_confidence == "good") |>
-    arrange(layer_label) |>
-    mutate(change = layer_label != layer_label_old)
-
-
 ##  Mess with annotation plot
 layer_order <- layer_anno |>
     filter(grepl("Excit", cluster), layer_confidence == "good") |>
@@ -144,22 +130,23 @@ pdf(here(plot_dir, "spatial_annotation_plot_sn_v_manual_top100.pdf"))
 layer_stat_cor_plot(cor_temp, max = max(cor_temp))
 dev.off()
 
+## Create Long Format
+layers_long <- layer_anno |>
+  mutate(layers = strsplit(layer_label, "/")) |>
+  unnest_longer(layers) |>
+  mutate(
+    layers = gsub("\\*", "", gsub("^([1-9])", "L\\1", layers)),
+    Annotation = "layer")
+
 ## make annotation grid
-anno_matrix <- layer_anno |>
-    mutate(layers = strsplit(layer_label, "/")) |>
-    unnest_longer(layers) |>
-    mutate(
-        layers = gsub("\\*", "", gsub("^([1-9])", "L\\1", layers)),
-        # layer_confidence = layer_confidence == "good",
-        layer_confidence = ifelse(layer_confidence == "good", 1, .5)
-    ) |>
-    select(-layer_label) |>
+anno_matrix <- layers_long |>
+    mutate(layer_confidence = ifelse(layer_confidence == "good", 1, .5)) |>
+    select(-layer_label, -Annotation) |>
     pivot_wider(names_from = "layers", values_from = "layer_confidence") |>
     column_to_rownames("cluster")
 
 anno_matrix <- anno_matrix[rownames(cor_top100), gsub("ayer", "", colnames(cor_top100))]
 colnames(anno_matrix) <- colnames(cor_top100)
-
 
 anno_matrix2 <- cor_top100
 anno_matrix2[is.na(anno_matrix)] <- NA
@@ -174,7 +161,7 @@ dev.off()
 source(here("code","analysis", "12_spatial_registration_sn","utils.R"))
 
 ## layer_annotation is the reordered layer label - removes detail from the ordering process but helps group
-layer_anno2 <- layer_anno |>
+layer_anno_all <- layer_anno_all |>
     arrange(cluster) |>
     mutate(
         layer_annotation = fix_layer_order2(layer_label),
@@ -188,7 +175,7 @@ layer_anno2 <- layer_anno |>
     select(-cellType_broad)
 
 ## Save for refrence
-write.csv(layer_anno2, file = here("processed-data", "rdata", "spe", "12_spatial_registration_sn", "cellType_layer_annotations.csv"))
+write.csv(layer_anno_all, file = here("processed-data", "rdata", "spe", "12_spatial_registration_sn", "cellType_layer_annotations.csv"))
 
 ## Add to sce object for future use
 load(file = "/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/sce/sce_DLPFC.Rdata", verbose = TRUE)
@@ -213,3 +200,6 @@ table(sce$layer_annotation)
 # 5690     66   6558   1932     82  10459  24335   3043   3655   1567    420   2505   2487   1792  10966   2047
 
 # save(sce, file = "/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/sce/sce_DLPFC.Rdata")
+
+#### Explore Annotations ####
+
