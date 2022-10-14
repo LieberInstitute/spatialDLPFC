2e5397840b221e346160a5ed4f1f4c669b085dbe
Nick-Eagles <nick.eagles@libd.org>
2022-10-14 16:36:20 -0400

    Add a shell script for producing the deconvo-comparison plots

---
+++ code/spot_deconvo/05-shared_utilities/05-assess_methods.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -cwd
+#$ -N "assess_methods"
+#$ -o ../../../processed-data/spot_deconvo/05-shared_utilities/logs/05-assess_methods_layer.log
+#$ -e ../../../processed-data/spot_deconvo/05-shared_utilities/logs/05-assess_methods_layer.log
+#$ -l mf=10G,h_vmem=10G
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
+module load conda_R/4.2.x
+Rscript 05-assess_methods.R
+
+echo "**** Job ends ****"
+date
