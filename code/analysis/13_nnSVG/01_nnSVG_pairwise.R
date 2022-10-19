975700a60d1683f36d07b62d124d0dfb7a5862de
lahuuki <lahuuki@gmail.com>
2022-10-19 15:21:31 -0400

    Modify args so pattern is domain_sample #119

--- code/analysis/13_nnSVG/01_nnSVG_pairwise.R
+++ code/analysis/13_nnSVG/01_nnSVG_pairwise.R
@@ -9,23 +9,27 @@ cluster_k = "bayesSpace_harmony_16"
 
 ## get sample and domains from args
 args = commandArgs(trailingOnly=TRUE)
-sample = args[1]
-domain_x = args[2]
-domain_y = args[3]
 
-## Example
-# sample = "Br2720_ant"
-# domain_x = 5
-# domain_y = 9
+domains = args[1]
+domains_n = as.integer(strsplit(domains, "v")[[1]])
 
-message("Running sample: ", sample, ", Domains", cluster_k, ": ", domain_x, " vs. ", domain_y)
+domain_x = domains_n[[1]]
+domain_y = domains_n[[2]]
+
+## sample i from array job
+sample_i = as.integer(args[2])
 
 message(Sys.time(), " - Loading spe")
 load(here("processed-data", "rdata","spe", "01_build_spe", "spe_filtered_final_with_clusters.Rdata"))
 
+samples <- unique(sce$Sample)
+sample <- samples[[sample_i]]
+
 stopifnot(sample %in% spe$sample_id)
 stopifnot(domain_x %in% spe[[cluster_k]])
 stopifnot(domain_y %in% spe[[cluster_k]])
+
+message("Running sample: ", sample, ", Domains", cluster_k, ": ", domain_x, " vs. ", domain_y)
 
 message(Sys.time(), " - Subset spe")
 
@@ -54,21 +58,29 @@ spe <- nnSVG(spe, X = mod, n_threads = 1)
 message(Sys.time(), " - Done! save data")
 nnSVG_data <- as.data.frame(rowData(spe))
 nnSVG_data$Sample <- sample
-nnSVG_data$combo <- paste0(order(c(domain_x, domain_y)), collapse = ",")
+nnSVG_data$domains <- domains
 
 # directory to save bayesspace informed results
 data_dir <- here("processed-data", "rdata", "spe", "13_nnSVG", "01_nnSVG_pairwise")
 
 # save bayesspace nnSVG results
-fn_out <- paste0("nnSVG_k16-", sample,"-",domain_x, "v", domain_y , ".RData")
+fn_out <- paste0("nnSVG_k16-",domains, "-", sample, ".RData")
 
 message("Saving to: ", fn_out)
 save(nnSVG_data, file = here(data_dir, fn_out))
 
-## Use sgejobs to write sh files 
+#### Use sgejobs to write sh files ### 
 # sgejobs::job_single('01_nnSVG_pairwise_single_test', create_shell = TRUE, queue= 'bluejay', memory = '25G', command = "Rscript 01_nnSVG_pairwise.R Br2720_ant 5 9")
 
-# combos <- readLines("nnSVG_input.txt")
+## Loop through domains, use array to run samples
+# domains <- readLines("nnSVG_domains.txt")
+sgejobs::job_loop(
+  loops = list(domains = domains),
+  cores = 5,
+  task_num = 18,
+  name = '01_nnSVG_pairwise_loop',
+  create_shell = TRUE
+)
 
 ## Reproducibility information
 print("Reproducibility information:")
