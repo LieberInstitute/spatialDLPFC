a31a59e89f69a8cb8d88ade890ff131cdfac1974
madhavitippani <madhavitippani28@gmail.com>
2022-10-19 13:28:12 -0400

    test watershed on round 1 images

---
+++ code/VistoSeg/code/WS.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
+#$ -pe local 4
+#$ -o /dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/code/VistoSeg/code/Logs/WS.$TASK_ID.txt
+#$ -e /dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/code/VistoSeg/code/Logs/WS.$TASK_ID.txt
+#$ -m e
+#$ -M madhavitippani28@gmail.com
+#$ -t 1-12
+#$ -tc 6
+
+echo "**** Job starts ****"
+date
+
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+echo "****"
+echo "Sample id: $(cat /dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/code/VistoSeg/code/ALLsamples.txt | awk '{print $1}' | awk "NR==${SGE_TASK_ID}") "
+echo "****"
+
+module load matlab/R2019a
+
+toolbox='/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/code/VistoSeg/code'
+fname=$(cat /dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/code/VistoSeg/code/ALLsamples.txt | awk '{print $1}' | awk "NR==${SGE_TASK_ID}")
+
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), WS('$fname')"
+
+echo "**** Job ends ****"
+date
