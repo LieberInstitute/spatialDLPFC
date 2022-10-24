3bb242ee79e5dafad0c8c1cc2a70796853b71b0c
lahuuki <lahuuki@gmail.com>
2022-10-24 10:23:34 -0400

    Add loop sh files

---
+++ code/analysis/13_nnSVG/.01_nnSVG_pairwise_loop_5v9.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
+#$ -N nnSVG_pairwise_loop_5v9
+#$ -o logs/01_nnSVG_pairwise_loop_5v9.$TASK_ID.txt
+#$ -e logs/01_nnSVG_pairwise_loop_5v9.$TASK_ID.txt
+#$ -t 1-30
+#$ -tc 10
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/4.2
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 01_nnSVG_pairwise.R 5v9 $SGE_TASK_ID
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
+
+
