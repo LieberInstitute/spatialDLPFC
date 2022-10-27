b975c51a2a6b56ef0425cb65b975b4a3532870b4
madhavitippani <madhavitippani28@gmail.com>
2022-10-26 23:03:49 -0400

    change countSpots to count by centroid

--- code/VistoSeg/code/countSpots.m
+++ code/VistoSeg/code/countSpots.m
@@ -1,41 +1,66 @@
-function count = countSpots(BW, R, tbl, posPath)
-    
+function [count,prop] = countSpots(BW, R, tbl, posPath)
+  
 count = [];
+prop = [];
 
+O = fieldnames(BW);
 
     nSpots = size(tbl, 1);
-	[~, numNuclei] = bwlabel(BW);
-	disp([num2str(numNuclei), ' cells detected'])
-	disp([num2str(nSpots),' spots detected'])
-		disp('counting nuclei per spot')
+    disp([num2str(nSpots),' Visium spots detected'])
+    
+    disp('Building spot grid')
     crow = round(table2array(tbl(:, 5)));
     ccol = round(table2array(tbl(:, 6)));
-    mask = zeros(size(BW));
-    count = zeros(nSpots, 1);
-    for i = 1:nSpots
-        mask(crow(i), ccol(i)) = 1;
-    end
+    mask = zeros(size(BW.(O{1})));
+    mask(sub2ind(size(mask),crow,ccol)) = 1;
     mask = bwdist(mask) <= R;
     mask = bwlabel(mask);
-    BW = bwlabel(BW);
-    for i = 1:nSpots
-        idx = mask(crow(i), ccol(i));
-        %tmpBW = BW;
-        %tmpBW(mask~=idx) = 0;
-        %[~, c] = bwlabel(tmpBW);
-        spot = BW(mask==idx & BW>0);
-        c = length(unique(spot));
-        count(i) = c;
-        if mod(i,100) == 0
-        disp([num2str(i),' spots finished in time ', num2str(toc),'s'])
+    
+for C = 1:numel(O)
+    [BW.(O{C}),numROI] = bwlabel(BW.(O{C}));
+	disp([num2str(numROI),' ', O{C},' ROIS detected'])
+    count.(O{C}) = zeros(nSpots, 1);
+    prop.(O{C}) = zeros(nSpots, 1);
+end
+
+tic
+disp('counting dots per Visium spot')
+
+for i = 1:nSpots 
+    idx = mask(crow(i), ccol(i));
+    spot = regionprops(mask==idx);
+      for C = 1:numel(O)
+        signal = struct2table(regionprops(mask==idx & BW.(O{C})>0));
+        points = signal.Centroid;
+        if isempty(points)
+            isincircle = 0;
+        else 
+        isincircle = sum((points - [ccol(i) crow(i)]).^2,2)<= R^2;
         end
-        
-    end
-    tbl = [tbl array2table(count)];
-    tbl.Properties.VariableNames = {'barcode','tissue','row','col','imagerow','imagecol','count'};
-    if ~exist(posPath, 'dir')
-        mkdir(posPath);
-    end
+        %check
+%         [tempx,tempy] = find(mask == idx);
+%         temp = BW.(O{C})(min(tempx):max(tempx),min(tempy):max(tempy));
+%         imshow(temp)
+%         viscircles(size(temp)/2, repelem(R, 1), 'Color', 'r');
+
+        count.(O{C})(i) = length(find(isincircle));
+        prop.(O{C})(i) = sum([signal.Area])/spot.Area;
+      end
+      
+      if mod(i,100) == 0
+        disp([num2str(i),' spots finished in time ', num2str(toc),'s'])
+      end
+
+end
+
+for C = 1:numel(O)
+ temp = [count.(O{C}) prop.(O{C})];  
+ tbl = [tbl array2table(temp, 'VariableNames', {['N',O{C}],['P',O{C}]})];        
+end
+
+if ~exist(posPath, 'dir')
+   mkdir(posPath);
+end
     
     disp('writing table')
     writetable(tbl, fullfile(posPath, 'tissue_spot_counts.csv'), 'Delimiter', ',');
