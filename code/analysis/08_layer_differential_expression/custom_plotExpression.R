33853d240a13b9a5599f0872297de42c14dee73e
lahuuki <lahuuki@gmail.com>
2022-11-08 11:09:30 -0500

    Highlight by sample_id to match example spatial plot

--- code/analysis/08_layer_differential_expression/custom_plotExpression.R
+++ code/analysis/08_layer_differential_expression/custom_plotExpression.R
@@ -17,17 +17,19 @@
 #'     cat = "Mutation_Status",
 #'     fill_colors = c(negative = "green", positive = "pink")
 #' )
-custom_plotExpression <- function(sce, genes, assay = "logcounts", cat, highlight = "highlight", fill_colors = NULL, title = NULL) {
-    cat_df <- as.data.frame(colData(sce))[, c(cat, highlight), drop = FALSE]
+custom_plotExpression <- function(sce, genes, assay = "logcounts", cat, highlight_sample = "None", fill_colors = NULL, title = NULL) {
+    cat_df <- as.data.frame(colData(sce))[, c("sample_id", cat), drop = FALSE]
     
     expression_long <- reshape2::melt(as.matrix(assays(sce)[[assay]][genes, ,drop = FALSE]))
     cat <- cat_df[expression_long$Var2, ]
-    colnames(cat) <- c("cat", "highlight")
+    colnames(cat) <- c("sample_id", "cat") 
     expression_long <- cbind(expression_long, cat)
+    
+    # return(expression_long)
 
     expression_violin <- ggplot(data = expression_long, aes(x = cat, y = value, fill = cat)) +
-        geom_violin(aes(color = highlight), scale = "width") +
-        geom_jitter(size = 1) +
+        geom_violin(scale = "width") +
+        geom_jitter(aes(color = sample_id == highlight_sample), size = 1) +
         facet_wrap(~Var1) +
         labs(
             y = paste0("Expression (", assay, ")"),
@@ -49,7 +51,7 @@ custom_plotExpression <- function(sce, genes, assay = "logcounts", cat, highligh
             geom = "crossbar",
             width = 0.3
         ) +
-      scale_color_manual(values = c(`TRUE` = "goldenrod", `FALSE` = "black"))
+      scale_color_manual(values = c(`TRUE` = "yellow", `FALSE` = "black"))
 
     if (!is.null(fill_colors)) expression_violin <- expression_violin + scale_fill_manual(values = fill_colors)
 
