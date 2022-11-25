d1ed4e1dba87d7a03fe989fc00c67819b9e74c6f
Boyi Guo <boyi.guo.work@gmail.com>
2022-11-25 16:26:50 -0500

    add exploratory data analysis * function to match cell types in spot deconv and sce meta object

--- code/analysis/15_cell_composition/02-eda.R
+++ code/analysis/15_cell_composition/02-eda.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(SingleCellExperiment)
 
 # NOTE: this line only relevents to Boyi
 # .libPaths(c(.libPaths(), "/users/bguo/R/4.2"))
@@ -13,9 +14,19 @@ library(tidyverse)
 if(!"fnl_dat" %in% ls()) fnl_dat <- readRDS("~/cell_comp_full_dat.rds")
 
 # Find Colors
-# setwd("")
 sce <- readRDS("/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/sce/sce_DLPFC_annotated/se.rds")
 cell_type_colors_layer <- metadata(sce)$cell_type_colors_layer[levels(sce$cellType_layer)]
+
+factor_cell_type_layer <- function(vec){
+    factor(vec,
+           levels = c("Astro",  "EndoMural", "Micro", "Oligo", "OPC",
+                      "Excit_L2_3", "Excit_L3", "Excit_L3_4_5", "Excit_L4","Excit_L5",
+                      "Excit_L5_6","Excit_L6","Inhib"),
+           labels = c("Astro",  "EndoMural", "Micro", "Oligo", "OPC",
+                      "Excit_L2/3", "Excit_L3", "Excit_L3/4/5", "Excit_L4","Excit_L5",
+                      "Excit_L5/6","Excit_L6","Inhib")
+    )
+}
 
 # # of Samples per pos ------------------------------------------------------------
 
@@ -57,7 +68,7 @@ ggsave("~/cell_comp/cell_count_diff.pdf",
            facet_grid(rows = vars(region),
                       cols = vars(subject)) +
            labs(title = "count-n_cell_deconv"),
-        height = 5,
+       height = 5,
        width = 10
 )
 # TODO: Possibly highlight which have matching snRNA
@@ -102,20 +113,21 @@ fnl_dat |>
 
 ggsave("~/cell_comp/cell_comp_overall.pdf",
        plot =
-cell_count_fnl_dat |>
-    group_by(region, subject) |>
-    summarize(
-        across(Astro:OPC, sum),
-              n_cell_deconv = sum(n_cell_deconv)) |>
-    ungroup() |>
-    mutate(across(Astro:OPC,  ~.x/n_cell_deconv)) |>
-    pivot_longer(Astro:OPC, names_to = "cell_type", values_to = "cell_perc") |>
-    ggplot(aes( x = subject, y = cell_perc, fill = cell_type)) +
-    geom_bar(position = "stack", stat = "identity") +
-    facet_wrap(~region) +
-    #TODO: check if the color mapping is correct
-    scale_fill_manual(values = cell_type_colors_layer) +
-    theme_bw()
+           cell_count_fnl_dat |>
+           group_by(region, subject) |>
+           summarize(
+               across(Astro:OPC, sum),
+               n_cell_deconv = sum(n_cell_deconv)) |>
+           ungroup() |>
+           mutate(across(Astro:OPC,  ~.x/n_cell_deconv)) |>
+           pivot_longer(Astro:OPC, names_to = "cell_type", values_to = "cell_perc") |>
+           mutate(cell_type = factor_cell_type_layer(cell_type)) |>
+           ggplot(aes( x = subject, y = cell_perc, fill = cell_type)) +
+           geom_bar(position = "stack", stat = "identity") +
+           facet_wrap(~region) +
+           #TODO: check if the color mapping is correct
+           scale_fill_manual(values = cell_type_colors_layer) +
+           theme_bw()
 )
 # NOTE: Overall cell_composition doesn't seems to be very different
 # TODO: combine total number of cells and total number of spots into this plot
@@ -166,9 +178,9 @@ ggsave(filename = "~/cell_comp/layer_comp_sp19.pdf",
 sp9_comp_pc_smp |>
     map_dfr(.f = function(dat){
         dat |>
-        pivot_wider(id_cols = subject,
-                    names_from = sp9,
-                    values_from = layer_perc) |>
+            pivot_wider(id_cols = subject,
+                        names_from = sp9,
+                        values_from = layer_perc) |>
             mutate(region = head(dat$region,1))
     })
 
@@ -178,18 +190,21 @@ sp9_comp_pc_smp |>
 
 ggsave("~/cell_comp/cell_comp_sp9.pdf",
        plot =
-cell_count_fnl_dat |>
-    group_by(region, sp9, subject) |>
-    summarise(
-        n_cell_deconv = sum(n_cell_deconv),
-        across(Astro:OPC, sum)) |>
-    ungroup() |>
-    mutate(across(Astro:OPC, ~.x/n_cell_deconv)) |>
-    pivot_longer(Astro:OPC, names_to = "cell_type", values_to = "cell_perc") |>
-    ggplot(aes( x = subject, y = cell_perc, fill = cell_type)) +
-    geom_bar(position = "stack", stat = "identity") +
-    facet_grid(rows = vars(sp9), cols = vars(region)),
-height = 10, width = 5
+           cell_count_fnl_dat |>
+           group_by(region, sp9, subject) |>
+           summarise(
+               n_cell_deconv = sum(n_cell_deconv),
+               across(Astro:OPC, sum)) |>
+           ungroup() |>
+           mutate(across(Astro:OPC, ~.x/n_cell_deconv)) |>
+           pivot_longer(Astro:OPC, names_to = "cell_type", values_to = "cell_perc") |>
+           mutate(cell_type = factor_cell_type_layer(cell_type)) |>
+           ggplot(aes( x = subject, y = cell_perc, fill = cell_type)) +
+           geom_bar(position = "stack", stat = "identity") +
+           facet_grid(rows = vars(sp9), cols = vars(region)) +
+           scale_fill_manual(values = cell_type_colors_layer) +
+           theme_bw(),
+       height = 10, width = 5
 )
 
 # TODO: possibly horizontal plot
@@ -262,9 +277,12 @@ ggsave("~/cell_comp/cell_comp_sp16.pdf",
            ungroup() |>
            mutate(across(Astro:OPC, ~.x/n_cell_deconv)) |>
            pivot_longer(Astro:OPC, names_to = "cell_type", values_to = "cell_perc") |>
+           mutate(cell_type = factor_cell_type_layer(cell_type)) |>
            ggplot(aes( x = subject, y = cell_perc, fill = cell_type)) +
            geom_bar(position = "stack", stat = "identity") +
-           facet_grid(rows = vars(sp16), cols = vars(region)),
+           facet_grid(rows = vars(sp16), cols = vars(region)) +
+           scale_fill_manual(values = cell_type_colors_layer) +
+           theme_bw(),
        height = 25, width = 10
 )
 
