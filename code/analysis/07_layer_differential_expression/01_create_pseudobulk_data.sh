6ddcebb11c3b4a8b2d6e4b8e31a2b6b98a4ecb26
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-11-28 10:08:00 -0500

    Ran sgejobs::job_single()

---
+++ code/analysis/07_layer_differential_expression/01_create_pseudobulk_data.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=20G,h_vmem=20G,h_fsize=100G
+#$ -N 01_create_pseudobulk_data
+#$ -o logs/01_create_pseudobulk_data.$TASK_ID.txt
+#$ -e logs/01_create_pseudobulk_data.$TASK_ID.txt
+#$ -m e
+#$ -t 1-28
+#$ -tc 10
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript -e "options(width = 120); sessioninfo::session_info()"
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
