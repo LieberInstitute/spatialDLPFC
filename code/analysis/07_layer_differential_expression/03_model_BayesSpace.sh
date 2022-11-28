4689aab67fabd41bfa18fdaa98d18e0a4b8515ed
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-11-28 11:19:35 -0500

    Created an sgejobs script

---
+++ code/analysis/07_layer_differential_expression/03_model_BayesSpace.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=15G,h_vmem=15G,h_fsize=100G
+#$ -N 03_model_BayesSpace
+#$ -o logs/03_model_BayesSpace.$TASK_ID.txt
+#$ -e logs/03_model_BayesSpace.$TASK_ID.txt
+#$ -m e
+#$ -t 1-28
+#$ -tc 10
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript -e "options(width = 120); sessioninfo::session_info()"
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
