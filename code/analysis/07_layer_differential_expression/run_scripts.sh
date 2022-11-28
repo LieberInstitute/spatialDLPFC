92c5cca6bfae1870f01af9fa628f63f0acf79033
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-11-28 12:08:30 -0500

    Add a little script for re-running all scripts in step 07

---
+++ code/analysis/07_layer_differential_expression/run_scripts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "**** Job starts ****"
+date
+
+
+cd /dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/code/analysis/07_layer_differential_expression
+
+rm logs/01*.txt
+qsub 01_create_pseudobulk_data.sh
+
+rm logs/02*.txt
+qsub 02_explore_expr_variability.sh
+
+rm logs/03*.txt
+qsub 03_model_BayesSpace.txt
+
+echo "**** Job ends ****"
+date
