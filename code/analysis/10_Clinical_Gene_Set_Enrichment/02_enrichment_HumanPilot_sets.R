8fdcdea7c41d2b075351acc939df10d227126062
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-11-29 17:36:02 -0500

    Make it a regular job instead of an array one since we can load all the modeling results at once

--- code/analysis/10_Clinical_Gene_Set_Enrichment/02_enrichment_HumanPilot_sets.R
+++ code/analysis/10_Clinical_Gene_Set_Enrichment/02_enrichment_HumanPilot_sets.R
@@ -1,41 +1,33 @@
 # library(sgejobs)
 # sgejobs::job_single(
-#     name = "03_model_BayesSpace",
+#     name = "02_enrichment_HumanPilot_sets",
 #     create_shell = TRUE,
 #     queue = "bluejay",
-#     memory = "5G",
-#     task_num = 28,
-#     tc = 10
+#     memory = "5G"
 # )
-# To execute the script builder, use: qsub 03_model_BayesSpace.sh
-
-k <- as.numeric(Sys.getenv("SGE_TASK_ID"))
-
-## For testing
-if (FALSE) {
-    k <- 2
-}
-
+# To execute the script builder, use: qsub 02_enrichment_HumanPilot_sets.sh
 
 library("here")
 library("sessioninfo")
-library("spatialLIBD")
 
 ## output directory
 dir_rdata <- here::here(
     "processed-data",
     "rdata",
     "spe",
-    "07_layer_differential_expression"
+    "10_Clinical_Gene_Set_Enrichment"
 )
 dir.create(dir_rdata, showWarnings = FALSE, recursive = TRUE)
 stopifnot(file.exists(dir_rdata)) ## Check that it was created successfully
 dir_plots <- here::here(
     "plots",
-    "07_layer_differential_expression"
+    "10_Clinical_Gene_Set_Enrichment"
 )
 dir.create(dir_plots, showWarnings = FALSE, recursive = TRUE)
 stopifnot(file.exists(dir_plots))
+
+## Load the gene sets from the HumanPilot project
+load(here(dir_rdata, "gene_sets_HumanPilot.Rdata"), verbose = TRUE)
 
 
 ## Reproducibility information
