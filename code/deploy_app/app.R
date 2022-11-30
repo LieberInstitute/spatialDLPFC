e0e0ed4621bc424c1fb4a401d6ad208b03a6cfbb
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-11-30 01:13:49 -0500

    quote Polychrome

--- code/deploy_app/app.R
+++ code/deploy_app/app.R
@@ -3,7 +3,7 @@ library("markdown") ## Hm... to avoid this error
 # 2021-11-11T05:30:49.941401+00:00 shinyapps[5096402]: Listening on http://127.0.0.1:32863
 # 2021-11-11T05:30:50.218127+00:00 shinyapps[5096402]: Warning: Error in loadNamespace: there is no package called ‘markdown’
 # 2021-11-11T05:30:50.222437+00:00 shinyapps[5096402]:   111: <Anonymous>
-library(Polychrome)
+library("Polychrome")
 
 ## spatialLIBD uses golem
 options("golem.app.prod" = TRUE)
