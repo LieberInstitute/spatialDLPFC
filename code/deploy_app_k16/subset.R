65dc2aafc9c72e450b2ad73c542a2df05d6528d9
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-11-30 01:30:47 -0500

    Update the subsetting code in k16 to match the code in k09; create soft links as well

--- code/deploy_app_k16/subset.R
+++ code/deploy_app_k16/subset.R
@@ -3,61 +3,65 @@ library("lobstr")
 library("here")
 library("sessioninfo")
 
-## Load the spe object
-load(here::here("code", "deploy_app", "spe_subset.Rdata"),
-    verbose = TRUE
+## Set up soft links if needed
+withr::with_dir(
+    here("code", "deploy_app_k16"),
+    system("ln -s ../../processed-data/rdata/spe/07_layer_differential_expression/modeling_results_BayesSpace_k16.Rdata modeling_results_BayesSpace_k16.Rdata")
+)
+withr::with_dir(
+    here("code", "deploy_app_k16"),
+    system("ln -s ../../processed-data/rdata/spe/07_layer_differential_expression/sce_pseudo_BayesSpace_k16.rds sce_pseudo_BayesSpace_k16.rds")
+)
+withr::with_dir(
+    here("code", "deploy_app_k16"),
+    system("ln -s ../deploy_app_k09/spe_subset.Rdata spe_subset.Rdata")
 )
 
-# load the pseudobulked object spe_pseudo
-spe_pseudo <-
+# load the pseudobulked object sce_pseudo
+sce_pseudo <-
     readRDS(
         here(
             "code",
             "deploy_app_k16",
-            "spe_pseudobulk_bayesSpace_normalized_filtered_cluster_k16.RDS"
+            "sce_pseudo_BayesSpace_k16.rds"
         )
     )
 
-lobstr::obj_size(spe_pseudo)
-# 75.50 MB
+
+lobstr::obj_size(sce_pseudo)
+# 75.05 MB
 
 # load modeling results for k16 clustering/pseudobulking
-load(
-    here(
-        "code",
-        "deploy_app_k16",
-        "parsed_modeling_results_k16.Rdata"
-    ),
-    verbose = TRUE
-)
+load(here(
+    "code",
+    "deploy_app_k16",
+    "modeling_results_BayesSpace_k16.Rdata"
+),
+    verbose = TRUE)
 lobstr::obj_size(modeling_results)
-# 33.28 MB
+# 33.43 MB
 
 ## For sig_genes_extract_all() to work https://github.com/LieberInstitute/Visium_IF_AD/blob/5e3518a9d379e90f593f5826cc24ec958f81f4aa/code/05_deploy_app_wholegenome/app.R#L37-L44
-spe_pseudo$spatialLIBD <- spe_pseudo$BayesSpace
+sce_pseudo$spatialLIBD <- sce_pseudo$BayesSpace
 
 ## Check that we have the right number of tests
 k <- 16
-tests <-
-    lapply(modeling_results, function(x) {
-        colnames(x)[grep("stat", colnames(x))]
-    })
-stopifnot(length(tests$anova) == 1) ## assuming only noWM
+tests <- lapply(modeling_results, function(x) {
+    colnames(x)[grep("stat", colnames(x))]
+})
+stopifnot(length(tests$anova) == 1) ## assuming only "all"
 stopifnot(length(tests$enrichment) == k)
 stopifnot(length(tests$pairwise) == choose(k, 2))
 
 sig_genes <- sig_genes_extract_all(
-    n = nrow(spe_pseudo),
+    n = nrow(sce_pseudo),
     modeling_results = modeling_results,
-    sce_layer = spe_pseudo
+    sce_layer = sce_pseudo
 )
 
 ## Check that we have the right number of tests.
-## the + 1 at the end assumes only noWM
+## the + 1 at the end assumes only "all"
 stopifnot(length(unique(sig_genes$test)) == choose(k, 2) * 2 + k + 1)
-
-## Fix the pairwise colors
-sig_genes$test <- gsub("BayesSpace", "", sig_genes$test)
 
 lobstr::obj_size(sig_genes)
 # 2.77 GB
@@ -74,24 +78,37 @@ lobstr::obj_size(sig_genes)
 # ## Subset sig_genes
 # sig_genes <- subset(sig_genes, fdr < 0.05)
 # dim(sig_genes)
-# # [1] 679801     10
+# # [1] 342449     10
 # lobstr::obj_size(sig_genes)
-# # 59.64 MB
+# # 28.37 MB
 
 ## Extract FDR < 5%
 ## From
 ## https://github.com/LieberInstitute/brainseq_phase2/blob/be2b7f972bb2a0ede320633bf06abe1d4ef2c067/supp_tabs/create_supp_tables.R#L173-L181
-# fix_csv <- function(df) {
-#   for (i in seq_len(ncol(df))) {
-#     if (any(grepl(",", df[, i]))) {
-#       message(paste(Sys.time(), "fixing column", colnames(df)[i]))
-#       df[, i] <- gsub(",", ";", df[, i])
-#     }
-#   }
-#   return(df)
-# }
-# z <- fix_csv(as.data.frame(subset(sig_genes, fdr < 0.05)))
-# write.csv(z, file = file.path(dir_rdata, "Visium_IF_AD_wholegenome_model_results_FDR5perc.csv"))
+fix_csv <- function(df) {
+    for (i in seq_len(ncol(df))) {
+        if (any(grepl(",", df[, i]))) {
+            message(paste(Sys.time(), "fixing column", colnames(df)[i]))
+            df[, i] <- gsub(",", ";", df[, i])
+        }
+    }
+    return(df)
+}
+z <- fix_csv(as.data.frame(subset(sig_genes, fdr < 0.05)))
+dim(z)
+# [1] 684783     10
+dim(subset(z, top <= 25))
+# [1] 6127   10
+write.csv(
+    subset(z, top <= 25),
+    file = here(
+        "processed-data",
+        "rdata",
+        "spe",
+        "07_layer_differential_expression",
+        "spatialDLPFC_model_results_FDR5perc_top25_k16.csv"
+    )
+)
 
 save(sig_genes,
     file = here::here("code", "deploy_app_k16", "sig_genes_subset_k16.Rdata")
