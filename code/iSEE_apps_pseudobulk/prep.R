f83f1222ee0c4648102b0c8f2cdfa39bb71c1ff1
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-12-01 14:11:53 -0500

    Write a script for making soft links + make the soft links for the sce_pseudo files

---
+++ code/iSEE_apps_pseudobulk/prep.R
@@ -0,0 +1,16 @@
+## Create soft links to the sce_pseudo files
+for (k in c(9, 16, 28)) {
+    withr::with_dir(here("code", "iSEE_apps_pseudobulk", paste0("k", sprintf("%02d", k))),
+        system(
+            paste0(
+                "ln -s ../../../processed-data/rdata/spe/07_layer_differential_expression/sce_pseudo_BayesSpace_k",
+                sprintf("%02d", k),
+                ".rds sce_pseudo_BayesSpace_k",
+                sprintf("%02d", k),
+                ".rds"
+            )
+        ))
+
+}
+
+
