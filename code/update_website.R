bb123ac28c9a70cb929e72e1e28fc4b71716c5f2
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-12-01 18:46:38 -0500

    Add a script for updating the website that works only on my laptop.
    I won't have to remember these commands in the future ^_^

---
+++ code/update_website.R
@@ -0,0 +1,12 @@
+library("here")
+library("withr")
+
+with_dir(here(), {
+    rmarkdown::render('README.Rmd', 'html_document')
+    system("mv README.html ~/Dropbox/Code/spatialDLPFC_website/index.html")
+})
+
+with_dir(
+    "~/Dropbox/Code/spatialDLPFC_website",
+    system("git ci -am -'Updated website with code/update_website.R'; git push origin gh-pages")
+)
