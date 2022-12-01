ad023ccf3dedbc211be61392d718912e8137431b
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-12-01 14:14:40 -0500

    Update k09 deploy script

--- code/iSEE_apps_pseudobulk/k09/deploy.R
+++ code/iSEE_apps_pseudobulk/k09/deploy.R
@@ -1,11 +1,18 @@
-
 library("rsconnect")
 
-source("token.R")
+## Or you can go to your shinyapps.io account and copy this
+## Here we do this to keep our information hidden.
+# load(here("code", "deploy_app_k09", ".deploy_info.Rdata"), verbose = TRUE)
+# rsconnect::setAccountInfo(
+#     name = deploy_info$name,
+#     token = deploy_info$token,
+#     secret = deploy_info$secret
+# )
 
 options(repos = BiocManager::repositories())
+
 rsconnect::deployApp(
-    appFiles = c("app.R", "spe_pseudobulk_bayesSpace_normalized_filtered_region_k9.RDS", "initial.R"),
+    appFiles = c("app.R", "sce_pseudo_BayesSpace_k09.rdsS", "initial.R"),
     appName = "spatialDLPFC_Visium_Sp09_pseudobulk",
     account = "libd",
     server = "shinyapps.io"
