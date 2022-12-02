4cb82f6dd9b62644e330d2c1f4585cbb633c0b59
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-12-01 19:09:07 -0500

    Update deploy.R for k09_position

--- code/deploy_app_k09_position/deploy.R
+++ code/deploy_app_k09_position/deploy.R
@@ -15,16 +15,16 @@ options(repos = BiocManager::repositories())
 
 ## Deploy the app, that is, upload it to shinyapps.io
 rsconnect::deployApp(
-    appDir = here("code", "deploy_app_k09"),
+    appDir = here("code", "deploy_app_k09_position"),
     appFiles = c(
         "app.R",
         "spe_subset_for_spatialLIBD.rds",
         "sce_pseudo_BayesSpace_k09.rds",
-        "modeling_results_BayesSpace_k09.Rdata",
-        "sig_genes_subset_k09.Rdata",
-        withr::with_dir(here("code", "deploy_app_k09"), dir("www", full.names = TRUE))
+        "modeling_results_position_k09.Rdata",
+        "sig_genes_subset_k09_position.Rdata",
+        withr::with_dir(here("code", "deploy_app_k09_position"), dir("www", full.names = TRUE))
     ),
-    appName = "spatialDLPFC_Visium_Sp09",
+    appName = "spatialDLPFC_Visium_Sp09_position",
     account = "libd",
     server = "shinyapps.io"
 )
