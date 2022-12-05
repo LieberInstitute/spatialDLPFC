f492c29d30fd68311bfd187a2b7df0c3d57a25bc
lahuuki <lahuuki@gmail.com>
2022-12-05 15:39:01 -0500

    Customize spatialLIBD::vis_gene to crop image to frame limits #135

--- code/analysis/99_spatial_plotting/02_plot_test.R
+++ code/analysis/99_spatial_plotting/02_plot_test.R
@@ -40,11 +40,37 @@ vis_gene_test <- vis_gene(
   geneid = "CLDN5"
 ) + 
   coord_fixed() +
-  geom_hline(yintercept = c(sl$x_min, sl$x_max)/100)
+  geom_hline(yintercept = c(sl$x_min, sl$x_max)*0.0148894, color = "red", linetype = "dashed") +
+  geom_vline(xintercept = c(sl$y_min, sl$y_max)*0.0148894, color = "red", linetype = "dashed")
 
 
 ggsave(vis_gene_test, filename = here(plot_dir, "vis_gene_ggsave.png"))
 ggsave(vis_gene_test, filename = here(plot_dir, "vis_gene_ggsave.pdf"))
+
+
+vis_gene_test_lim <- vis_gene(
+  spe = spe,
+  point_size = 1.2,
+  sampleid = "Br2720_ant",
+  geneid = "CLDN5"
+) + 
+  coord_fixed() +
+  xlim(sl$x_min*0.0148894, sl$x_max*0.0148894) +
+  ylim(sl$y_min*0.0148894, sl$y_max*0.0148894)
+
+ggsave(vis_gene_test_lim, filename = here(plot_dir, "vis_gene_lim_ggsave.png"))
+
+vis_gene_test_lim_nohist <- vis_gene(
+  spe = spe,
+  point_size = 1.2,
+  sampleid = "Br2720_ant",
+  geneid = "CLDN5",
+  spatial = FALSE
+) + 
+  coord_fixed()
+
+ggsave(vis_gene_test_lim_nohist, filename = here(plot_dir, "vis_gene_test_lim_nohist_ggsave.png"))
+
 
 
 img <- SpatialExperiment::imgRaster(spe, sample_id = "Br2720_ant", image_id = "lowres")
@@ -52,3 +78,20 @@ dim(img)
 
 # sample_id x_min x_max y_min y_max
 # 1   Br2720_ant 15782 33439  2876 21502
+sample <- "Br2720_ant"
+sample_lims[match(sample,sample_lims$sample_id),]
+
+
+## try custom functions
+source("vis_gene_crop.R")
+
+vis_gene_custom_test <- vis_gene_crop(
+  spe = spe,
+  point_size = 2,
+  frame_lim_df = sample_lims,
+  sampleid = "Br6432_ant",
+  geneid = "CLDN5"
+) 
+
+ggsave(vis_gene_custom_test, filename = here(plot_dir, "vis_gene_custom.png"))
+
---
+++ code/analysis/99_spatial_plotting/vis_gene_crop.R
@@ -0,0 +1,147 @@
+
+vis_gene_crop <-
+  function(spe,
+           sampleid,
+           geneid = "SCGB2A2; ENSG00000110484",
+           spatial = TRUE,
+           assayname = "logcounts",
+           minCount = 0,
+           viridis = TRUE,
+           frame_lim_df,
+           image_id = "lowres",
+           alpha = 1,
+           cont_colors = if (viridis) viridisLite::viridis(21) else c("aquamarine4", "springgreen", "goldenrod", "red"),
+           point_size = 2,
+           ...) {
+    spe_sub <- spe[, spe$sample_id == sampleid]
+    d <- as.data.frame(cbind(colData(spe_sub), SpatialExperiment::spatialCoords(spe_sub)), optional = TRUE)
+    
+    if (geneid %in% colnames(colData(spe_sub))) {
+      d$COUNT <- colData(spe_sub)[[geneid]]
+    } else if (geneid %in% rowData(spe_sub)$gene_search) {
+      d$COUNT <-
+        assays(spe_sub)[[assayname]][which(rowData(spe_sub)$gene_search == geneid), ]
+    } else if (geneid %in% rownames(spe_sub)) {
+      d$COUNT <- assays(spe_sub)[[assayname]][which(rownames(spe_sub) == geneid), ]
+    } else {
+      stop("Could not find the 'geneid' ", geneid, call. = FALSE)
+    }
+    d$COUNT[d$COUNT <= minCount] <- NA
+    p <- vis_gene_p_crop(
+      spe = spe_sub,
+      d = d,
+      sampleid = sampleid,
+      spatial = spatial,
+      frame_lim_df = frame_lim_df,
+      title = paste(
+        sampleid,
+        geneid,
+        ...
+      ),
+      viridis = viridis,
+      image_id = image_id,
+      alpha = alpha,
+      cont_colors = cont_colors,
+      point_size = point_size
+    )
+    p + labs(caption = paste(
+      if (!geneid %in% colnames(colData(spe_sub))) {
+        assayname
+      } else {
+        NULL
+      },
+      "min >", minCount
+    ))
+  }
+
+vis_gene_p_crop <-
+  function(spe,
+           d,
+           sampleid,
+           spatial,
+           title,
+           viridis = TRUE,
+           image_id = "lowres",
+           frame_lim_df,
+           alpha = 1,
+           cont_colors = if (viridis) viridisLite::viridis(21) else c("aquamarine4", "springgreen", "goldenrod", "red"),
+           point_size = 2) {
+    
+    ## Some variables
+    pxl_row_in_fullres <- pxl_col_in_fullres <- key <- COUNT <- NULL
+    # stopifnot(all(c("pxl_col_in_fullres", "pxl_row_in_fullres", "COUNT", "key") %in% colnames(d)))
+    
+    scale_factor <- SpatialExperiment::scaleFactors(spe, sample_id = sampleid, image_id = image_id)
+    frame_lims <- ceiling(frame_lim_df[match(sampleid,frame_lim_df$sample_id),-1]*scale_factor)
+    img <- SpatialExperiment::imgRaster(spe, sample_id = sampleid, image_id = image_id)
+    img <- img[frame_lims$x_min:frame_lims$x_max, frame_lims$y_min:frame_lims$y_max]
+    
+    p <-
+      ggplot(
+        d,
+        aes(
+          x = (pxl_col_in_fullres * scale_factor) - frame_lims$y_min,
+          y = (pxl_row_in_fullres * scale_factor) - frame_lims$x_min,
+          fill = COUNT,
+          color = COUNT,
+          key = key
+        )
+      )
+    
+    if (spatial) {
+      grob <- grid::rasterGrob(img, width = grid::unit(1, "npc"), height = grid::unit(1, "npc"))
+      p <-
+        p + geom_spatial(
+          data = tibble::tibble(grob = list(grob)),
+          aes(grob = grob),
+          x = 0.5,
+          y = 0.5
+        )
+    }
+    
+    ## From https://github.com/LieberInstitute/HumanPilot/blob/master/Analysis/Layer_Guesses/layer_marker_genes_plots.R
+    # add.alpha('black', 0.175)
+    # black
+    # "#0000002D"
+    
+    p <- p +
+      geom_point(
+        shape = 21,
+        size = point_size,
+        stroke = 0,
+        alpha = alpha
+      ) +
+      coord_cartesian(expand = FALSE)
+    
+    p <- p + scale_fill_gradientn(
+      colors = cont_colors,
+      na.value = c("black" = "#0000002D")
+    ) +
+      scale_color_gradientn(
+        colors = cont_colors,
+        na.value = c("black" = "#0000002D")
+      )
+    
+    
+    
+    p <- p +
+      coord_fixed() +
+      # xlim(frame_lims$x_min, frame_lims$x_max) +
+      # ylim(frame_lims$y_min, frame_lims$y_max) +
+      xlim(0, ncol(img)) +
+      ylim(nrow(img), 0) +
+      xlab("") + ylab("") +
+      labs(fill = NULL, color = NULL) +
+      ggtitle(title) +
+      theme_set(theme_bw(base_size = 20)) +
+      theme(
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = c(0.95, 0.10)
+      )
+    return(p)
+  }
