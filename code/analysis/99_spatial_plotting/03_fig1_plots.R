5f75d1eed90f77b8d84b4dad436f5d33cdb73a06
lahuuki <lahuuki@gmail.com>
2022-12-14 12:03:53 -0500

    Plot fig 1 dot plots with vis_gene_crop and frame edge lims #135

--- code/analysis/99_spatial_plotting/03_fig1_plots.R
+++ code/analysis/99_spatial_plotting/03_fig1_plots.R
@@ -14,6 +14,7 @@ if (!dir.exists(plot_dir)) {
 }
 
 frame_lims <- read.csv(here("processed-data","rdata", "spe","99_spatial_plotting","frame_limits.csv"))
+frame_edge_lims <- read.csv(here("processed-data","rdata", "spe","99_spatial_plotting","frame_edge_limits.csv"))
 
 # Load full data
 load(
@@ -39,9 +40,10 @@ walk(c("SNAP25","MBP","PCP4"), function(gene){
   vis_gene_crop_plot <- vis_gene_crop(
     spe = spe,
     point_size = 2.2,
-    frame_lim_df = frame_lims,
+    frame_lim_df = frame_edge_lims,
     sampleid = s,
-    geneid = gene
+    geneid = gene,
+    legend_overlap = TRUE
   ) 
   
   ggsave(vis_gene_crop_plot, filename = here(plot_dir, paste0("vis_gene_crop_",s,"-",gene,".png")))
@@ -49,17 +51,17 @@ walk(c("SNAP25","MBP","PCP4"), function(gene){
   
 })
 
-walk(c("SNAP25","MBP","PCP4"), function(gene){
-  s <- "Br8667_mid"
-  vis_gene_plot <- vis_gene(
-    spe = spe,
-    point_size = 1.8,
-    # frame_lim_df = frame_lims,
-    sampleid = s,
-    geneid = gene
-  ) +coord_fixed()
-  
-  ggsave(vis_gene_plot, filename = here(plot_dir, paste0("vis_gene_",s,"-",gene,".png")), height = 10, width = 10)
-  
-})
+# walk(c("SNAP25","MBP","PCP4"), function(gene){
+#   s <- "Br8667_mid"
+#   vis_gene_plot <- vis_gene(
+#     spe = spe,
+#     point_size = 1.8,
+#     # frame_lim_df = frame_lims,
+#     sampleid = s,
+#     geneid = gene
+#   ) +coord_fixed()
+#   
+#   ggsave(vis_gene_plot, filename = here(plot_dir, paste0("vis_gene_",s,"-",gene,".png")), height = 10, width = 10)
+#   
+# })
 
