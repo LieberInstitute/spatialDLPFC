5d9ca5f68b94acccaaa3f59b1861c6d6a7c3f5e0
lahuuki <lahuuki@gmail.com>
2022-12-16 16:13:22 -0500

    Add variable legend position for CLDN5 row, work on k16 row

--- code/analysis/99_spatial_plotting/04_fig2_plots.R
+++ code/analysis/99_spatial_plotting/04_fig2_plots.R
@@ -94,6 +94,10 @@ ggsave(cluster_grid, filename = here(plot_dir, "vis_grid.pdf"), width = 18, heig
 #### Vis gene row ####
 plot_gene <- "CLDN5"
 
+legend_positions = list("Br8667_mid" = c(0.9, 0.8), # top right
+                        "Br6522_ant" = c(0.1, 0.2), # bottom left
+                        "Br6432_ant"= c(0.1, 0.2)) # bottom left 
+
 gene_CLDN5_row_plots <- map(samples, function(s){
   vis_gene_plot <- vis_gene(
     spe = spe,
@@ -104,7 +108,7 @@ gene_CLDN5_row_plots <- map(samples, function(s){
     geneid = plot_gene
   ) +
     labs(title = NULL)+
-    theme(legend.position = "None", ## using heat maps label colors
+    theme(legend.position = legend_positions[[s]], 
           axis.title.x = element_blank(),
           text = element_text(size = text_size),
           axis.title.y = element_text(face = "italic")) ## Italic Gene
@@ -126,27 +130,27 @@ ggsave(CLDN5_row, filename = here(plot_dir, "vis_gene_CLDN5_row.pdf"), width = 1
 
 
 #### k16_row ####
+spe_subset <- spe[, spe$bayesSpace_harmony_16 %in% c(2, 14)]
+spe_subset$bayesSpace_harmony_16 <- droplevels(spe_subset$bayesSpace_harmony_16)
 
-spe_subset <- spe[spe]
+cluster_k16_subset <- vis_clus(
+  spe = spe_subset,
+  viridis = FALSE,
+  point_size = 2.2,
+  colors = k_colors,
+  sampleid = "Br6522_ant",
+  clustervar = k_levels$k16
+) +
+  labs(y = "Br6522_ant", title = "k16 ~ L1") +
+  theme(legend.position = c(0.8, 0.8), ##top right
+        axis.title.x = element_blank(),
+        text = element_text(size = text_size)) 
 
-cluster_k16_subset <- map(samples, function(s){
-  vis_clus_plot <- vis_clus(
-    spe = spe,
-    viridis = FALSE,
-    point_size = 2.2,
-    colors = k_colors,
-    sampleid = "Br6522_ant",
-    clustervar = 
-  ) +
-    theme(legend.position = "None", ## using heat maps label colors
-          axis.title.x = element_blank(),
-          text = element_text(size = text_size)) 
-
-
-gene_plots <- map(example_genes, function(gene){
-  s <- "Br8667_mid"
+k16_genes <- c("SPARC", "HTRA1")
+k16_gene_plots <- map(k16_genes, function(gene){
+  s <- "Br6522_ant"
   vis_gene_plot <- vis_gene(
-    spe = spe,
+    spe = spe_subset,
     viridis = FALSE,
     point_size = 2.2,
     cont_colors = viridisLite::plasma(21),
@@ -156,30 +160,21 @@ gene_plots <- map(example_genes, function(gene){
     labs(title = gene) + 
     theme(plot.title = element_text(face = "italic"),
           # legend.position = c(0.9, 0.02) # bottom right
-          legend.position = c(0.9, 0.8) # top right
+          legend.position = c(0.8, 0.8) # top right
     )
   
   ggsave(vis_gene_plot, filename = here(plot_dir, paste0("vis_gene_",s,"-",gene,".png")))
-  ggsave(vis_gene_plot, filename = here(plot_dir, paste0("vis_gene_",s,"-",gene,".pdf")))
+  # ggsave(vis_gene_plot, filename = here(plot_dir, paste0("vis_gene_",s,"-",gene,".pdf")))
   return(vis_gene_plot)
 })
 
+k16_row <- Reduce("+",c(list(cluster_k16_subset), k16_gene_plots))
+ggsave(k16_row, filename = here(plot_dir, "vis_k16_row.png"), width = 18)
 
 #### Plot all together ####
-ggsave(cluster_grid/CLDN5_row, 
+#k16 is different  - add in AI
+ggsave(cluster_grid/CLDN5_row,  
        filename = here(plot_dir, "Fig2_grid.pdf"), width = 18, height = 21)
 
-
-
-
-
-## patchwork together
-
-patchwork_genes <- gene_plots$SNAP25 + gene_plots$MBP + gene_plots$PCP4
-ggsave(patchwork_genes, filename = here(plot_dir, "vis_gene_Br8667_mid-ALL.png"),width = 18)
-ggsave(patchwork_genes, filename = here(plot_dir, "vis_gene_Br8667_mid-ALL.pdf"),width = 18)
-
-
-# })
 
 
