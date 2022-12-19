0799e43508f2b702e9cb0dc6c5a8010030c6ccda
lahuuki <lahuuki@gmail.com>
2022-12-19 10:41:43 -0500

    Add k7 dot plot, up font size, center titles

--- code/analysis/99_spatial_plotting/03_fig1_plots.R
+++ code/analysis/99_spatial_plotting/03_fig1_plots.R
@@ -27,28 +27,49 @@ load(
 )
 
 rowData(spe)$gene_search <- rowData(spe)$gene_name
+spe$bayesSpace_harmony_7 <- as.factor(spe$bayesSpace_harmony_7)
 spe$bayesSpace_harmony_9 <- as.factor(spe$bayesSpace_harmony_9)
 spe$bayesSpace_harmony_16 <- as.factor(spe$bayesSpace_harmony_16)
 
-## plot example genes 
+#### k7 dotplot ####
+## prep colors
+k_colors <- Polychrome::palette36.colors(28)
+names(k_colors) <- c(1:28)
+
+vis_clust_k7 <- vis_clus(
+  spe = spe,
+  viridis = FALSE,
+  point_size = 2,
+  colors = k_colors,
+  sampleid = "Br8667_mid",
+  clustervar = "bayesSpace_harmony_7"
+) + labs(title = NULL)+ 
+  theme(legend.position = c(0.9, 0.8) # top right
+  )
+
+ggsave(vis_clust_k7, filename = here(plot_dir, "vis_clust_Br8667_mid_k7.png"))
+ggsave(vis_clust_k7, filename = here(plot_dir, "vis_clust_Br8667_mid_k7.pdf"))
+
+#### plot example genes  ####
 example_genes <- c("SNAP25","MBP","PCP4")
 names(example_genes) <- example_genes
+my_text_size <- 25
 
 gene_plots <- map(example_genes, function(gene){
   s <- "Br8667_mid"
   vis_gene_plot <- vis_gene(
     spe = spe,
     viridis = FALSE,
-    point_size = 2.2,
+    point_size = 2,
     cont_colors = viridisLite::plasma(21),
     # frame_lim_df = frame_lims,
     sampleid = s,
     geneid = gene
   ) +
     labs(title = gene) + 
-    theme(plot.title = element_text(face = "italic"),
-          # legend.position = c(0.9, 0.02) # bottom right
-          legend.position = c(0.9, 0.8) # top right
+    theme(plot.title = element_text(face = "italic", hjust = 0.5),
+          legend.position = c(0.9, 0.8), # top right
+          text = element_text(size = my_text_size)
           )
   
   ggsave(vis_gene_plot, filename = here(plot_dir, paste0("vis_gene_",s,"-",gene,".png")))
