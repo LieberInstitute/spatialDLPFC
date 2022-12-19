7dc43ef52eeff0d861e5b83e2a6f1be01c9ce8c6
Leonardo Collado Torres <lcolladotor@gmail.com>
2022-12-19 10:53:07 -0500

    Auto indent code

--- code/analysis/03_BayesSpace/04_BayesSpace_grid_plots.R
+++ code/analysis/03_BayesSpace/04_BayesSpace_grid_plots.R
@@ -7,14 +7,24 @@ library("ggplot2")
 k <- as.numeric(Sys.getenv("SGE_TASK_ID"))
 
 # load spe object and clusters
-load(file = here::here("processed-data", "rdata", "spe", "01_build_spe", "spe_filtered_final_with_clusters.Rdata"), verbose = TRUE)
+load(
+    file = here::here(
+        "processed-data",
+        "rdata",
+        "spe",
+        "01_build_spe",
+        "spe_filtered_final_with_clusters.Rdata"
+    ),
+    verbose = TRUE
+)
 
 mycolors <- Polychrome::palette36.colors(k)
 ## Reverse colors only for k2 to avoid WM vs GM confusion
 if (k == 2) {
     mycolors <- rev(mycolors)
 }
-names(mycolors) <- sort(unique(colData(spe)[[paste0("bayesSpace_harmony_", k)]]))
+names(mycolors) <-
+    sort(unique(colData(spe)[[paste0("bayesSpace_harmony_", k)]]))
 sample_order <- unique(spe$sample_id)
 
 
@@ -30,11 +40,20 @@ pk <- vis_grid_clus(
 
 pk <- lapply(sample_order, function(sampleid) {
     p <- pk[[sampleid]]
-    p + theme(legend.position = "none", plot.title = ggplot2::element_text(size = 40))
+    p + theme(legend.position = "none",
+        plot.title = ggplot2::element_text(size = 40))
 })
 names(pk) <- sample_order
 
-pdf(file = here::here("plots", "03_BayesSpace", paste0("polychrome_vis_grid_clus_sfigu_BayesSpace_k", k, ".pdf")), height = 5 * 8, width = 6 * 8)
+pdf(
+    file = here::here(
+        "plots",
+        "03_BayesSpace",
+        paste0("polychrome_vis_grid_clus_sfigu_BayesSpace_k", k, ".pdf")
+    ),
+    height = 5 * 8,
+    width = 6 * 8
+)
 print(cowplot::plot_grid(plotlist = pk))
 dev.off()
 
