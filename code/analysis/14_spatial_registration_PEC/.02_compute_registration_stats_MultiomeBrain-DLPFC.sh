69578cb3f5ec6afbe1cb3900558edb89dcee4d09
lahuuki <lahuuki@gmail.com>
2023-01-11 13:02:27 -0500

    MultiomeBrain-DLPFC replaces Urban-DLPFC (looks like new name same data)

--- code/analysis/14_spatial_registration_PEC/.02_compute_registration_stats_MultiomeBrain-DLPFC.sh
+++ code/analysis/14_spatial_registration_PEC/.02_compute_registration_stats_MultiomeBrain-DLPFC.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #$ -cwd
 #$ -l mem_free=25G,h_vmem=25G,h_fsize=100G
-#$ -N compute_registration_stats_Urban-DLPFC
-#$ -o logs/02_compute_registration_stats_Urban-DLPFC.txt
-#$ -e logs/02_compute_registration_stats_Urban-DLPFC.txt
-#$ -hold_jid pseudobulk_data_Urban-DLPFC
+#$ -N compute_registration_stats_MultiomeBrain-DLPFC
+#$ -o logs/02_compute_registration_stats_MultiomeBrain-DLPFC.txt
+#$ -e logs/02_compute_registration_stats_MultiomeBrain-DLPFC.txt
+#$ -hold_jid pseudobulk_data_DevBrain
 #$ -m e
 
 echo "**** Job starts ****"
@@ -18,13 +18,13 @@ echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SGE_TASK_ID}"
 
 ## Load the R module (absent since the JHPCE upgrade to CentOS v7)
-module load conda_R/4.2
+module load conda_R/4.2.x
 
 ## List current modules for reproducibility
 module list
 
 ## Sumbit Rscript
-Rscript 02_compute_registration_stats.R Urban-DLPFC
+Rscript 02_compute_registration_stats.R MultiomeBrain-DLPFC
 
 echo "**** Job ends ****"
 date
