11cbd54d650cfb9a032ac7e22d7321dda7007c62
lahuuki <lahuuki@gmail.com>
2023-01-18 12:51:14 -0500

    Remove empty levels at the Dx level

--- code/analysis/14_spatial_registration_PEC/05_compute_registration_Dx.R
+++ code/analysis/14_spatial_registration_PEC/05_compute_registration_Dx.R
@@ -21,6 +21,20 @@ sce_pseudo <- readRDS(file = here(
 message("\nSCE Dimesions:")
 dim(sce_pseudo)
 
+#### Add Dx col ####
+dx_data <- read.csv(here("processed-data", "rdata", "spe", "14_spatial_registration_PEC",
+                         paste0("primaryDiagnosis_",dataset,".csv")))
+
+dx_data |> dplyr::count(primaryDiagnosis)
+
+sce_pseudo$primaryDiagnosis <- dx_data$primaryDiagnosis[match(sce_pseudo$individualID, dx_data$individualID)]
+
+table(sce_pseudo$registration_variable, sce_pseudo$primaryDiagnosis)
+
+## exclude any Dx NAs
+sce_pseudo <- sce_pseudo[, !is.na(sce_pseudo$primaryDiagnosis)]
+
+# Drop pb samples with < 10 nuc input
 min_ncells <- 10
 message(
     Sys.time(),
@@ -30,30 +44,18 @@ message(
 )
 sce_pseudo <- sce_pseudo[, sce_pseudo$ncells >= min_ncells]
 
-message("Cell Types:")
-## must be syntactically valid
-(var_tab <- table(sce_pseudo$registration_variable))
-
-if (any(var_tab == 0)) message("Dropping Empty Levels: ", paste0(names(var_tab)[var_tab == 0], collpase = " "))
-## Drop Levels
-sce_pseudo$registration_variable <- droplevels(sce_pseudo$registration_variable)
-
-#### Add Dx col ####
-dx_data <- read.csv(here("processed-data", "rdata", "spe", "14_spatial_registration_PEC",
-                                     paste0("primaryDiagnosis_",dataset,".csv")))
-
-dx_data |> dplyr::count(primaryDiagnosis)
-
-sce_pseudo$primaryDiagnosis <- dx_data$primaryDiagnosis[match(sce_pseudo$individualID, dx_data$individualID)]
-
-## exclude any Dx NAs
-sce_pseudo <- sce_pseudo[, !is.na(sce_pseudo$primaryDiagnosis)]
-table(sce_pseudo$registration_variable, sce_pseudo$primaryDiagnosis)
 
 #### Run models ####
-results_enrichment <- purrr::map(rafalib::splitit(sce_pseudo$primaryDiagnosis), function(i){
+dx_split <- rafalib::splitit(sce_pseudo$primaryDiagnosis)
+results_enrichment <- purrr::map2(dx_split, names(dx_split), function(i, dx_name){
   sce_temp <- sce_pseudo[,i]
-  message("New Dx - ", ncol(sce_temp), " cols")
+  message("Dx: ", dx_name, " - ", ncol(sce_temp), " cols")
+  var_tab <- table(sce_temp$registration_variable)
+  
+  if (any(var_tab == 0)) message("Dropping Empty Levels: ", paste0(names(var_tab)[var_tab == 0], collpase = " "))
+  ## Drop Levels
+  sce_temp$registration_variable <- droplevels(sce_temp$registration_variable)
+  
   
   registration_mod <- registration_model(sce_temp)
   block_cor <- registration_block_cor(sce_temp, registration_model = registration_mod)
