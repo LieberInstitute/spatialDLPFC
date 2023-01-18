9ae2cc52da35d9f5e8745407587f84ab94d416f2
lahuuki <lahuuki@gmail.com>
2023-01-18 11:50:50 -0500

    Add registration_Dx  sh files generated with job_loop, and log files

---
+++ code/analysis/14_spatial_registration_PEC/.05_compute_registration_Dx_CMC.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
+#$ -N compute_registration_Dx_CMC
+#$ -o logs/05_compute_registration_Dx_CMC.txt
+#$ -e logs/05_compute_registration_Dx_CMC.txt
+#$ -hold_jid pseudobulk_data_CMC
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/4.2.x
+
+## List current modules for reproducibility
+module list
+
+## Sumbit Rscript
+Rscript 05_compute_registration_Dx.R CMC
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
+
+
