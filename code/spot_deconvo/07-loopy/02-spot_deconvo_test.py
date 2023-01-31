f58fe3c1757af3dbfe33308ab20c6cbf8aeb4b41
Nick-Eagles <nick.eagles@libd.org>
2023-01-31 14:17:28 -0500

    Generalize code to work for all 4 IF samples; add a shell script to execute as a 4-task array job

--- code/spot_deconvo/07-loopy/02-spot_deconvo_test.py
+++ code/spot_deconvo/07-loopy/02-spot_deconvo_test.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from pyhere import here
 import json
+import os
 
 import numpy as np
 import pandas as pd
@@ -16,16 +17,40 @@ img_channels = ['Lipofuscin', 'DAPI', 'GFAP', 'NeuN', 'OLIG2', 'TMEM119']
 
 loopy_results_path = here(
     'processed-data', 'spot_deconvo', '05-shared_utilities', 'IF',
-    'loopy_Br2720_Ant_IF.csv'
+    'loopy_{}.csv'
 )
 img_path = here(
-    'raw-data', 'Images', 'VisiumIF', 'VistoSeg', 'V10B01-087_A1.tif'
+    'raw-data', 'Images', 'VisiumIF', 'VistoSeg', '{}.tif'
 )
 json_path = here(
-    'processed-data', '01_spaceranger_IF', 'Br2720_Ant_IF', 'outs', 'spatial',
+    'processed-data', '01_spaceranger_IF', '{}', 'outs', 'spatial',
     'scalefactors_json.json'
 )
-out_dir = here('processed-data', 'spot_deconvo', '07-loopy', 'Br2720_Ant_IF')
+out_dir = here('processed-data', 'spot_deconvo', '07-loopy', '{}')
+
+sample_info_path = here(
+    'raw-data', 'sample_info', 'Visium_IF_DLPFC_MasterExcel_01262022.xlsx'
+)
+
+
+#   Different sample IDs are used for different files associated with each
+#   sample. Determine both forms of the sample ID for this sample and update
+#   path variables accordingly
+sample_info = pd.read_excel(sample_info_path)[:4]
+sample_ids_img = sample_info['Slide SN #'] + '_' + sample_info['Array #']
+sample_ids_spot = 'Br' + sample_info['BrNumbr'].astype(int).astype(str) + \
+    '_' + pd.Series([x.split('_')[1] for x in sample_info['Br_Region']]) + \
+    '_IF'
+
+#   Subset both types of IDs to this sample only
+sample_id_img = sample_ids_img[int(os.environ['SGE_TASK_ID']) - 1]
+sample_id_spot = sample_ids_spot[int(os.environ['SGE_TASK_ID']) - 1]
+
+#   Fill in sample ID for the path-related variables
+img_path = Path(str(img_path).format(sample_id_img))
+json_path = Path(str(json_path).format(sample_id_spot))
+loopy_results_path = Path(str(loopy_results_path).format(sample_id_spot))
+out_dir = Path(str(out_dir).format(sample_id_spot))
 
 #   Import spot-deconvo results
 loopy_csv = pd.read_csv(loopy_results_path)
@@ -35,7 +60,7 @@ loopy_csv.rename(
 )
 
 #   Define the Sample object and add coordinates
-this_sample = Sample(name = "Br2720_Ant_IF", path = out_dir)
+this_sample = Sample(name = sample_id_spot, path = out_dir)
 
 coords = loopy_csv.loc[
     (loopy_csv['deconvo_tool'] == deconvo_tools[0]) &
@@ -49,7 +74,9 @@ with open(json_path, 'r') as f:
 
 m_per_px = spot_diameter_m / spaceranger_json['spot_diameter_fullres']
 
-this_sample.add_coords(coords, name="coords", mPerPx=m_per_px, size=spot_diameter_m)
+this_sample.add_coords(
+    coords, name="coords", mPerPx=m_per_px, size=spot_diameter_m
+)
 
 #   For each deconvolution tool and cell type, add a feature (cell counts for
 #   each spot)
@@ -69,6 +96,8 @@ for deconvo_tool in deconvo_tools:
 
 
 #   Add the IF image for this sample
-this_sample.add_image(tiff = img_path, channels = img_channels, scale = m_per_px)
+this_sample.add_image(
+    tiff = img_path, channels = img_channels, scale = m_per_px
+)
 
 this_sample.write()
