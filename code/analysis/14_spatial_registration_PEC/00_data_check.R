71fc149fab650261da1a77dd8c8fa1298dff80ee
lahuuki <lahuuki@gmail.com>
2023-02-01 16:40:42 -0500

    Build botplot for ALL pce data V5, create standard color pallet for datasets in 00_data_check

--- code/analysis/14_spatial_registration_PEC/00_data_check.R
+++ code/analysis/14_spatial_registration_PEC/00_data_check.R
@@ -1,4 +1,4 @@
-
+library("SingleCellExperiment")
 library("tidyverse")
 library("here")
 library("jaffelab")
@@ -53,6 +53,18 @@ pec_cell_type_tb <- tibble(cell_type = factor(pec_cell_types, levels = pec_cell_
 
 save(pec_cell_type_tb, file = here(data_dir, "pec_cell_type_tb.Rdata"))
 
+## color set         
+pec_dataset_colors <- c(CMC = "#00d995",
+                        `DevBrain-snRNAseq` = "#362600",
+                        IsoHuB ="#bc17d7",
+                        LIBD="#ff6266",
+                        `MultiomeBrain-DLPFC` = "#02e3fc",
+                        PTSDBrainomics = "#b3005a",
+                        `SZBDMulti-Seq`= "#beae00",
+                        `UCLA-ASD`="#afacff")
+
+save(pec_dataset_colors, file = here(data_dir, "pec_dataset_colors.Rdata"))
+
 #### Chck V3 data ####
 raw_data_dir <- here("raw-data", "psychENCODE", "version3", "scRNAseq_AllenCTHarmonized")
 list.files(raw_data_dir)
@@ -94,22 +106,12 @@ map(h5ad_files, function(input_file) {
 })
 
 
-job_loop(
-    loops = list(PE_data = h5ad_files),
-    name = "01_pseudobulk_data",
-    create_shell = TRUE
-)
+# job_loop(
+#     loops = list(PE_data = h5ad_files),
+#     name = "01_pseudobulk_data",
+#     create_shell = TRUE
+# )
 
-## What outputs exist?
-output_dir <- here("processed-data", "rdata", "spe", "14_spatial_registration_PEC")
-list.files(output_dir, pattern = "pseudobulk")
-# [1] "pseudobulk_CMC.rds"               "pseudobulk_DevBrain-snRNAseq.rds" "pseudobulk_IsoHuB.rds"           
-# [4] "pseudobulk_SZBDMulti.rds"         "pseudobulk_UCLA-ASD.rds"          "pseudobulk_Urban-DLPFC.rds"
-
-list.files(output_dir, pattern = "registration_stats")
-# [1] "registration_stats_CMC.rds"               "registration_stats_DevBrain-snRNAseq.rds"
-# [3] "registration_stats_IsoHuB.rds"            "registration_stats_UCLA-ASD.rds"         
-# [5] "registration_stats_Urban-DLPFC.rds"   
 
 #### V4 files ####
 raw_data_dir_v4 <- here("raw-data", "psychENCODE", "version4")
@@ -129,11 +131,38 @@ datasets_v4 <- unique(names(h5ad_files_v4))
 #   create_shell = TRUE
 # )
 
+#### V5 files ####
+raw_data_dir_v5 <- here("raw-data", "psychENCODE", "version5")
+h5ad_files_v5 <- list.files(raw_data_dir_v5, pattern = "h5ad")
+(names(h5ad_files_v5) <- ss(basename(h5ad_files_v5),"_"))
+
+# CMC                            DevBrain-snRNAseq 
+# "CMC_annotated.h5ad"           "DevBrain-snRNAseq_annotated.h5ad" 
+# IsoHuB                                         LIBD 
+# "IsoHuB_annotated.h5ad"                        "LIBD_annotated.h5ad" 
+# MultiomeBrain-DLPFC                               PTSDBrainomics 
+# "MultiomeBrain-DLPFC_annotated.h5ad"              "PTSDBrainomics_annotated.h5ad" 
+# SZBDMulti-Seq                                     UCLA-ASD 
+# "SZBDMulti-Seq_annotated.h5ad" "UCLA-ASD_annotated_mismatches_removed.h5ad" 
+# UCLA-ASD 
+# "UCLA-ASD_annotated.h5ad" 
+# UCLA-ASD ## use mismatches_removed?
+# "UCLA-ASD_annotated_mismatches_removed.h5ad" 
+
+datasets_v5 <- unique(names(h5ad_files_v5))
+
+## add LIBD & PTSD
+job_loop(
+  loops = list(PE_data = c("LIBD", "PTSDBrainomics")),
+  name = "01_pseudobulk_data_check",
+  create_shell = TRUE
+)
+
 
 #### Check Dx in metadata ####
 
-metadata_fn <- map(datasets_v4, ~here("raw-data", "psychENCODE", "version4", paste0("individual_", .x, "_metadata.csv")))
-names(metadata_fn) <- datasets_v4
+metadata_fn <- map(datasets_v5, ~here("raw-data", "psychENCODE", "metadata", paste0("individual_", .x, "_metadata.csv")))
+names(metadata_fn) <- datasets_v5
 
 ## no metadata for multiomeBrain
 metadata <- map(metadata_fn[map_lgl(metadata_fn, file.exists)], ~read.csv(.x))
@@ -155,6 +184,10 @@ map(metadata , ~.x |> dplyr::count(primaryDiagnosis))
 # primaryDiagnosis n
 # 1          control 3
 # 2   Not Applicable 2
+# 
+# $LIBD
+# primaryDiagnosis  n
+# 1          control 10
 # 
 # $`SZBDMulti-Seq`
 # primaryDiagnosis  n
@@ -217,7 +250,13 @@ map(dx_data , ~.x |> dplyr::count(primaryDiagnosis))
 # 2          Control  5
 # 3    Schizophrenia  6
 
-walk2(dx_data, names(dx_data), ~write.csv(.x, file = here("processed-data", "rdata", "spe", "14_spatial_registration_PEC",
+walk2(dx_data["LIBD"], names(dx_data["LIBD"]), ~write.csv(.x, file = here("processed-data", "rdata", "spe", "14_spatial_registration_PEC",
                                                           paste0("primaryDiagnosis_",.y,".csv")),
                                           row.names = FALSE))
 
+## What outputs exist?
+output_dir <- here("processed-data", "rdata", "spe", "14_spatial_registration_PEC")
+list.files(output_dir, pattern = "pseudobulk")
+# [1] "pseudobulk_CMC.rds"                 "pseudobulk_DevBrain-snRNAseq.rds"   "pseudobulk_IsoHuB.rds"             
+# [4] "pseudobulk_LIBD.rds"                "pseudobulk_MultiomeBrain-DLPFC.rds" "pseudobulk_PTSDBrainomics.rds"     
+# [7] "pseudobulk_SZBDMulti-
--- code/analysis/14_spatial_registration_PEC/03_PEC_correlation_annotation.R
+++ code/analysis/14_spatial_registration_PEC/03_PEC_correlation_annotation.R
@@ -7,7 +7,7 @@ library("here")
 library("sessioninfo")
 
 ## plot dir
-plot_dir <- here("plots", "14_spatial_registration_PEC")
+plot_dir <- here("plots", "14_spatial_registration_PEC","03_PEC_correlation_annotation")
 data_dir <-
     here(
         "processed-data",
@@ -38,40 +38,7 @@ modeling_results <-
     c(list(layer = layer_modeling_results), modeling_results)
 names(modeling_results)
 
-pec_cell_types <- c(
-  # Non-neuronal cells (5)
-  "Astro",
-  "Endo",
-  "Micro/PVM",
-  "OPC",
-  "Oligo",
-  #Excit (9)
-  "L2/3 IT",
-  "L4 IT",
-  "L5 ET",
-  "L5 IT",
-  "L5/6 NP",
-  "L6 CT",
-  "L6 IT",
-  "L6 IT Car3",
-  "L6b",
-  # Inhib (10)
-  "Chandelier",
-  "Lamp5",
-  "Lamp5 Lhx6",
-  "Pax6",
-  "Pvalb",
-  "Sncg",
-  "Sst",
-  "Sst Chodl",
-  "VLMC",
-  "Vip"
-)
-
-cell_type_order <- tibble(cell_type = factor(pec_cell_types, levels = pec_cell_types), 
-                          cluster =  make.names(cell_type))
-
-
+load(here(data_dir, "pec_cell_type_tb.Rdata"), verbose = TRUE)
 
 correlate_and_annotate <- function(dataset, make_cor_plot = FALSE) {
     #### Load Registration Stats ####
@@ -89,9 +56,9 @@ correlate_and_annotate <- function(dataset, make_cor_plot = FALSE) {
     colnames(registration_t_stats) <-
         gsub("^t_stat_", "", colnames(registration_t_stats))
 
-    # Fix Cell Types
-    colnames(registration_t_stats) <-
-        cell_types[colnames(registration_t_stats)]
+    # # Fix Cell Types
+    # colnames(registration_t_stats) <-
+    #     cell_types[colnames(registration_t_stats)]
 
     #### Correlate with modeling results ####
     cor_top100 <-
@@ -134,13 +101,15 @@ correlate_and_annotate <- function(dataset, make_cor_plot = FALSE) {
 
 # datasets <- c("CMC", "DevBrain-snRNAseq", "IsoHuB", "SZBDMulti", "UCLA-ASD", "Urban-DLPFC")
 datasets <-
-    c(
-        "CMC",
-        "DevBrain-snRNAseq",
-        "IsoHuB",
-        "UCLA-ASD",
-        "Urban-DLPFC"
-    )
+  c("CMC",
+    "DevBrain-snRNAseq",
+    "IsoHuB",
+    "LIBD",
+    "MultiomeBrain-DLPFC",
+    "PTSDBrainomics",
+    # "SZBDMulti-Seq", - hopefully this runs this time 
+    "UCLA-ASD"
+  )
 names(datasets) <- datasets
 
 ## Calculate correlations and annotations for each dataset
@@ -209,6 +178,22 @@ source(
     max.deparse.length = 500
 )
 
+## Match with bayesSpace spatial annotations for k09 k16 exploration
+bayes_layers <- get(load(here("processed-data", "rdata", "spe", "08_spatial_registration", "bayesSpace_layer_annotations.Rdata"))) |>
+  select(Annotation = bayesSpace, layers = cluster, layer_combo) |>
+  filter(Annotation %in% c("k09", "k16"))
+
+## Add Layer levels + update factor
+spatial_layer_anno <- tibble(
+  Annotation = "layer",
+  layers = c(paste0("Layer", 1:6), "WM"),
+  layer_combo = layers
+) |>
+  bind_rows(bayes_layers) |>
+  mutate(layer_combo = factor(layer_combo, levels = c(paste0("Layer", 1:6), "WM", levels(bayes_layers$layer_combo))),
+         Annotation = factor(Annotation, levels = c("k16", "k09", "layer")))
+
+## extract layer annotation
 layer_anno <- transpose(pe_correlation_annotation)$layer_anno
 
 layer_anno_all <- do.call("rbind", layer_anno) |>
@@ -219,30 +204,33 @@ layer_anno_all <- do.call("rbind", layer_anno) |>
     )
 
 layer_anno_long <- layer_anno_all |>
-    select(Dataset, cluster, ends_with("label")) |>
-    pivot_longer(!c(Dataset, cluster),
-        names_to = "Annotation",
-        values_to = "label"
-    ) |>
-    mutate(
-        confidence = !grepl("\\*", label),
-        layers = str_split(gsub("\\*", "", label), "/"),
-        Annotation = gsub("_label", "", Annotation)
-    ) |>
-    unnest_longer("layers") |>
-    # mutate(layers = ifelse(Annotation == "layer" & grepl("^[0-9]",layers), paste0("L",layers), layers))
-    mutate(
-        layers = ifelse(Annotation == "layer",
-            ifelse(
-                grepl("^[0-9]", layers),
-                paste0("Layer", layers),
-                gsub("L","Layer",layers)
-            ),
-            layers
-        ),
-        # layers_long = gsub("Layers","L",layers),
-        anno_confidence = ifelse(confidence, "high", "low")
-    )
+  left_join(pec_cell_type_tb,  by = "cluster")|>
+  select(Dataset, cell_type, ct_cat, ends_with("label")) |>
+  pivot_longer(!c(Dataset, cell_type, ct_cat),
+               names_to = "Annotation",
+               values_to = "label"
+  ) |>
+  mutate(
+    confidence = !grepl("\\*", label),
+    layers = str_split(gsub("\\*", "", label), "/"),
+    Annotation = gsub("_label", "", Annotation)
+  ) |>
+  unnest_longer("layers") |>
+  # mutate(layers = ifelse(Annotation == "layer" & grepl("^[0-9]",layers), paste0("L",layers), layers))
+  mutate(
+    layers = ifelse(Annotation == "layer",
+                    ifelse(
+                      grepl("^[0-9]", layers),
+                      paste0("Layer", layers),
+                      gsub("L","Layer",layers)
+                    ),
+                    layers
+    ),
+    # layers_long = gsub("Layers","L",layers),
+    anno_confidence = ifelse(confidence, "good", "poor"),
+    Annotation = factor(Annotation, levels = c("k16", "k09", "layer"))
+  ) |> 
+  left_join(spatial_layer_anno)
 
 layer_anno_long |> count(layers)
 # layer_anno_long |> count(layer_long)
@@ -253,7 +241,7 @@ layer_anno_long |>
     count(Dataset, Annotation)
 layer_anno_long |>
     filter(Annotation == "layer", confidence) |>
-    count(Dataset, Annotation, cluster)
+    count(Dataset, Annotation, cell_type)
 
 #### Plot layer annotation ####
 ## prep color scheme
@@ -283,221 +271,44 @@ ex_dotplot <- ggplot(tile_data,
 ggsave(ex_dotplot, filename = here(plot_dir, "ex_dotplot.pdf"), height = 2, width = 3)
 ggsave(ex_dotplot, filename = here(plot_dir, "ex_dotplot.png"), height = 2, width = 3)
 
-# highlight cell type layer annotations
-cell_type_anno <- tibble(cluster = cell_types) |>
-    mutate(layer_label = ifelse(grepl("^L[0-9]", cluster), sub(" .*", "", cluster), NA)) |>
-    filter(!is.na(layer_label)) |>
-    mutate(ml = str_split(gsub("\\*", "", layer_label), "/")) |> ## ml = "manual layer"
-    unnest_longer("ml") |>
-    mutate(
-        ml = gsub("b", "", ifelse(
-            grepl("^[0-9]", ml), paste0("L", ml), ml)
-        ),
-        match = TRUE)
+#### Plot full datasets ###
+## colors
+load(here(data_dir, "pec_dataset_colors.Rdata"), verbose = TRUE)
 
-cell_type_anno |> count(ml)
-
-## Match with bayesSpace spatial annotations for k09 k16 exploration
-bayes_layers <- get(load(here("processed-data", "rdata", "spe", "08_spatial_registration", "bayesSpace_layer_annotations.Rdata"))) |>
-  select(Annotation = bayesSpace, layers = cluster, layer_combo) |>
-  filter(Annotation %in% c("k09", "k16"))
-
-## Add Layer levels + update factor
-spatial_layer_anno <- tibble(
-    Annotation = "layer",
-    layers = c(paste0("Layer", 1:6), "WM"),
-    layer_combo = layers
-    ) |>
-    bind_rows(bayes_layers) |>
-  mutate(layer_combo = factor(layer_combo, levels = c(paste0("Layer", 1:6), "WM", levels(bayes_layers$layer_combo))),
-         Annotation = factor(Annotation, levels = c("k16", "k09", "layer")))
-
-spatial_layer_anno |> count(Annotation = layer_combo)
-
-spatial_layer_grid <- spatial_layer_anno |> 
+## prep cell type annotation
+spatial_layer_anno_long <- spatial_layer_anno |> 
   mutate(ml = str_split(gsub("S.* ~ |ayer","", layer_combo), "/")) |> ## ml = "manual layer"
+  unnest_longer("ml") |> 
+  mutate(ml = ifelse(grepl("^[0-9]", ml), paste0("L", ml), ml))
+
+cell_type_anno <- pec_cell_type_tb |>
+  mutate(layer_label = ifelse(grepl("^L[0-9]", cell_type), sub(" .*", "", cell_type), NA)) |>
+  filter(!is.na(layer_label)) |>
+  mutate(ml = str_split(gsub("\\*", "", layer_label), "/")) |> ## ml = "manual layer"
   unnest_longer("ml") |>
-  mutate( ml = ifelse(grepl("^[0-9]", ml), paste0("L", ml), ml)) |>
-  left_join(expand_grid(layer_combo = levels(spatial_layer_anno$layer_combo), 
-                        cluster = cell_types)) |> ## expand to all cell types
-  left_join(cell_type_anno |> select(cluster, ml, match)) |> ## TRUE if annotation suggests match
-  replace_na(list(match = FALSE)) |>
-  mutate(layer_combo = factor(layer_combo, levels = levels(spatial_layer_anno$layer_combo)))
-
-spatial_layer_grid |> filter(match) |> count(Annotation)
-
-anno_all_grid <- spatial_layer_grid |>
-  ggplot(aes(x = cluster, y = layer_combo)) +
-  geom_tile(aes(fill = match), color = "grey30") +
-  grid_fill+
-  facet_grid(Annotation ~ ., scales = "free_y", space = "free") +
-  scale_y_discrete(limits = rev) +
-  theme(axis.text.x = element_text(
-    angle = 90,
-    vjust = 0.5,
-    hjust = 1
-  )) 
-
-ggsave(anno_all_grid, filename = here(plot_dir, "anno_all_test.png"))
-
-## Add layer_combo to layer_anno_long to add bayesSpace details
-layer_anno_long <- layer_anno_long |> 
-  # left_join(spatial_layer_anno |> select(Annotation, layers, layer_combo)) |>
-  mutate(layer_combo = factor(layer_combo, levels =  levels(spatial_layer_anno$layer_combo)),
-         Annotation = factor(Annotation, levels = c("k16", "k09", "layer")))
-
-layer_anno_long |> count(Annotation, layer_combo)  
-  
-#### Dot plots ####
-anno_layer_grid <- spatial_layer_grid |>
-  filter(Annotation == "layer") |>
-  ggplot(aes(x = cluster, y = layer_combo)) +
-  geom_tile(aes(fill = match), color = "grey30") +
-  grid_fill +
-  facet_grid(Annotation ~ ., scales = "free_y", space = "free") +
-  scale_y_discrete(limits = rev) +
-  theme(axis.text.x = element_text(
-    angle = 90,
-    vjust = 0.5,
-    hjust = 1
-  )) 
-
-layer_anno_plot <- anno_layer_grid +
-  geom_point(data = layer_anno_long |> filter(Annotation == "layer"), 
-             aes(color = Dataset, shape = anno_confidence),
-             position = position_dodge(width = .8)
-  )  
-
-ggsave(
-    layer_anno_plot,
-    filename = here(plot_dir, "PE_datasets_layer_annotation.png"),
-    width = 10,
-    height = 5
-)
-
-## Filter to high confidence
-layer_anno_plot_filter <- anno_layer_grid +
-  geom_point(data = layer_anno_long |> 
-               filter(Annotation == "layer" & confidence), 
-             aes(color = Dataset),
-             position = position_dodge(width = .8)
-  )  
-
-ggsave(
-    layer_anno_plot_filter,
-    filename = here(plot_dir, "PE_datasets_layer_annotation_confident.png"),
-    width = 10,
-    height = 5
-)
-
-## All annotations
-label_anno_plot <- anno_all_grid +
-  geom_point(data = layer_anno_long, 
-             aes(color = Dataset, shape = anno_confidence),
-             position = position_dodge(width = .8))
-
-ggsave(
-    label_anno_plot,
-    filename = here(plot_dir, "PE_datasets_all_annotations.png"),
-    width = 10
-)
-
-## All filtered to confident annotations 
-label_anno_plot_filter <- anno_all_grid +
-  geom_point(data = layer_anno_long |> filter(confidence), 
-             aes(color = Dataset),
-             position = position_dodge(width = .8)) +
-    labs(y = "BayesSpace Domain & Annotation", x = "PsychENCODE DLPFC Cell Types")+
-  theme(legend.position = "top")
-
-ggsave(
-    label_anno_plot_filter,
-    filename = here(plot_dir, "PE_datasets_all_annotations_confident.png"),
-    height = 7.25,
-    width = 8
-)
-
-## Just layer + k09 for main fig
-
-layer_k9_anno_plot <- spatial_layer_grid |>
-  filter(Annotation != "k16") |>
-  ggplot(aes(x = cluster, y = layer_combo)) +
-  geom_tile(aes(fill = match), color = "grey30") +
-  grid_fill +
-  facet_grid(Annotation ~ ., scales = "free_y", space = "free") +
-  scale_y_discrete(limits = rev) +
-  theme(axis.text.x = element_text(
-    angle = 90,
-    vjust = 0.5,
-    hjust = 1
-  )) +
-  geom_point(data = layer_anno_long |> filter(Annotation != "k16", confidence), 
-             aes(color = Dataset),
-             position = position_dodge(width = .8)
-  )  +
-  labs(y = "BayesSpace Domain & Annotation", x = "PsychENCODE DLPFC Cell Types")+
-  theme(legend.position = "top")
-
-ggsave(
-  layer_k9_anno_plot,
-  filename = here(plot_dir, "PE_datasets_layer-k9_annotation.pdf"),
-  width = 10
-)
+  mutate(
+    ml = gsub("b", "", ifelse(
+      grepl("^[0-9]", ml), paste0("L", ml), ml)),
+    Match = TRUE) |>
+  left_join(spatial_layer_anno_long) |>
+  select(cell_type, layer_combo, Match)
 
 
-## Just k16 for supp
-k16_anno_plot <- spatial_layer_grid |>
-  filter(Annotation == "k16") |>
-  ggplot(aes(x = cluster, y = layer_combo)) +
-  geom_tile(aes(fill = match), color = "grey30") +
-  grid_fill +
-  facet_grid(Annotation ~ ., scales = "free_y", space = "free") +
-  scale_y_discrete(limits = rev) +
-  theme(axis.text.x = element_text(
-    angle = 90,
-    vjust = 0.5,
-    hjust = 1
-  )) +
-  geom_point(data = layer_anno_long |> filter(Annotation == "k16", confidence), 
-             aes(color = Dataset),
-             position = position_dodge(width = .8)
-  )  +
-  labs(y = "BayesSpace Domain & Annotation", x = "PsychENCODE DLPFC Cell Types")+
-  theme(legend.position = "top")
+spatial_layer_anno |> 
+  mutate(ml = str_split(gsub("S.* ~ |ayer","", layer_combo), "/")) |> ## ml = "manual layer"
+  unnest_longer("ml")
 
-ggsave(
-  k16_anno_plot,
-  filename = here(plot_dir, "PE_datasets_k16_annotation.pdf"),
-  width = 10
-)
+source("registration_dot_plot.R")
+
+dotplot <- registration_dot_plot2(layer_anno_long, ct_anno = cell_type_anno, color_by = "Dataset") +
+  scale_color_manual(values = pec_dataset_colors)+
+  labs(x = "PsychENCODE DLPFC Cell Types", y = "Spatial Domains")
+
+ggsave(dotplot, filename = here(plot_dir, paste0("registration_anno_dotplot_all.png")), width = 13)
+ggsave(dotplot, filename = here(plot_dir, paste0("registration_anno_dotplot_all.pdf")), width = 13)
 
 
-# #### Heatmap ####
-# cor_top100 <- transpose(pe_correlation_annotation)$cor_top100
-# corner(cor_top100_2$CMC)
-#
-# ## DEvBrain, IsoHub, and UrbanDLPFC missing
-# # [1] "Sst Chodl"
-# cor_top100_2 <-
-#     map2(cor_top100, names(cor_top100), function(cor_data, dataset) {
-#         cor_data <- map(cor_data, function(cd) {
-#             if (!setequal(rownames(cd), cell_types)) {
-#                 return(setdiff(cell_types, rownames(cd)))
-#             }
-#             cd <- cd[cell_types, ]
-#             rownames(cd) <- paste0(dataset, "_", rownames(cd))
-#             return(cd)
-#         })
-#         return(do.call("cbind", cor_data))
-#     })
-#
-# map(cor_top100$DevBrain$k09, ~ .x[cell_types, ])
-#
-# map(cor_top100$DevBrain, ~ setdiff(rownames(.x), cell_types))
-# map(cor_top100$DevBrain, ~ setdiff(cell_types, rownames(.x)))
-# map(cor_top100$DevBrain, ~ union(cell_types, rownames(.x)))
-
-# sgejobs::job_single('03_correlate_spatial', create_shell = TRUE, memory = '25G', command = "Rscript 03_correlate_spatial.R")
+# sgejobs::job_single('03_correlate_spatial', create_shell = TRUE, memory = '10G', command = "Rscript 03_correlate_spatial.R")
 
 ## Reproducibility information
 print("Reproducibility information:")
