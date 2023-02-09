6fecf72bb78043fefa8e400e946617301d8288d0
Nick-Eagles <nick.eagles@libd.org>
2023-02-09 14:49:30 -0500

    Add wrinkle and manual layer annotations (provided by Prashanti) to the nonIF SPE final object. Part of #137.
    Co-authored-by: Prashanthi Ravichandran <prashanthi.ravichandran@gmail.com>

--- code/analysis/01_build_spe/03_add_deconvolution.R
+++ code/analysis/01_build_spe/03_add_deconvolution.R
@@ -2,7 +2,21 @@ library("spatialLIBD")
 library("here")
 library("jaffelab")
 library("sessioninfo")
+library("tidyverse")
 
+#   Paths for manual annotation of layers and wrinkles, which was done for
+#   3 of the 30 samples
+anno_samples <- c("Br6522_ant", "Br6522_mid", "Br8667_post")
+
+anno_wrinkle_path <- here(
+    "processed-data", "spot_deconvo", "05-shared_utilities", "nonIF",
+    "spatialLIBD_ManualAnnotation_{sample_id}_wrinkle.csv"
+)
+
+anno_layers_path <- here(
+    "processed-data", "spot_deconvo", "05-shared_utilities", "nonIF",
+    "spatialLIBD_ManualAnnotation_{sample_id}_layers.csv"
+)
 
 ## Takes about 4 min
 Sys.time()
@@ -128,6 +142,67 @@ if (any(is.na(spe$VistoSeg_count))) {
 }
 stopifnot(all(!is.na(spe$VistoSeg_proportion)))
 
+#   Collect manual annotation of layers and wrinkles for the 3 annotated
+#   samples, forming a list of tibbles
+anno_list <- list()
+for (sample_id in anno_samples) {
+    #   Adjust paths for this sample
+    this_anno_wrinkle_path <- sub(
+        "\\{sample_id\\}", sample_id, anno_wrinkle_path
+    )
+    this_anno_layers_path <- sub("\\{sample_id\\}", sample_id, anno_layers_path)
+    
+    #   Read in wrinkle annotation and use unique and informative colnames
+    anno_wrinkle <- this_anno_wrinkle_path |>
+        read.csv() |>
+        as_tibble() |>
+        rename(wrinkle_type = ManualAnnotation, barcode = spot_name)
+    
+    #   Read in layer annotation and use unique and informative colnames
+    anno_layers <- this_anno_layers_path |>
+        read.csv() |>
+        as_tibble() |>
+        rename(manual_layer_label = ManualAnnotation, barcode = spot_name)
+    
+    anno_list[[sample_id]] <- anno_layers |>
+        full_join(anno_wrinkle, by = c("sample_id", "barcode"))
+}
+
+#   Combine into a single tibble with all 3 samples
+anno <- do.call(rbind, anno_list) |>
+    as_tibble()
+
+stopifnot(all(anno$barcode %in% colnames(spe)))
+
+added_coldata <- tibble(
+    "barcode" = colnames(spe), "sample_id" = spe$sample_id
+) |>
+    left_join(anno)
+
+#   Add the wrinkle and layer annotation to the SPE's colData
+colData(spe) <- cbind(
+    colData(spe),
+    added_coldata |> select(c("manual_layer_label", "wrinkle_type"))
+)
+
+#   Colnames started out sorted, so we'll mantain that organization
+colData(spe) <- colData(spe)[, sort(colnames(colData(spe)))]
+
+#   Interactively double-check that the annotations merged as expected
+
+# table(is.na(spe$manual_layer_label))
+# FALSE   TRUE 
+# 11991 101936
+
+# table(is.na(spe$wrinkle_type))
+# FALSE   TRUE 
+# 2094 111833
+
+# vis_clus(spe, sampleid = anno_samples[1], clustervar = "wrinkle_type") +
+#     #   Not sure why these are needed
+#     scale_color_discrete() +
+#     scale_fill_discrete()
+# vis_clus(spe, sampleid = anno_samples[1], clustervar = "manual_layer_label")
 
 ## Check the size in GB
 Sys.time()
