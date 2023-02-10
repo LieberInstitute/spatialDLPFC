5915685a83063f7d4af5d76842982302ca50f7c2
Boyi Guo <boyi.guo.work@gmail.com>
2023-02-10 11:42:13 -0500

    styleR

--- code/analysis/15_cell_composition/fun_spatial_pie.R
+++ code/analysis/15_cell_composition/fun_spatial_pie.R
@@ -1,21 +1,24 @@
 # Helper function to generate vis_spatial_pie
 vis_scatter_pie <- function(spe,
-                            plot_df,
-                            sampleid,
-                            image_id,
-                            cell_names,
-                            auto_crop = TRUE,
-                            spatial = FALSE){
-
-    pxl_row_in_fullres <- spatialCoords(spe)[,"pxl_row_in_fullres"]
-    pxl_col_in_fullres <- spatialCoords(spe)[,"pxl_col_in_fullres"]
-    img <- SpatialExperiment::imgRaster(spe, sample_id = sampleid,
-                                        image_id = image_id)
+    plot_df,
+    sampleid,
+    image_id,
+    cell_names,
+    auto_crop = TRUE,
+    spatial = FALSE) {
+    pxl_row_in_fullres <- spatialCoords(spe)[, "pxl_row_in_fullres"]
+    pxl_col_in_fullres <- spatialCoords(spe)[, "pxl_col_in_fullres"]
+    img <- SpatialExperiment::imgRaster(spe,
+        sample_id = sampleid,
+        image_id = image_id
+    )
 
     ## Crop the image if needed
     if (auto_crop) {
-        frame_lims <- spatialLIBD::frame_limits(spe, sampleid = sampleid,
-                                                image_id = image_id)
+        frame_lims <- spatialLIBD::frame_limits(spe,
+            sampleid = sampleid,
+            image_id = image_id
+        )
         img <- img[frame_lims$y_min:frame_lims$y_max, frame_lims$x_min:frame_lims$x_max]
         adjust <- list(x = frame_lims$x_min, y = frame_lims$y_min)
     } else {
@@ -26,7 +29,7 @@ vis_scatter_pie <- function(spe,
     plot_df$scaled_y <- pxl_row_in_fullres * SpatialExperiment::scaleFactors(spe, sample_id = sampleid, image_id = image_id) - adjust$y
 
     ret_plot <- ggplot2::ggplot(
-        #data = plot_df,
+        # data = plot_df,
         # ggplot2::aes(
         #     x = scaled_x,
         #     y = scaled_y
@@ -56,7 +59,7 @@ vis_scatter_pie <- function(spe,
             data = plot_df,
             ggplot2::aes(
                 x = scaled_x,
-                y = -1*scaled_y
+                y = -1 * scaled_y
             ),
             col = cell_names,
             color = NA,
@@ -68,7 +71,8 @@ vis_scatter_pie <- function(spe,
         ggplot2::scale_fill_manual(
             limits = fill_vals,
             labels = fill_labs,
-            values = cell_type_colors) +
+            values = cell_type_colors
+        ) +
         # xlim(0, ncol(img)) +
         # ylim(nrow(img), 0) +
         xlab("") + ylab("") +
@@ -91,22 +95,23 @@ vis_scatter_pie <- function(spe,
 
 # Function to generate spatial scatter pie plot
 vis_spatial_pie <- function(spe,
-                            res,    # Cell Resolution
-                            deconv, # Deconvolution method
-                            image_id,
-                            sample_id,
-                            spatial = FALSE){
-
-    plot_df <-  colData(spe) |> data.frame() |>
+    res, # Cell Resolution
+    deconv, # Deconvolution method
+    image_id,
+    sample_id,
+    spatial = FALSE) {
+    plot_df <- colData(spe) |>
+        data.frame() |>
         dplyr::select(
-            starts_with(paste0(res, "_", deconv)), #cell_names
-            row, col  # Spatial Coordinates
+            starts_with(paste0(res, "_", deconv)), # cell_names
+            row, col # Spatial Coordinates
         ) |>
         rename_with(.fn = str_remove, pattern = paste0(res, "_", deconv, "_"))
     all_cell_names <- grep(
         pattern = paste0(res, "_", deconv),
         x = names(colData(spe)),
-        value = TRUE) |>
+        value = TRUE
+    ) |>
         str_remove(pattern = paste0(res, "_", deconv, "_"))
 
     ret_plot <- vis_scatter_pie(
@@ -115,5 +120,6 @@ vis_spatial_pie <- function(spe,
         sampleid = sample_id,
         image_id = image_id,
         cell_names = all_cell_names,
-        spatial = FALSE)
+        spatial = FALSE
+    )
 }
