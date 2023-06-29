749df4dc88ecfa79099a01a18fef2b4d339e2ac6
Nick-Eagles <nick.eagles@libd.org>
2023-06-29 14:22:21 -0400

    Download the newer CMC data from February 2023

--- code/synapse_upload/03-download/08-download_v6.sh
+++ code/synapse_upload/03-download/08-download_v6.sh
@@ -19,3 +19,20 @@ synapse \
 
 echo "Done at $(date)."
 } > $log_path 2>&1
+
+#   Later (06/29/23), we determined there was an update to the CMC dataset,
+#   which was downloaded to the same 'version6' directory
+
+mkdir -p $out_dir/CMC_update_Feb2023
+
+{
+module load synapse/2.6.0
+
+synapse \
+    -p $(grep "^token:" $credential_path | cut -d " " -f 2 | tr -d '"') \
+    get \
+        -r syn51123335 \
+        --downloadLocation $out_dir/CMC_update_Feb2023
+
+echo "Done at $(date)."
+} > $log_path 2>&1 # Oops, meant to append to log and not overwrite it
