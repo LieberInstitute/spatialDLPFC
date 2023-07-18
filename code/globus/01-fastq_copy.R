4ae1e274ab2a8561109092e0c2883cc71d8bd580
Nick-Eagles <nick.eagles@libd.org>
2023-07-18 15:34:17 -0400

    Make sure to use [slide]_[array] for Visium-SPG sample ID

--- code/globus/01-fastq_copy.R
+++ code/globus/01-fastq_copy.R
@@ -5,6 +5,10 @@ library(jaffelab)
 library(sessioninfo)
 
 visium_info_path = here('raw-data', 'sample_info','Visium_dlpfc_mastersheet.xlsx')
+visium_spg_info_path = here(
+    'raw-data', 'sample_info', 'Visium_IF_DLPFC_MasterExcel_01262022.xlsx'
+)
+
 visium_dir_new = here('raw-data', 'FASTQ_Globus', 'Visium')
 visium_spg_dir_new = here('raw-data', 'FASTQ_Globus', 'Visium_SPG')
 sn_dir_new = here('raw-data', 'FASTQ_Globus', 'snRNA-seq')
@@ -70,35 +74,59 @@ file.copy(fastq_df$fastq_old, fastq_df$fastq_new)
 
 message('Determining paths of old vs. new FASTQs for Visium-SPG...')
 
-fastq_old = list.files(
-    here('raw-data', 'FASTQ', 'Visium_IF'), pattern = '\\.fastq\\.gz$', 
-    recursive = TRUE, full.names = TRUE
+visium_spg_info = read_excel(visium_spg_info_path) |>
+    filter(!is.na(`Sample #`)) |>
+    mutate(
+        position = ss(Br_Region, '_', 2),
+        sample_id_old = paste0('Br', BrNumbr, '_', position, '_IF'),
+        sample_id_new = paste(`Slide SN #`, `Array #`, sep = '_')
+    )
+
+fastq_df = tibble(
+    fastq_old = list.files(
+        here('raw-data', 'FASTQ', 'Visium_IF'), pattern = '\\.fastq\\.gz$', 
+        recursive = TRUE, full.names = TRUE
+    )
 )
+fastq_df$sample_id_old = fastq_old |> str_extract('Br[0-9]{4}_(Ant|Post)_IF')
 
-sample_ids = fastq_old |> str_extract('Br[0-9]{4}_(Ant|Post)_IF')
-lane_mate = fastq_old |> str_extract('_L00[0-9]_R[12]')
-fastq_new = file.path(
-    visium_spg_dir_new, paste0(sample_ids, lane_mate, '.fastq.gz')
-)
+fastq_df = fastq_df |>
+    #   Add sample info columns (notably 'sample_id_new') to fastq_df
+    left_join(visium_spg_info, by = 'sample_id_old') |>
+    #   Determine a filepath for the new FASTQ files using sample_id_new
+    mutate(
+        fastq_new = file.path(
+            visium_spg_dir_new,
+            paste0(
+                sample_id_new,
+                basename(fastq_old) |> str_extract('_R[12]'),
+                '.fastq.gz'
+            )
+        )
+    ) |>
+    #   Only interested in FASTQ file paths: old and new
+    select(c(fastq_old, fastq_new))
 
-#   Actually, a single donor sometimes has more than one FASTQ per mate per
-#   lane. Deduplicate filenames accordingly (just add a suffix '_1', '_2', etc)
-for (fastq_path in unique(fastq_new)) {
-    num_copies = length(which(fastq_new == fastq_path))
-    base_path = ss(basename(fastq_new[fastq_new == fastq_path]), '\\.')
+#   Actually, a single sample sometimes has more than one FASTQ per mate.
+#   Deduplicate filenames accordingly (just add a suffix '_1', '_2', etc)
+for (fastq_path in unique(fastq_df$fastq_new)) {
+    num_copies = length(which(fastq_df$fastq_new == fastq_path))
+    base_path = ss(
+        basename(fastq_df$fastq_new[fastq_df$fastq_new == fastq_path]), '\\.'
+    )
 
-    fastq_new[fastq_new == fastq_path] = file.path(
+    fastq_df$fastq_new[fastq_df$fastq_new == fastq_path] = file.path(
         visium_spg_dir_new,
         paste0(base_path, '_', 1:num_copies, '.fastq.gz')
     )
 }
 
 #   Final sanity checks before copying
-stopifnot(all(file.exists(fastq_old)))
-stopifnot(length(unique(fastq_new)) == length(fastq_new))
+stopifnot(all(file.exists(fastq_df$fastq_old)))
+stopifnot(length(unique(fastq_df$fastq_new)) == length(fastq_df$fastq_new))
 
 message('Copying over Visium-SPG FASTQs to new location...')
-file.copy(fastq_old, fastq_new)
+file.copy(fastq_df$fastq_old, fastq_df$fastq_new)
 
 ################################################################################
 #   snRNA-seq
