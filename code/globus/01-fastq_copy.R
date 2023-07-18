fc1dcb33ba5e9ac167a854a7a8c0b4727c11484a
Nick-Eagles <nick.eagles@libd.org>
2023-07-18 14:39:17 -0400

    Also add code for copying Visium-SPG FASTQs

--- code/globus/01-fastq_copy.R
+++ code/globus/01-fastq_copy.R
@@ -1,11 +1,19 @@
 library(here)
 library(readxl)
 library(tidyverse)
+library(jaffelab)
+library(sessioninfo)
 
 visium_info_path = here('raw-data', 'sample_info','Visium_dlpfc_mastersheet.xlsx')
 visium_dir_new = here('raw-data', 'FASTQ_Globus', 'Visium')
+visium_spg_dir_new = here('raw-data', 'FASTQ_Globus', 'Visium_SPG')
 
 dir.create(visium_dir_new, recursive = TRUE, showWarnings = FALSE)
+dir.create(visium_spg_dir_new, recursive = FALSE, showWarnings = FALSE)
+
+################################################################################
+#   Visium
+################################################################################
 
 message('Determining paths of old vs. new FASTQs for Visium...')
 
@@ -46,5 +54,47 @@ fastq_df = fastq_df |>
     #   Only interested in FASTQ file paths: old and new
     select(c(fastq_old, fastq_new))
 
+#   Final sanity checks before copying
+stopifnot(all(file.exists(fastq_df$fastq_old)))
+stopifnot(length(unique(fastq_df$fastq_new)) == length(fastq_df$fastq_new))
+
 message('Copying over Visium FASTQs to new location...')
 file.copy(fastq_df$fastq_old, fastq_df$fastq_new)
+
+################################################################################
+#   Visium-SPG
+################################################################################
+
+message('Determining paths of old vs. new FASTQs for Visium-SPG...')
+
+fastq_old = list.files(
+    here('raw-data', 'FASTQ', 'Visium_IF'), pattern = '\\.fastq\\.gz$', 
+    recursive = TRUE, full.names = TRUE
+)
+
+sample_ids = fastq_old |> str_extract('Br[0-9]{4}_(Ant|Post)_IF')
+lane_mate = fastq_old |> str_extract('_L00[0-9]_R[12]')
+fastq_new = file.path(
+    visium_spg_dir_new, paste0(sample_ids, lane_mate, '.fastq.gz')
+)
+
+#   Actually, a single donor sometimes has more than one FASTQ per mate per
+#   lane. Deduplicate filenames accordingly (just add a suffix '_1', '_2', etc)
+for (fastq_path in unique(fastq_new)) {
+    num_copies = length(which(fastq_new == fastq_path))
+    base_path = ss(basename(fastq_new[fastq_new == fastq_path]), '\\.')
+
+    fastq_new[fastq_new == fastq_path] = file.path(
+        visium_spg_dir_new,
+        paste0(base_path, '_', 1:num_copies, '.fastq.gz')
+    )
+}
+
+#   Final sanity checks before copying
+stopifnot(all(file.exists(fastq_old)))
+stopifnot(length(unique(fastq_new)) == length(fastq_new))
+
+message('Copying over Visium-SPG FASTQs to new location...')
+file.copy(fastq_old, fastq_new)
+
+session_info()
