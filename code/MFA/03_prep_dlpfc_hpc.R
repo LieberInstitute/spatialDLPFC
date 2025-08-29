289049de65c247519788186109a363366e940fc9
Nick-Eagles <nick.eagles@libd.org>
2025-08-29 12:40:10 -0400

    Adjust MOFA prep script to generate input files needed for DLPFC and DLPFC + HPC

--- code/MFA/03_prep_dlpfc_hpc.R
+++ code/MFA/03_prep_dlpfc_hpc.R
@@ -3,11 +3,22 @@ library(sessioninfo)
 library(here)
 library(tidyverse)
 
-spe_path = here('processed-data', 'MFA', 'pseudobulk_rds', 'visium_pb.rds')
-sce_path = here('processed-data', 'MFA', 'pseudobulk_rds', 'sn_fine_pb.rds')
-out_path = here('processed-data', 'MFA', 'combined_rds', 'DLPFC.rds')
+dlpfc_spe_path = here('processed-data', 'MFA', 'pseudobulk_rds', 'visium_pb.rds')
+dlpfc_sce_path = here(
+    'processed-data', 'MFA', 'pseudobulk_rds', 'sn_fine_pb.rds'
+)
+hpc_spe_path = here(
+    'processed-data', 'MFA', 'pseudobulk_rds', 'hpc_visium_pb.rds'
+)
+hpc_sce_path = here(
+    'processed-data', 'MFA', 'pseudobulk_rds', 'hpc_sn_pb.rds'
+)
+dlpfc_out_path = here('processed-data', 'MFA', 'combined_rds', 'DLPFC.rds')
+combined_out_path = here(
+    'processed-data', 'MFA', 'combined_rds', 'combined.rds'
+)
 
-dir.create(dirname(out_path), showWarnings = FALSE)
+dir.create(dirname(dlpfc_out_path), showWarnings = FALSE)
 
 ################################################################################
 #   Functions
@@ -34,13 +45,9 @@ simplify_sce = function(sce, donor_var, cluster_var, shared_cols) {
 #   previously pseudobulked by donor and cluster, but may have different column
 #   names for these colData variables. Return a list of inputs for MOFA
 combine_sce = function(
-        sce_path1, sce_path2, donor_var1, donor_var2, cluster_var1, cluster_var2,
+        sce1, sce2, donor_var1, donor_var2, cluster_var1, cluster_var2,
         shared_cols
     ) {
-    #   Load objects
-    sce1 = readRDS(sce_path1)
-    sce2 = readRDS(sce_path2)
-
     #   Ensure genes match
     shared_genes = intersect(rownames(sce1), rownames(sce2))
     sce1 = sce1[shared_genes, ]
@@ -64,15 +71,44 @@ combine_sce = function(
 ################################################################################
 
 #   Combine spatialDLPFC Visium and fine snRNA-seq objects
-mofa_list = combine_sce(
-    sce_path1 = sce_path,
-    sce_path2 = spe_path,
+dlpfc_mofa_list = combine_sce(
+    sce1 = readRDS(dlpfc_sce_path),
+    sce2 = readRDS(dlpfc_spe_path),
     donor_var1 = 'BrNum',
     donor_var2 = 'subject',
     cluster_var1 = 'cellType_layer',
     cluster_var2 = 'BayesSpace_harmony_09',
     shared_cols = c('age', 'sex')
 )
-saveRDS(mofa_list, out_path)
+
+#   Combine HPC Visium and snRNA-seq objects
+hpc_spe = readRDS(hpc_spe_path)
+hpc_spe$cluster = NULL
+hpc_mofa_list = combine_sce(
+    sce1 = readRDS(hpc_sce_path),
+    sce2 = hpc_spe,
+    donor_var1 = 'brnum',
+    donor_var2 = 'brnum',
+    cluster_var1 = 'broad.class',
+    cluster_var2 = 'domain',
+    shared_cols = c()
+)
+
+#   Combine DLPFC and HPC
+dlpfc_mofa_list$sce$cluster = paste0("DLPFC_", dlpfc_mofa_list$sce$cluster)
+hpc_mofa_list$sce$cluster = paste0("HPC_", hpc_mofa_list$sce$cluster)
+stopifnot(setequal(dlpfc_mofa_list$sce$donor, hpc_mofa_list$sce$donor))
+combined_mofa_list = combine_sce(
+    sce1 = dlpfc_mofa_list$sce,
+    sce2 = hpc_mofa_list$sce,
+    donor_var1 = 'donor',
+    donor_var2 = 'donor',
+    cluster_var1 = 'cluster',
+    cluster_var2 = 'cluster',
+    shared_cols = c()
+)
+combined_mofa_list$pd = dlpfc_mofa_list$pd
+saveRDS(dlpfc_mofa_list, dlpfc_out_path)
+saveRDS(combined_mofa_list, combined_out_path)
 
 session_info()
