8aee8b78071118fedd4fbbcc27ff0499ce28cc8e
Nick-Eagles <nick.eagles@libd.org>
2025-09-11 13:17:14 -0400

    Add code for producing gene-weight heatmaps from https://github.com/LieberInstitute/LFF_spatial_ERC/blob/b9988e0c6ec9f944c490f99b5f54b5f2023a76df/code/14_MOFA/02_MOFA_gw_heatmaps.R#L67C1-L112C10
    Co-authored-by: Louise Huuki <lahuuki@gmail.com>

--- code/MFA/05_gene_weights.R
+++ code/MFA/05_gene_weights.R
@@ -20,3 +20,50 @@ sce_path = here(
 model = load_model(model_path)
 
 sce = readRDS(sce_path)$sce
+
+top_gene_weights <- gene_weights$Factor3 |>
+    mutate(abs_value = abs(value),
+           weight_pos = value > 0,
+           datatype = ifelse(grepl("_Sp", ctype), "Visium", "snRNA-seq")) |>
+    filter(ctype %in% my_views) |>
+    group_by(ctype,weight_pos) |>
+    arrange(-abs_value) |>
+    dplyr::slice(1:5)
+
+
+## prep heatmap 
+top_gw_value_matrix <- gene_weights$Factor3 |>
+    filter(feature %in% top_gene_weights$feature) |>
+    select(gene_name, ctype, value) |>
+    pivot_wider(names_from = ctype, values_from = value) |>
+    column_to_rownames("gene_name") |>
+    as.matrix()
+
+col_order <- cluster_levels[cluster_levels %in% colnames(top_gw_value_matrix)]
+
+# row_order <- order(top_gw_value_matrix[,"Oligo"])
+
+row_order <- top_gene_weights |>
+    group_by(gene_name) |> 
+    summarise(max_value = max(value)) |>
+    arrange(-max_value) |>
+    pull(gene_name)
+
+top_gw_value_matrix <- top_gw_value_matrix[row_order,col_order]
+
+dim(top_gw_value_matrix)
+top_gw_value_matrix[1:5, 1:5]
+
+
+pdf_width_all <- (nrow(top_gw_value_matrix)/8) + 3
+pdf_width_select <- (length(my_views)/8) + 3
+
+pdf_height <- length(row_order)/5
+
+## weights across all clusters
+pdf(here(plot_dir, sprintf("MOFA_gene_weight_heatmap_%s_all.pdf", opt$datatype)), width = pdf_width_all, height = pdf_height)
+Heatmap(top_gw_value_matrix,
+        name = "feature\nweights",
+        cluster_rows = FALSE,
+        cluster_columns = FALSE)
+dev.off()
