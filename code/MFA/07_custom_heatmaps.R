33ec91353b644ebb9bf5410c2a41f9333c807489
Nick-Eagles <nick.eagles@libd.org>
2025-10-13 10:17:52 -0400

    Finish up the heatmap as a whole

--- code/MFA/07_custom_heatmaps.R
+++ code/MFA/07_custom_heatmaps.R
@@ -117,3 +117,41 @@ column_ha = HeatmapAnnotation(
     border = TRUE,
     col = list(R2 = col_fun_r2, pvalue = col_fun_assoc)
 )
+
+################################################################################
+#   Main heatmap body
+################################################################################
+
+factor_matrix = get_factors(model, factors = "all")$single_group
+
+max_fact = abs(max(factor_matrix))
+col_fun_fact = colorRamp2(
+    seq((max_fact + 0.5) * -1, max_fact + 0.5, length = 50),
+    hcl.colors(50, "Green-Brown", rev = TRUE)
+)
+
+################################################################################
+#   Putting it all together
+################################################################################
+
+scores_hmap = Heatmap(
+    factor_matrix,
+    name = "factor_scores",
+    top_annotation = column_ha,
+    cluster_columns = FALSE,
+    show_row_dend = FALSE,
+    show_row_names = FALSE,
+    border = TRUE,
+    gap = unit(2.5, "mm"),
+    col = col_fun_fact
+)
+
+pdf(
+    plot_path,
+    width = as.integer(round(num_factors / 3) + 2),
+    height = as.integer(round(num_views_per_type * 0.75) + 4)
+)
+ComplexHeatmap::draw(scores_hmap, padding = unit(c(2, 2, 2, 15), "mm"))
+dev.off()
+
+session_info()
