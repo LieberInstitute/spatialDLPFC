1e340a17796404c92326ab8149e14fdb1cb8e3d6
Nick-Eagles <nick.eagles@libd.org>
2025-10-17 15:31:14 -0400

    For HPC_sn_CA2.4 specifically, create a gene weight heatmap of just genes positively associated with the factor with MF GO terms

--- code/MFA/05_gene_weights.R
+++ code/MFA/05_gene_weights.R
@@ -86,7 +86,8 @@ gene_weight_heatmap = function(
     ## weights across all clusters
     pdf(
         file.path(plot_dir, sprintf('%s_all.pdf', plot_prefix)),
-        width = (ncol(top_gw_value_matrix) / 4) + 3, height = nrow(view_table) / 4
+        width = (ncol(top_gw_value_matrix) / 4) + 3,
+        height = length(unique(select_genes)) / 4 + 1.5
     )
     Heatmap(
         top_gw_value_matrix[rownames(view_table),],
@@ -100,7 +101,8 @@ gene_weight_heatmap = function(
     ## weights for selected clusters only
     pdf(
         file.path(plot_dir, sprintf('%s_select.pdf', plot_prefix)),
-        width = (length(my_views) / 4) + 3, height = nrow(view_table) / 4
+        width = (length(my_views) / 4) + 3,
+        height = length(unique(select_genes)) / 4 + 1.5
     )
     Heatmap(
         top_gw_value_matrix[rownames(view_table), my_views],
@@ -110,6 +112,37 @@ gene_weight_heatmap = function(
         right_annotation = view_table_row
     ) |> print()
     dev.off()
+}
+
+do_go = function(gene_list, universe, plot_path) {
+    go_result_list = list()
+    for (ont_type in c("BP", "MF", "CC")) {
+        go_obj = compareCluster(
+            gene_list, fun = "enrichGO", universe = universe,
+            OrgDb = org.Hs.eg.db, ont = "ALL", pAdjustMethod = "BH",
+            pvalueCutoff = 1, qvalueCutoff = 1, readable = TRUE,
+            keyType = "ENSEMBL"
+        )
+
+        if(!is.null(go_obj)) {
+            go_obj@compareClusterResult = go_obj@compareClusterResult |>
+                filter(p.adjust < fdr_cutoff, ONTOLOGY == ont_type)
+
+            go_result_list[[ont_type]] = go_obj@compareClusterResult
+            
+            if(nrow(go_obj@compareClusterResult) > 0) {
+                pdf(
+                    sprintf(plot_path, ont_type),
+                    height = as.integer(
+                        min(11, 2 + nrow(go_obj@compareClusterResult) * 0.7)
+                    ),
+                )
+                print(dotplot(go_obj, showCategory = 15))
+                dev.off()
+            }
+        }
+    }
+    return(go_result_list)
 }
 
 ################################################################################
@@ -177,33 +210,6 @@ gene_weight_heatmap(
 #   GO of top-weighted genes
 ################################################################################
 
-do_go = function(gene_list, universe, plot_path) {
-    for (ont_type in c("BP", "MF", "CC")) {
-        go_obj = compareCluster(
-            gene_list, fun = "enrichGO", universe = universe,
-            OrgDb = org.Hs.eg.db, ont = "ALL", pAdjustMethod = "BH",
-            pvalueCutoff = 1, qvalueCutoff = 1, readable = TRUE,
-            keyType = "ENSEMBL"
-        )
-
-        if(!is.null(go_obj)) {
-            go_obj@compareClusterResult = go_obj@compareClusterResult |>
-                filter(p.adjust < fdr_cutoff, ONTOLOGY == ont_type)
-            
-            if(nrow(go_obj@compareClusterResult) > 0) {
-                pdf(
-                    sprintf(plot_path, ont_type),
-                    height = as.integer(
-                        min(11, 2 + nrow(go_obj@compareClusterResult) * 0.7)
-                    ),
-                )
-                print(dotplot(go_obj, showCategory = 15))
-                dev.off()
-            }
-        }
-    }
-}
-
 #-------------------------------------------------------------------------------
 #   One view at a time
 #-------------------------------------------------------------------------------
@@ -229,13 +235,41 @@ for (this_view in my_views) {
         filter(ctype == this_view) |>
         pull(feature)
 
-    do_go(
+    go_result_list = do_go(
         gene_list,
         universe,
         file.path(
             plot_dir, 'GO', sprintf('%%s_%s_%s.pdf', specific_factor, this_view)
         )
     )
+
+    #   Hardcode a special case: for HPC_sn_CA2.4, MF GO results should return
+    #   one term positively associated with the factor; extract the gene names
+    #   for that term and generate a gene weight heatmap
+    if (this_view == 'HPC_sn_CA2.4') {
+        stopifnot(
+            go_result_list[['MF']] |> filter(Cluster == 'up') |> nrow() == 1
+        )
+
+        gene_names = go_result_list[['MF']] |>
+            filter(Cluster == 'up') |>
+            pull(geneID) |>
+            strsplit('/') |>
+            unlist()
+        select_genes = rowData(sce)$gene_id[
+            match(gene_names, rowData(sce)$gene_name)
+        ]
+        stopifnot(all(select_genes %in% top_gene_weights$feature))
+
+        gene_weight_heatmap(
+            top_gene_weights = top_gene_weights,
+            my_views = my_views,
+            view_colors = view_colors,
+            select_genes = select_genes,
+            plot_dir = file.path(plot_dir, 'gene_weights'),
+            plot_prefix = sprintf('%s_%s_GO_MF', specific_factor, this_view)
+        )
+    }
 }
 
 #-------------------------------------------------------------------------------
